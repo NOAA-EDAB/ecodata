aaf51f17497e6bf624c1e6d51c392a97708bb63d
seanhardison1 <seanhardison@gmail.com>
2018-10-24 12:55:50 -0400

    SOE data package

---
+++ NEWS.md
@@ -0,0 +1,4 @@
+DataVersion: 0.1.0
+=======================
+Package built in non-interactive mode
+
---
+++ R/soe.R
@@ -0,0 +1,14 @@
+#' soe
+#' A data package for soe.
+#' @docType package
+#' @aliases soe-package
+#' @title Package Title
+#' @name soe
+#' @description A description of the data package
+#' @details Use \code{data(package='soe')$results[, 3]} tosee a list of availabledata sets in this data package
+#'     and/or DataPackageR::load_all
+#' _datasets() to load them.
+#' @seealso
+#' NA
+#' \link{soe}
+NULL
