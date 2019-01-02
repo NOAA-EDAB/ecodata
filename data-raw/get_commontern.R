612917e856e0a0f02b034660c6e50e2213a83faa
seanhardison1 <seanhardison@gmail.com>
2019-01-02 11:34:56 -0500

    Added function for processing common tern data to data-raw/. Raw data added to inst/extdata and clean data to data/.

---
+++ data-raw/get_commontern.R
@@ -0,0 +1,42 @@
+# Process raw common tern productivity and diet data
+
+# If save == TRUE, processed data are saved as a .Rds file to the data directory. 
+
+# Function returns a processed data frame containing time series of productivity and diet
+# composition for common tern at different islands in southern Gulf of Maine. The first three letters
+# in the variable "Var" correspond to the island at which sampling occurred, with COTE referring to 
+# Common Tern. If "Var" includes productivity, then "Value" is the average number of fledged chicks.
+# Otherwise, if "Var" specifies diet, then  "Value"  is equal to the count of observed prey type. 
+# For example, "EER COTE Diet Amphipod" refers to the number of preyed upon amphipods observed at Eastern
+# Egg Rock in a given year. 
+
+
+
+get_commontern <- function(save = F){
+  
+  #Get raw data
+  raw.dir <- here::here("inst","extdata")
+  d <- read.csv(file.path(raw.dir,"Audubon SRP Common Tern Data.csv"))
+  
+  #Process
+  cote <- d %>%
+    dplyr::filter(Island != "") %>% 
+    tidyr::gather(., Var, Value, -Year, -Species, -Island) %>%
+    dplyr::mutate(Species = ifelse(Var != "Productivity",
+                                   paste(Species, "Diet"), "COTE")) %>% 
+    tidyr::unite(., "Var", c("Island", "Species", "Var"), sep = " ") %>% 
+    dplyr::rename(Time = Year) %>% 
+    dplyr::mutate(EPU = "GOM",
+                  Units = ifelse(stringr::str_detect(Var, "Productivity"),
+                                 "fledged chicks per nest","N")) 
+  
+  if (save){
+    clean.dir <- here::here("data")
+    save(cote, file = file.path(clean.dir,"common_tern.rds"))
+  } else {
+    return(cote)
+  }
+}
+
+  
+
