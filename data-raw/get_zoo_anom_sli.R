aad9d7452f7b0fa23d64b754d18d20766a160cc1
seanhardison1 <seanhardison@gmail.com>
2019-01-03 13:24:13 -0500

    Added function to process zooplanton abundance anomaly and small-large index time series; data now available

---
+++ data-raw/get_zoo_anom_sli.R
@@ -0,0 +1,32 @@
+# Process zooplankton abundance anomalies and small-large index 
+
+library(dplyr)
+library(tidyr)
+
+raw.dir <- here::here("inst","extdata")
+
+get_zoo_anom_sli <- function(save_clean = F){
+  
+  load(file.path(raw.dir, "1977_2017_SLI_Calfin_Pseudo_Ctyp.Rdata"))
+  
+  zoo_anom_sli <- Zooplankton_Primary_Prod %>%
+  gather(.,Var, Value, -year) %>% 
+  dplyr::rename(Time = year) %>% 
+  separate(., Var, c("Var","EPU")) %>% 
+  mutate(EPU = plyr::mapvalues(EPU, from = c("gom","gbk","scs","mab"), 
+                               to = c("GOM","GB","SS","MAB")),
+         Var = plyr::mapvalues(Var, from = c("PseAnom",
+                                             "CtyAnom",
+                                             "CalAnom",
+                                             "SLI"), 
+                               to = c("pseudocalanus anomaly",
+                                      "centropages anomaly",
+                                      "calanus anomaly",
+                                      "small-large index"))) 
+  
+  if (save_clean){
+    usethis::use_data(zoo_anom_sli, overwrite = T)
+  } else {
+    return(zoo_anom_sli)
+  }
+}
