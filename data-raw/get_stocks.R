903138174cfccc182767088af4cc1d26d111a711
seanhardison1 <seanhardison@gmail.com>
2019-01-07 15:15:52 -0500

    Function to process stock status data into long format

---
+++ data-raw/get_stocks.R
@@ -0,0 +1,34 @@
+# Processing single-species stock mortality and biomass data
+
+# Read more about this data at https://noaa-edab.github.io/tech-memo/stockstatus.html
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+data.dir <- here::here('inst','extdata')
+
+get_stocks <- function(save_clean = F){
+  assess <- read.csv(file.path(data.dir, "2018assess.csv"))
+  decode <- read.csv(file.path(data.dir, "2018decoder.csv"))
+  
+  stock_status <- 
+    assess %>%
+    group_by(Entity.Name) %>%
+    filter(Assessment.Year == max(Assessment.Year)) %>% #Find last year assessment occurred for each stock
+    ungroup() %>% 
+    left_join(.,decode, by = "Entity.Name") %>% #Join in list of managed species
+    dplyr::select(Entity.Name, Assessment.Year, F.Fmsy, B.Bmsy, Council, Code) %>% #select column variables to keep
+    mutate(id = 1:length(Entity.Name)) %>% 
+    gather(.,Var, Value,-id,-Entity.Name,-Assessment.Year,-Council,-Code) %>% #wide to long
+    dplyr::select(-id) %>% 
+    dplyr::rename(`Last assessment` = Assessment.Year,
+                  Stock = Entity.Name) %>% #rename variables for clarity
+    mutate(Units = "unitless")
+  
+  if (save_clean){
+    usethis::use_data(stock_status, overwrite = T)
+  } else {
+    return(stock_status)
+  }
+}
