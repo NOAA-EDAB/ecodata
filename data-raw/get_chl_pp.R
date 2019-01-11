4eae3e108edde5fd9a162d67863554456485f2d9
seanhardison1 <seanhardison@gmail.com>
2019-01-11 10:43:39 -0500

    Fixed variables names to make unique

--- data-raw/get_chl_pp.R
+++ data-raw/get_chl_pp.R
@@ -18,16 +18,22 @@ get_chl_pp <- function(save_clean = F){
   chl_pp <- NULL
   for (i in ls()){
     if (str_detect(i, "OCCCI|MODIS")){
-      assign('chl_pp', rbind(chl_pp, get(i)))    
+      print(i)
+      t <- get(i) %>% 
+        mutate(ALGORITHM = word(str_replace(ALGORITHM, "_", " "))) %>% 
+        unite(.,VARIABLE, c("VARIABLE","SENSOR","ALGORITHM"), sep = " ") %>% 
+        mutate(VARIABLE = ifelse(str_detect(FILENAME, "1998_2018"), paste(VARIABLE,"1998_2018"),
+                                 ifelse(str_detect(FILENAME, "1998_2017"), paste(VARIABLE, "1998_2017"),
+                                    ifelse(str_detect(FILENAME, "1997_2018"), paste(VARIABLE, "1997_2018"),
+                                        ifelse(str_detect(FILENAME, "1997_2017"), paste(VARIABLE, "1997_2017"),
+                                           VARIABLE))))) %>% 
+        dplyr::select(TIME, UNITS, VARIABLE, VALUE, REGION) %>% 
+        dplyr::rename(Time = TIME, Units = UNITS, Var = VARIABLE,
+                      EPU = REGION, Value = VALUE)  
+      
+    assign('chl_pp', rbind(chl_pp, t))    
     }
   }
-  
-  chl_pp <- chl_pp %>%
-    mutate(ALGORITHM = word(str_replace(ALGORITHM, "_", " "))) %>% 
-    unite(.,VARIABLE, c("VARIABLE","SENSOR","ALGORITHM"), sep = " ") %>% 
-    dplyr::select(TIME, UNITS, VARIABLE, VALUE, REGION) %>% 
-    dplyr::rename(Time = TIME, Units = UNITS, Var = VARIABLE,
-                  EPU = REGION, Value = VALUE)
   
   if (save_clean){
     usethis::use_data(chl_pp, overwrite = T)
