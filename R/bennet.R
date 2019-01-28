6fee8d952ac229f9f3f6e7716f76ca38d8700880
seanhardison1 <seanhardison@gmail.com>
2019-01-28 10:43:40 -0500

    Documentation for the Bennet indicator.

---
+++ R/bennet.R
@@ -0,0 +1,19 @@
+#' Bennet indicator
+#' 
+#' Data contain volume and price indicators for commercial fisheries on the Northeast Continental Shelf,
+#' along with their sum totals for each year and species groupings (i.e. the Bennet indicator). This 
+#' metric allows for examining the drivers of revenue change by Ecological Production Unit (EPU).
+#' 
+#' @format A data set containing 2240 rows and 5 columns.
+#' 
+#' \itemize{
+#'     \item Var: Specifies indicator type (price, volume, or total) and associated species grouping.
+#'     \item Value: Value of variable \code{Var}.
+#'     \item Time: Year. 
+#'     \item Units: Units of variable \code{Var}.
+#'     \item EPU: Ecological Production Unit (EPU) from which data were drawn. 
+#' }
+#' 
+#' @details These data are documented in full at \url{https://noaa-edab.github.io/tech-memo/bennet-indicator.html}.
+#' 
+"bennet"
