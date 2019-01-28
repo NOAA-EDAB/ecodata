2ba6a24dabd9fe46b48626a5c561a064e29d7d0b
seanhardison1 <seanhardison@gmail.com>
2019-01-28 11:25:00 -0500

    Documentation for CHL/PP

--- data-raw/get_chl_pp.R
+++ data-raw/get_chl_pp.R
@@ -20,17 +20,17 @@ get_chl_pp <- function(save_clean = F){
     if (str_detect(i, "OCCCI|MODIS")){
       print(i)
       t <- get(i) %>% 
-        mutate(ALGORITHM = word(str_replace(ALGORITHM, "_", " "))) %>% 
-        unite(.,VARIABLE, c("VARIABLE","SENSOR","ALGORITHM"), sep = " ") %>% 
-        mutate(VARIABLE = ifelse(str_detect(FILENAME, "1998_2018"), paste(VARIABLE,"1998_2018"),
-                                 ifelse(str_detect(FILENAME, "1998_2017"), paste(VARIABLE, "1998_2017"),
-                                    ifelse(str_detect(FILENAME, "1997_2018"), paste(VARIABLE, "1997_2018"),
-                                        ifelse(str_detect(FILENAME, "1997_2017"), paste(VARIABLE, "1997_2017"),
-                                           VARIABLE))))) %>% 
-        dplyr::select(TIME, UNITS, VARIABLE, VALUE, REGION) %>% 
-        dplyr::rename(Time = TIME, Units = UNITS, Var = VARIABLE,
-                      EPU = REGION, Value = VALUE)  
-      
+       mutate(ALGORITHM = word(str_replace(ALGORITHM, "_", " "))) %>%
+       unite(.,VARIABLE, c("VARIABLE","SENSOR","ALGORITHM"), sep = " ") %>%
+       mutate(VARIABLE = ifelse(str_detect(FILENAME, "1998_2018"), paste(VARIABLE,"1998_2018"),
+                                ifelse(str_detect(FILENAME, "1998_2017"), paste(VARIABLE, "1998_2017"),
+                                   ifelse(str_detect(FILENAME, "1997_2018"), paste(VARIABLE, "1997_2018"),
+                                       ifelse(str_detect(FILENAME, "1997_2017"), paste(VARIABLE, "1997_2017"),
+                                          VARIABLE))))) %>%
+      dplyr::select(TIME, UNITS, VARIABLE, VALUE, REGION) %>%
+       dplyr::rename(Time = TIME, Units = UNITS, Var = VARIABLE,
+                     EPU = REGION, Value = VALUE)
+
     assign('chl_pp', rbind(chl_pp, t))    
     }
   }
