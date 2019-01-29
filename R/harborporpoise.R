b9008d4f5f12a1c3270071117f14747a9f44a348
seanhardison1 <seanhardison@gmail.com>
2019-01-28 20:55:06 -0500

    Documentation for harbor porpoise bycatch indicator

---
+++ R/harborporpoise.R
@@ -0,0 +1,22 @@
+#' Harbor porpoise bycatch
+#' 
+#' Data included here show the 5 year moving average for harbor porpoise bycatch estimates and potential
+#' biological removal (PBR) levels in the Northeast United States. These data were extracted from NEFSC Center
+#' Reference Documents and marine mammal stock assessments. 
+#' 
+#' @format Data set includes 120 rows and 5 columns.
+#' 
+#' \itemize{
+#'     \item Var: Specifies variable of interest, including bycatch estimates (five year moving average) with
+#'     coefficient of variation and confidence intervals. Also included is the potential biological removal (PBR) for
+#'     harbor porpoise.
+#'     \item Value: Value of variable \code{Var}.
+#'     \item Time: Year.
+#'     \item Units: Units of variable \code{Var}.
+#'     \item EPU: Ecological Production Unit (EPU) where sampling occurred.
+#' }
+#' 
+#' @details Links to source data, further data definitions, and extended methodology are available at
+#' \url{https://noaa-edab.github.io/tech-memo/harbor-porpoise-bycatch.html}
+#' 
+"harborporpoise"
