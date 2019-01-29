78ab49a5851cd5877f03ad63d7e1a7ae40c88e16
seanhardison1 <seanhardison@gmail.com>
2019-01-28 20:54:23 -0500

    Documentation for EPU sf objects

---
+++ R/epu_sf.R
@@ -0,0 +1,13 @@
+#' Ecological Production Units
+#' 
+#' Ecological Production Units (EPUs) delineate regions unique in physiographic, oceanographic and biotic aspects
+#' on the Northeast U.S. Continental Shelf, representing an area of approximately 264,000 km within the 200 m
+#' isobath. There are four EPUs on the Shelf, including the Mid-Atlantic Bight (MAB), Georges Bank (GB), Gulf of
+#' Maine (GOM), and Scotian Shelf (SS). The Scotian Shelf lies outside the management domain of US Fishery Management
+#' Councils and is generally not considered in State of the Ecosystem Reports.
+#' 
+#' @format This data set is a \code{sf} object with 4 features and 3 fields. 
+#' 
+#' @details More information about the delineation of Ecological Production Units can be found at 
+#' \url{https://noaa-edab.github.io/tech-memo/epu.html}.
+"epu_sf"
