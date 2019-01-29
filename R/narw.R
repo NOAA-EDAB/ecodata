84f3bceb58bad121b3f62c76ddc313a4f8e780db
seanhardison1 <seanhardison@gmail.com>
2019-01-28 21:30:31 -0500

    Documentation for NARW data

---
+++ R/narw.R
@@ -0,0 +1,23 @@
+#' North Atlantic right whale abundance
+#' 
+#' Data include abundance estimates for the North Atlantic right whale (NARW) provided by the Protected Species
+#' Branch of the NEFSC. Abundance was estimated following the methodology of Pace et. al 2017.
+#' 
+#' @format These data include 84 rows and 5 columns.
+#' 
+#' \itemize{
+#'     \item Var: Specifies variable of interest, including NARW abundance estimate median and 95% credible
+#'     intervals.
+#'     \item Value: Value of variable \code{Var}.
+#'     \item Units: Units of variable \code{Var}.
+#'     \item Time: Year.
+#'     \item EPU: Ecological Production Unit (EPU).
+#' }
+#' 
+#' @details More information about these data may be found \url{https://noaa-github.io/tech-memo/right-whale-abundance.html}.
+#' 
+#' @references 
+#' 
+#' Pace, R. M., Corkeron, P. J., Krauss, S. D. (2017) State-space mark-recapture estimates reveal a recent decline in abundance of North Atlantic right whales.
+#'  \emph{Ecology and Evolution}, 1-17.
+"narw"
