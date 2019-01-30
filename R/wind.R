a0b0cd73baa0202e59db001f26c00d1544333e78
seanhardison1 <seanhardison@gmail.com>
2019-01-30 07:45:54 -0500

    Fixed exporting issues

--- R/wind.R
+++ R/wind.R
@@ -1,7 +1,7 @@
 #' NCEP NARR Wind 
 #' 
 #' These time series include mean surface wind speed and direction ("uwnd" and "vwnd" respectively), 
-#  surface turbulent kinetic energy (TKE), storm relative helicity (HLCY), and total wind speed (TWS) 
+#' surface turbulent kinetic energy (TKE), storm relative helicity (HLCY), and total wind speed (TWS) 
 #' in the Northeast Large Marine Ecosystem. 
 #' 
 #' @format Data set inclues 2385 rows and 5 columns
@@ -15,13 +15,10 @@
 #' }
 #' 
 #' @details 
-#' 
 #' Indicator for total wind speed is calculated as TWS = sqrt(u^2 + v^2), where u = surface wind speed
 #' and v = surface wind direction. Seasons are defined as: Fall = October, November, December;
 #' Winter = January, February, March; Spring = April, May, June; Summer = July, August, September.
 #' 
 #' @source 
-#' 
 #' \url{https://www.esrl.noaa.gov/psd/data/gridded/data.narr.monolevel.html}
-#' 
 "wind"
