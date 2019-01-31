c000d3cb15453e00d39dd79c957f9668d7cd3e75
seanhardison1 <seanhardison@gmail.com>
2019-01-31 17:24:07 -0500

    Added long-term ERSST data set

---
+++ data-raw/get_long_term_sst.R
@@ -0,0 +1,22 @@
+#process ERSST long-term SST data
+
+library(dplyr)
+library(tidyr)
+
+raw.dir <- here::here("inst","extdata")
+
+get_long_term_sst <- function(save_clean = F){
+  long_term_sst <- read.csv(file.path(raw.dir,"ersst annual mean.csv")) %>% 
+    dplyr::rename(Time = Year,
+                  Value = Mean) %>% 
+    mutate(Var = "long-term sst",
+           EPU = "All",
+           Units = "degreesC")
+  
+  if (save_clean) {
+    usethis::use_data(long_term_sst, overwrite = T)
+  } else {
+    return(long_term_sst)
+  }
+  
+}
