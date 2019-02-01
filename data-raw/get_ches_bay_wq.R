016439b61a983e1e32d9c742ae7c80ed4f5818fd
seanhardison1 <seanhardison@gmail.com>
2019-02-01 13:42:44 -0500

    Chesapeake bay WQ attainment indicators

---
+++ data-raw/get_ches_bay_wq.R
@@ -0,0 +1,21 @@
+#Chesapeake bay water quality attainment indicator
+
+library(dplyr)
+library(tidyr)
+
+
+raw.dir <- here::here("inst","extdata")
+
+get_ches_bay_wq <- function(save_clean =F){
+  ches_bay_wq <- read.csv(file.path(raw.dir, "Attainment_indicator.csv")) %>% 
+    dplyr::select(Time = Year.1, Value = Total) %>% 
+    mutate(Var = "chesapeake bay water quality attainment",
+           Units = "estimated attainment, percent",
+           EPU = "MAB")
+  
+  if (save_clean){
+    usethis::use_data(ches_bay_wq, overwrite = T)
+  } else {
+    return(ches_bay_wq)
+  }
+}
