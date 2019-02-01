7acf7b2d7d38cc0cc634c70b6181fa74eca87987
seanhardison1 <seanhardison@gmail.com>
2019-02-01 14:10:51 -0500

    Mass inshore survey data

---
+++ data-raw/get_mass_inshore_survey.R
@@ -0,0 +1,23 @@
+#MA state survey data
+
+library(dplyr)
+library(tidyr)
+
+raw.dir <- here::here("inst","extdata")
+
+get_mass_survey <- function(save_clean){
+  
+  load(file.path(raw.dir,"Aggregate_Mass_State_Survey_biomass_19.Rdata")) 
+  
+  mass_inshore_survey <- mass.survey %>% 
+    dplyr::select(-Source) %>% 
+    dplyr::rename(EPU = Region)
+  
+  if (save_clean){
+    usethis::use_data(mass_inshore_survey, overwrite = T)
+  } else {
+    return(mass_inshore_survey)
+  }
+  
+
+}
