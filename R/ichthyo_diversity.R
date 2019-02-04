cf5c1c2e4b1e91b33565befc0a0ef88b6ff40cb6
seanhardison1 <seanhardison@gmail.com>
2019-02-04 16:36:13 -0500

    Added ichthyoplankton diversity

---
+++ R/ichthyo_diversity.R
@@ -0,0 +1,26 @@
+#' Ichthyoplankton diversity
+#'
+#' Data from NOAA NEFSC Oceans and Climate branch EcoMon public dataset were used to examine changes in
+#' diversity of abundance among 45 ichthyoplankton taxa. The 45 taxa were established in Walsh et al. 2015,
+#' and include the most abundant taxa from the 1970s to present that represent consistency in the 
+#' identification of larvae. Both species counts and estimates of Shannon-Wiener diversity are included
+#' in this data set.
+#'
+#' @format
+#'
+#' \itemize{
+#'     \item Var: Specifies variable of interest and sampling season.
+#'     \item Time: Sampling year.
+#'     \item Units: Units of variable \code{Var}.
+#'     \item EPU: Ecological Production Units (EPU) where sampling occurred. \code{All} refers to data aggregated to the level of the Northeast Continental Shelf.
+#'     \item Value: Value of variable \code{Var}. 
+#' }
+#'
+#' @details 
+#' \code{NaN} values were included in the data where there were too few sampling stations to be included in diversity calculations. 
+#' More information about these data are available at \url{https://noaa-edab.github.io/tech-memo/ichthyoplankton-diversity.html}.
+#'
+#' @references
+#' Walsh, H. J., Richardson, D. E., Marancik, K. E., & Hare, J. A. (2015). Long-term changes in the distributions of larval and adult fish in the northeast US shelf ecosystem. \emph{PloS one}, 10(9), e0137382.
+#'
+"ichthyo_diversity"
---
+++ data-raw/get_ichthyoplankton.R
@@ -0,0 +1,44 @@
+#Ichthyoplankton species counts, diversity, and abundance
+
+library(dplyr)
+library(tidyr)
+library(readxl)
+library(stringr)
+
+raw.dir <- here::here("inst","extdata")
+
+ichthyo_spec_counts <- read_excel(file.path(raw.dir,"NEFSCIchthyoplanktonSpeciesCount_v3_3.xlsx")) %>% 
+  dplyr::select(-Source) %>% 
+  dplyr::rename(Time = Year,
+                EPU = Region) %>% 
+  mutate(Var = paste(Season,str_replace_all(Var, "_", " ")),
+         EPU = ifelse(EPU == "all", "All", 
+                      EPU)) %>% 
+  dplyr::select(-Season) %>% 
+  mutate(Value  = as.numeric(Value))
+
+ichthyo_diversity <- read_excel(file.path(raw.dir,"NEFSCIchthyoplanktonDiversity_v3_3.xlsx")) %>% 
+  dplyr::select(-Source) %>% 
+  dplyr::rename(Time = Year,
+                EPU = Region) %>% 
+  mutate(Var = paste(Season,str_replace_all(Var, "_", " ")),
+         EPU = ifelse(EPU == "all", "All", 
+                      EPU)) %>% 
+  dplyr::select(-Season) %>% 
+  mutate(Value  = as.numeric(Value)) %>% 
+  rbind(.,ichthyo_spec_counts)
+
+usethis::use_data(ichthyo_diversity, overwrite = T)
+
+# ichthyo_abundance <- read_excel(file.path(raw.dir,"NEFSCIchthyoplanktonAbundance_v3_3.xlsx")) %>% 
+#   dplyr::select(-Source) %>% 
+#   dplyr::rename(Time = Year,
+#                 EPU = Region) %>% 
+#   mutate(Var = paste(Season,str_replace_all(Var, "_", " ")),
+#          EPU = ifelse(EPU == "all", "All", 
+#                       EPU)) %>% 
+#   tidyr::gather(.,Var,Value,-Time,-Season,-Var,-Units,-EPU) %>% 
+#   unite(.,Var, c("Season","Var"), sep = " ") %>% 
+#   mutate(Value  = as.numeric(Value))
+# 
+# usethis::use_data(ichthyo_abundance, overwrite = T)
