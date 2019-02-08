35d1c3b972933c1494705d07242e48e2891bad71
seanhardison1 <seanhardison@gmail.com>
2019-02-07 20:48:33 -0500

    Updated NAO to Hurrell NAO index

--- data-raw/get_nao.R
+++ data-raw/get_nao.R
@@ -1,10 +1,5 @@
 # Process North Atlantic Oscillation time series
 
-# North Atlantic Oscillation data were taken from the NOAA NWS Climate Prediction Center.
-# These data show the monthly NAO index time series beginning in 1950 and ending in November 2018.
-# The index is standardized by the standard deviation of the 1950-2000 reference period. More
-# information regarding the methodology involved in deriving the NAO and its significance is
-# available at http://www.cpc.ncep.noaa.gov/data/teledoc/nao.shtml.
 
 library(dplyr)
 library(tidyr)
@@ -17,17 +12,12 @@ raw.dir <- here::here("inst","extdata") #input raw
 
 get_nao <- function(save_clean = F){
   
-  nao <- read.table(file.path(raw.dir, "monthly_nao.txt")) %>% 
-    dplyr::rename(Year = V1, Month = V2, Value = V3) %>% 
-    # mutate(Quarter = plyr::mapvalues(Month, from = c(1:12),
-    #                                  to = rep(c("Q1","Q2","Q3","Q4"), each = 3))) %>% 
-    # group_by(Year, Quarter) %>% 
-    # dplyr::summarise(Value = mean(Value))
-    mutate(Units = "unitless", EPU = "All") %>% 
-    #unite(.,"Time",c("Time","Month"), sep = "-") %>% 
-    #mutate(Time = as.Date(as.yearmon(Time, "%Y-%m")),
-    #       Var = "north atlantic oscillation") %>%
-    mutate(Var = "north atlantic oscillation") %>% 
+  nao <- read.csv(file.path(raw.dir, "nao_station_djfm.csv")) %>% 
+    dplyr::rename(Time = Hurrell.Station.Based.DJFM.NAO.Index,
+                  Value = X) %>% 
+    mutate(Var = "north atlantic oscillation",
+           Units = "unitless",
+           EPU = "All") %>% 
     as.data.frame()
 
   if (save_clean){
