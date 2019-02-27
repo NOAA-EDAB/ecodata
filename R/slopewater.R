4adfe66646d846b88c5262ad75fa508dee857717
seanhardison1 <seanhardison@gmail.com>
2019-02-27 13:39:55 -0500

    slopewater data documentation

---
+++ R/slopewater.R
@@ -0,0 +1,18 @@
+#' Northeast Channel slopewater proportions
+#' 
+#' Percent total of water type observed in the deep Northeast Channel (150-200 m water depth).
+#' 
+#' @format 74 rows and 5 columns
+#' 
+#' \itemize{
+#'     \item Var: Specifies slopewater source (either Labrador Slope Water (LSLW) or Warm Slope Water (WSW))
+#'     \item Value: Proportion of slopewater derived from distinct source.
+#'     \item Units: Units of variable \code{Var}; proportion.
+#'     \item Time: Year.
+#'     \item EPU: Ecological Production Unit where data originated. 
+#' }
+#' 
+#' @details 
+#' Read more about the roles of different slopewater types in the Gulf of Maine 
+#' at \url{https://www.integratedecosystemassessment.noaa.gov/regions/northeast/components/other-physical-drivers}.
+"slopewater"
