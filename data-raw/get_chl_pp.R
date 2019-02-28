5c7bd8efb9f408afaf046199ef4d74f3a9814abc
seanhardison1 <seanhardison@gmail.com>
2019-02-28 12:53:13 -0500

    Update CHL/PP. Export ne_wind

--- data-raw/get_chl_pp.R
+++ data-raw/get_chl_pp.R
@@ -7,7 +7,7 @@ library(stringr)
 raw.dir <- here::here("inst","extdata")
 
 
-ppd <- read.csv(file.path(raw.dir,"SOE_V2019_1-NES_ECOREGIONS-PPD-STATS_ANOMS-SEAWIFS_MODIS.csv")) %>% 
+ppd <- read.csv(file.path(raw.dir,"SOE_V2019_2-NES_ECOREGIONS-PPD-STATS_ANOMS-SEAWIFS_MODIS.csv")) %>% 
   mutate(ALGORITHM = word(str_replace(ALGORITHM, "_", " "))) %>%
   unite(.,VARIABLE, c("VARIABLE","SENSOR","ALGORITHM"), sep = " ") %>%
   mutate(VARIABLE = ifelse(str_detect(FILENAME, "1998_2018"), paste(VARIABLE,"1998_2018"),
@@ -19,7 +19,7 @@ ppd <- read.csv(file.path(raw.dir,"SOE_V2019_1-NES_ECOREGIONS-PPD-STATS_ANOMS-SE
   dplyr::rename(Time = TIME, Units = UNITS, Var = VARIABLE,
                 EPU = REGION, Value = VALUE)
 
-chl <- read.csv(file.path(raw.dir,"SOE_V2019_1-NES_ECOREGIONS-CHLOR_A-STATS_ANOMS-SEAWIFS_MODIS.csv")) %>% 
+chl <- read.csv(file.path(raw.dir,"SOE_V2019_2-NES_ECOREGIONS-CHLOR_A-STATS_ANOMS-SEAWIFS_MODIS.csv")) %>% 
   mutate(ALGORITHM = word(str_replace(ALGORITHM, "_", " "))) %>%
   unite(.,VARIABLE, c("VARIABLE","SENSOR","ALGORITHM"), sep = " ") %>%
   mutate(VARIABLE = ifelse(str_detect(FILENAME, "1998_2018"), paste(VARIABLE,"1998_2018"),
