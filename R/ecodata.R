83aa3950d6940efb932e22b91a1d670729b4811c
Andrew Beet <andrew.beet>
2019-03-20 11:24:58 -0400

    added package.r documentation

---
+++ R/ecodata.R
@@ -0,0 +1,16 @@
+#' ecodata: indicator data for reporting ecosystem health
+#'
+#'ecodata is developed by the Ecosystems Dynamics and Assessment Branch of the Northeast Fisheries Science Center for use in State of the Ecosystem (SOE) reporting.  SOE reports are high-level overviews of ecosystem indicator status and trends occurring on the Northeast Continental Shelf. Unless otherwise stated, data are representative of specific Ecological Production Units (EPUs), referring to the Mid-Atlantic Bight (MAB), Georges Bank (GB), Gulf of Maine (GOM), and Scotian Shelf (SS). SOE reports are developed for US Fishery Management Councils (FMCs), and therefore indicator data for Scotian Shelf  are included when available, but this is not always the case.
+#'
+#'\itemize{
+#'The main goals are:
+#'\item To create a centralized repository for indicator data
+#'\item To facilitate creation of annual reports
+#'}
+#'
+#'To learn more about using \code{ecodata}, start with the vignette: \code{browseVignettes(package="ecodata")} or click the index link below
+#'
+#'
+#' @docType package
+#' @name ecodata
+NULL
