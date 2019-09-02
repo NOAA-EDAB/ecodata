a18f8b574ef3a58594e9f955ba4ba8e6b1000d6a
seanhardison1 <seanhardison@gmail.com>
2019-09-02 19:39:36 -0400

    Fixed wording on bottom temperature data

--- data-raw/get_oceantemp_insitu.R
+++ data-raw/get_oceantemp_insitu.R
@@ -1,23 +1,23 @@
-# Process ocean temperature anomaly data
+# Process derived ocean temperature anomaly data
 
 # These data include in situ regional time series of both surface and bottom water temperature anomalies
-# on the Northeast Continental Shelf. Raw data is split into four files by EPU (SS, GOM, GB, and MAB).
+# on the Northeast Continental Shelf. Derived data is split into four files by EPU (SS, GOM, GB, and MAB).
 
 
 library(dplyr)
 library(tidyr)
 library(lubridate)
 
-#Get raw
-raw.dir <- here::here("data-raw") #input raw
+#Get derived data
+data.dir <- here::here("data-raw") #input data
 
 get_oceantemp_insitu <- function(save_clean = F){
 
-  ss <- read.csv(file.path(raw.dir,"EcoSS_core_Ttopbot.csv")) %>% mutate(EPU = "SS")
-  gom <- read.csv(file.path(raw.dir,"EcoGoM_core_Ttopbot.csv")) %>% mutate(EPU = "GOM")
-  gb <- read.csv(file.path(raw.dir,"EcoGB_core_Ttopbot.csv")) %>% mutate(EPU = "GB")
-  mab <- read.csv(file.path(raw.dir,"EcoMAB_core_Ttopbot.csv")) %>% mutate(EPU = "MAB")
-  
+  ss <- read.csv(file.path(data.dir,"EcoSS_core_Ttopbot.csv")) %>% mutate(EPU = "SS")
+  gom <- read.csv(file.path(data.dir,"EcoGoM_core_Ttopbot.csv")) %>% mutate(EPU = "GOM")
+  gb <- read.csv(file.path(data.dir,"EcoGB_core_Ttopbot.csv")) %>% mutate(EPU = "GB")
+  mab <- read.csv(file.path(data.dir,"EcoMAB_core_Ttopbot.csv")) %>% mutate(EPU = "MAB")
+
   oceantemp_insitu <- rbind(ss, gom, gb, mab) %>% #bind all
     dplyr::rename(Time = decimal.year, Var = variable.name, Value = temperature) %>% #rename
     mutate(Units = "degreesC", Time = as.Date(format(date_decimal(Time), "%Y-%b-%d"), "%Y-%b-%d"),
@@ -28,11 +28,11 @@ get_oceantemp_insitu <- function(save_clean = F){
                                       to = c("sst anomaly in situ",
                                              "reference sst in situ (1981-2010)",
                                              "bottom temp anomaly in situ",
-                                             "reference bt in situ (1981-2010)"))) %>% 
+                                             "reference bt in situ (1981-2010)"))) %>%
     group_by(Time = year(Time), EPU, Var, Units) %>%
     dplyr::summarise(Value = mean(Value)) %>%
-    as.data.frame() 
-  
+    as.data.frame()
+
   if (save_clean){
     usethis::use_data(oceantemp_insitu, overwrite = T)
   } else {
