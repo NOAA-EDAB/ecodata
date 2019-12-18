7af6c6a0bf7958ed1a48faef205e4735d3c64ec8
andybeet <22455149+andybeet@users.noreply.github.com>
2019-12-18 13:03:35 -0500

    included carriage return at end of script. - tech doc may need this otherwise throws a warning

--- data-raw/get_long_term_sst.R
+++ data-raw/get_long_term_sst.R
@@ -6,17 +6,17 @@ library(tidyr)
 raw.dir <- here::here("data-raw")
 
 get_long_term_sst <- function(save_clean = F){
-  long_term_sst <- read.csv(file.path(raw.dir,"ersst annual mean.csv")) %>% 
+  long_term_sst <- read.csv(file.path(raw.dir,"ersst annual mean.csv")) %>%
     dplyr::rename(Time = Year,
-                  Value = Mean) %>% 
+                  Value = Mean) %>%
     mutate(Var = "long-term sst",
            EPU = "All",
            Units = "degreesC")
-  
+
   if (save_clean) {
     usethis::use_data(long_term_sst, overwrite = T)
   } else {
     return(long_term_sst)
   }
-  
+
 }
