b96bc16875877cefb4308698f05d80bc2c521c08
Kimberly Bastille <39955661+kimberly-bastille@users.noreply.github.com>
2019-12-19 08:32:38 -0500

    add harbor porpoise and fix spelling error

--- data-raw/get_habitat_occupancy.R
+++ data-raw/get_habitat_occupancy.R
@@ -7,21 +7,21 @@ library(tidyr)
 raw.dir <- here::here("data-raw")
 
 
-get_hab_occupency <- function(save_clean = F){
+get_hab_occupancy <- function(save_clean = F){
 
-  d <- read.csv(file.path(raw.dir,"hab_occupency.csv"))
+  d <- read.csv(file.path(raw.dir,"hab_occupancy.csv"))
 
   #Process
-  habitat_occupency <- d %>%
+  habitat_occupancy <- d %>%
     rename("Time" = "Year") %>%
     tidyr::gather(key  = "Var", value = "Value", -Time) %>%
     mutate(EPU = c("All"),
            Units = c("10^3km^2"))
 
   if (save_clean){
-    usethis::use_data(habitat_occupency, overwrite = T)
+    usethis::use_data(habitat_occupancy, overwrite = T)
   } else {
-    return(habitat_occupency)
+    return(habitat_occupancy)
   }
 }
-get_hab_occupency(save_clean = T)
+get_hab_occupancy(save_clean = T)
---
+++ data-raw/get_harborporpoise.R
@@ -0,0 +1,38 @@
+# Process harbor porpoise bycatch estimates
+
+# Time series figure is 5-yr running mean for harbor porpoise bycatch estimates for the Northeast US across all fisheries.
+
+library(dplyr)
+library(tidyr)
+
+raw.dir <- here::here("data-raw")
+
+#HP bycatch time series estimates------------------------------------------------------
+get_harborporpoise <- function(save_clean = F){
+  d <- read.csv(file.path(raw.dir,"1994-2017_5yr_hp_est.csv"))
+
+  #Create confidence intervals
+  var1nnum <- log(1+d$CV^2)
+  c <- exp(1.96 * sqrt(var1nnum))
+  d$up95ci <- d$EST * c
+  d$lo95ci <- d$EST / c
+
+
+  harborporpoise <- d %>% dplyr::rename(Time = YEAR) %>%
+    gather(., Var, Value, -Time) %>%
+    mutate(Units = "N",
+           EPU = "All",
+           Var, Var = plyr::mapvalues(Var, from = c("EST","CV","PBR","up95ci","lo95ci"),
+                                      to = c("harbor porpoise bycatch estimate",
+                                             "harbor porpoise bycatch cv",
+                                             "harbor porpoise bycatch pbr",
+                                             "harbor porpoise bycatch up95ci",
+                                             "harbor porpoise bycatch lo95ci"))) %>%
+    as.data.frame()
+
+  if (save_clean){
+    usethis::use_data(harborporpoise, overwrite = R)
+  } else {
+    return(harborporpoise)
+  }
+}
