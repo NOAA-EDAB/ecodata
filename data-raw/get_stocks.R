ca7c024c92b33e4e2bd0e8bf8904d264ee1ff4ce
Kimberly Bastille <39955661+kimberly-bastille@users.noreply.github.com>
2020-01-09 17:18:45 -0500

    tech doc PDF margin overrun

--- data-raw/get_stocks.R
+++ data-raw/get_stocks.R
@@ -15,12 +15,15 @@ get_stocks <- function(save_clean = F){
   stock_status <-
     assess %>%
     group_by(Entity.Name) %>%
-    filter(Assessment.Year == max(Assessment.Year)) %>% #Find last year assessment occurred for each stock
+    filter(Assessment.Year == max(Assessment.Year)) %>% 
+  #Find last year assessment occurred for each stock
     ungroup() %>%
     left_join(.,decode, by = "Entity.Name") %>% #Join in list of managed species
-    dplyr::select(Entity.Name, Assessment.Year, F.Fmsy, B.Bmsy, Council, Code) %>% #select column variables to keep
+    dplyr::select(Entity.Name, Assessment.Year, F.Fmsy, B.Bmsy, Council, Code) %>% 
+  #select column variables to keep
     mutate(id = 1:length(Entity.Name)) %>%
-    gather(.,Var, Value,-id,-Entity.Name,-Assessment.Year,-Council,-Code) %>% #wide to long
+    gather(.,Var, Value,-id,-Entity.Name,-Assessment.Year,-Council,-Code) %>%
+  #wide to long
     dplyr::select(-id) %>%
     dplyr::rename(`Last assessment` = Assessment.Year,
                   Stock = Entity.Name) %>% #rename variables for clarity
