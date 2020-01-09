fd6185855b79b89e948e4fd9553392061ea3917d
Kimberly Bastille <39955661+kimberly-bastille@users.noreply.github.com>
2020-01-09 13:06:43 -0500

    character return

--- data-raw/get_ches_bay_wq.R
+++ data-raw/get_ches_bay_wq.R
@@ -8,8 +8,8 @@ raw.dir <- here::here("data-raw")
 
 get_ches_bay_wq <- function(save_clean =F){
   ches_bay_wq <- read.csv(file.path(raw.dir, "Attainment_indicator.csv")) %>% 
-    dplyr::select(Time = Year.1, Value = Total) %>% 
-    mutate(Var = "chesapeake bay water quality attainment",
+  dplyr::select(Time = Year.1, Value = Total) %>% 
+  mutate(Var = "chesapeake bay water quality attainment",
            Units = "estimated attainment, percent",
            EPU = "MAB")
   
