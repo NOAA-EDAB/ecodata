013a1a1300ffc300615530bf831a73bf07951a8a
Kimberly Bastille <39955661+kimberly-bastille@users.noreply.github.com>
2020-01-22 11:30:52 -0500

    update HD for synthesis meeting

--- data-raw/get_comdat.R
+++ data-raw/get_comdat.R
@@ -1,8 +1,8 @@
 # Process commerical data
 
 
-# These data were derived from the Commercial Fisheries Database Biological Sample. 
-# More information about these data are available at 
+# These data were derived from the Commercial Fisheries Database Biological Sample.
+# More information about these data are available at
 # https://noaa-edab.github.io/tech-doc/comdat.html
 
 library(dplyr)
@@ -12,7 +12,7 @@ raw.dir <- here::here("data-raw")
 
 get_comdat <- function(save_clean = F){
 
-  load(file.path(raw.dir, "Commercial_data_pull.RData"))
+  load(file.path(raw.dir, "Commerical_data_pull_20.RData"))
 
   comdat <- commercial %>%
     dplyr::rename(EPU = Region) %>%
