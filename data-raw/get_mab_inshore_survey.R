2538a2ed8a175a7526535315e2e7c027df770032
Kimberly Bastille <39955661+kimberly-bastille@users.noreply.github.com>
2020-01-31 12:54:48 -0500

    survey sd, add ppr data and plot, fix stock status labelling

--- data-raw/get_mab_inshore_survey.R
+++ data-raw/get_mab_inshore_survey.R
@@ -8,16 +8,14 @@ library(tidyr)
 raw.dir <- here::here("data-raw")
 
 get_mab_inshore_survey <- function(save_clean = F){
+
   mab_inshore_survey <- read_excel(file.path(raw.dir,
                                              "NEAMAP_SOE INDICES_2019_final.xlsx")) %>%
-     gather(.,Var,Value,-Year,-Season,-Category) %>%
-     unite(., Var, c("Season","Category","Var"), sep = " ") %>%
-     dplyr::rename(Time = Year) %>%
-     mutate(Var = tolower(Var),
-            Units = ifelse(str_detect(Var,"index"),
-                           "kg tow^-1","CV"),
-            EPU = "MAB")
-
+    unite(Var, "Category","Season", sep = " ") %>%
+    dplyr::rename(Time = Year,
+                  Value = Index) %>%
+    mutate(Units = c("kg tow^-1"),
+           EPU = "MAB")
   if (save_clean){
     usethis::use_data(mab_inshore_survey, overwrite = T)
   } else {
