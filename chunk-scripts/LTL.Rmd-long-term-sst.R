1c8dd823dde1b4850e35013d662c4a60830ebf11
andybeet <22455149+andybeet@users.noreply.github.com>
2020-02-19 10:05:07 -0500

    removed ? from text

--- chunk-scripts/LTL.Rmd-long-term-sst.R
+++ chunk-scripts/LTL.Rmd-long-term-sst.R
@@ -1,10 +1,10 @@
 
-lt_sst <- ecodata::long_term_sst %>% 
+lt_sst <- ecodata::long_term_sst %>%
   mutate(hline = mean(Value, na.rm = TRUE))
 
 hline <- mean(lt_sst$Value)
 
-lt_sst %>% 
+lt_sst %>%
   ggplot(aes(x = Time, y = Value, group = Var)) +
   annotate("rect", fill = shade.fill, alpha = shade.alpha,
       xmin = x.shade.min , xmax = x.shade.max,
@@ -16,7 +16,7 @@ lt_sst %>%
              size = hline.size,
              alpha = hline.alpha,
            linetype = hline.lty)+
-  ylab("Temperature (°C)") +
+  ylab("Temperature (C)") +
   xlab(element_blank())+
   ggtitle("Long-term SST") +
   scale_x_continuous(expand = c(0.01, 0.01), breaks = seq(1840,2010,10))+
