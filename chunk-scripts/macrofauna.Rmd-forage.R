b0054b3bd722de7e39a47061fbb47958096b3d77
Kimberly Bastille <39955661+kimberly-bastille@users.noreply.github.com>
2020-02-21 12:19:50 -0500

    col heads in forage table

--- chunk-scripts/macrofauna.Rmd-forage.R
+++ chunk-scripts/macrofauna.Rmd-forage.R
@@ -1,18 +1,9 @@
 
-d<-ecodata::energy_density %>% 
-    dplyr::rename("Mean ED (SD)" = "Mean.ED..SD.", 
-           "N" = "n",
-           "Mean ED (SD)" = "Mean.ED..SD..1", 
-           "N" = "n.1", 
-           "Mean ED (SD)" = "Mean.ED..SD..2", 
-           "N" = "n.2", 
-           "Mean ED (SD)" = "Mean.ED..SD..3", 
-           "N" = "n.3",
-           "Mean ED (SD)" = "Mean.ED..SD..4", 
-           "N" = "X",
-           "Mean ED (SD)" = "Mean.ED..SD..5", 
-           "Mean ED" = "Mean.ED" )
-kable(d, caption = "Forage fish energy content") %>% 
-  kable_styling(bootstrap_options = "striped", font_size = 9) %>% 
+d<-ecodata::energy_density
+
+colNames <- c("Species",rep(c("Mean ED (SD)","N"),5),"Mean ED", "Mean ED (SD)")
+
+kable(d, caption = "Forage fish energy content",col.names = colNames) %>%
+  kable_styling(bootstrap_options = "striped", font_size = 9) %>%
   add_header_above(c(" " = 1, "Spring" = 2, "Fall" = 2, "Spring" = 2, "Fall" = 2, " " = 4)) %>% 
   add_header_above(c(" " = 1, "2017" = 4, "2018" = 4, "Total" = 2, "Steimle and Terranove (1985)" = 1, "Lawson et al. (1998)" = 1))
