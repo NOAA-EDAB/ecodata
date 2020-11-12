9bf3e5ba36aeb664d8bed9bb37faf58d7cf7fc58
kimberly-bastille <kimberly.bastille@gmail.com>
2020-11-12 15:21:07 -0500

    update cold pool

--- data-raw/get_cold_pool.R
+++ data-raw/get_cold_pool.R
@@ -1,19 +1,30 @@
-### Cold pool index from Chris Melrose
+### Cold pool index from Zhuomin Chen
 
 library(dplyr)
 library(tidyr)
+library(tidync)
+
 
 
 raw.dir <- here::here("data-raw")
 
-cold_pool_csv<- "cold_pool_index.csv"
+cold_pool_nc<- "Glorys12v1_ColdPool_Extents.nc"
 
 get_cold_pool <- function(save_clean = F){
 
-  cold_pool <- read.csv(file.path(raw.dir, cold_pool_csv)) %>%
-    dplyr::rename(EPU = Region,
-                  Time = Year,
-                  Value = VAR)
+
+  cold_pool_file <- list.files(raw.dir, pattern=cold_pool_nc,full.names=TRUE)
+
+
+  cold_pool <- tidync::tidync(cold_pool_file) %>%
+    tidync::hyper_tibble() %>%
+    dplyr::mutate(Time = c(ny+1992)) %>%
+    tidyr::pivot_longer(cols = c("T_peak", "T_min", "T_mean", "V_max"), names_to = "Var",values_to = "Value") %>%
+    dplyr::group_by(Time, Var) %>%
+    dplyr::summarise(Val = mean(Value),
+                     Uncertainty = sd(Value)) %>%
+    dplyr::mutate(EPU = c("MAB"))
+
 
   if(save_clean){
     usethis::use_data(cold_pool, overwrite = T)
@@ -23,9 +34,34 @@ get_cold_pool <- function(save_clean = F){
   # metadata ----
   attr(cold_pool, "tech-doc_url") <- "https://noaa-edab.github.io/tech-doc/cold-pool-index.html"
   attr(cold_pool, "data_files")   <- list(
-    cold_pool_csv = cold_pool_csv)
+    cold_pool_nc = cold_pool_nc)
   attr(cold_pool, "data_steward") <- c(
-    "Chris Melrose <chris.melrose@noaa.gov>")
+    "Zhuomin Chen <zchen@whoi.edu>")
 
 }
 get_cold_pool(save_clean = T)
+
+### Cold pool index from Chris Melrose
+# cold_pool_csv<- "cold_pool_index.csv"
+#
+# get_cold_pool <- function(save_clean = F){
+#
+#   cold_pool <- read.csv(file.path(raw.dir, cold_pool_csv)) %>%
+#     dplyr::rename(EPU = Region,
+#                   Time = Year,
+#                   Value = VAR)
+#
+#   if(save_clean){
+#     usethis::use_data(cold_pool, overwrite = T)
+#   } else {
+#     return(cold_pool)
+#   }
+#   # metadata ----
+#   attr(cold_pool, "tech-doc_url") <- "https://noaa-edab.github.io/tech-doc/cold-pool-index.html"
+#   attr(cold_pool, "data_files")   <- list(
+#     cold_pool_csv = cold_pool_csv)
+#   attr(cold_pool, "data_steward") <- c(
+#     "Chris Melrose <chris.melrose@noaa.gov>")
+#
+# }
+# get_cold_pool(save_clean = T)
