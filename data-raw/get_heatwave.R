0aa2a8c3cecacea562bbc4b3a8867eed934123cf
kimberly-bastille <kimberly.bastille@gmail.com>
2020-12-15 14:50:22 -0500

    update ltl sst. heatwave and calanus stage

--- data-raw/get_heatwave.R
+++ data-raw/get_heatwave.R
@@ -7,9 +7,9 @@ library(readr)
 
 raw.dir <- here::here("data-raw")
 
-heatwave_gb_csv<-"GB_OISST - Vincent Saba - NOAA Federal.csv"
-heatwave_gom_csv<-"GOM_OISST - Vincent Saba - NOAA Federal.csv"
-heatwave_mab_csv<-"MAB_OISST - Vincent Saba - NOAA Federal.csv"
+heatwave_gb_csv<-"GB_OISST.csv"
+heatwave_gom_csv<-"GOM_OISSt.csv"
+heatwave_mab_csv<-"MAB_OISST.csv"
 
 get_heatwave <- function(save_clean = F){
 
@@ -20,19 +20,19 @@ get_heatwave <- function(save_clean = F){
   mab<-read_csv(file.path(raw.dir,heatwave_mab_csv),
                 col_types = cols(temp = col_double(),t = col_date()))
   #GB
-  ts <- heatwaveR::ts2clm(gb, climatologyPeriod = c("1982-01-01", "2010-12-31"))
+  ts <- heatwaveR::ts2clm(gb, climatologyPeriod = c("1982-01-01", "2011-12-31"))
   gb.mhw <- heatwaveR::detect_event(ts)
   gb.hw<- gb.mhw$event %>%
     dplyr::select(event_no, duration, date_start, date_peak, intensity_max, intensity_cumulative)%>%
     dplyr::mutate(EPU = "GB")
   #GOM
-  ts <- heatwaveR::ts2clm(gom, climatologyPeriod = c("1982-01-01", "2010-12-31"))
+  ts <- heatwaveR::ts2clm(gom, climatologyPeriod = c("1982-01-01", "2011-12-31"))
   gom.mhw <- heatwaveR::detect_event(ts)
   gom.hw<- gom.mhw$event %>%
     dplyr::select(event_no, duration, date_start, date_peak, intensity_max, intensity_cumulative) %>%
     dplyr::mutate(EPU = "GOM")
   # MAB
-  ts <- heatwaveR::ts2clm(mab, climatologyPeriod = c("1982-01-01", "2010-12-31"))
+  ts <- heatwaveR::ts2clm(mab, climatologyPeriod = c("1982-01-01", "2011-12-31"))
   mab.mhw <- heatwaveR::detect_event(ts)
   mab.hw<- mab.mhw$event %>%
     dplyr::select(event_no, duration, date_start, date_peak, intensity_max, intensity_cumulative) %>%
@@ -82,35 +82,35 @@ get_heatwave(save_clean = T)
 #### get_heatwave_year get single year of heatwave
 get_heatwave_year <- function(save_clean = F){
   # import data
-  gom<-read_csv(file.path(raw.dir,"GOM_OISST - Vincent Saba - NOAA Federal.csv"),
+  gom<-read_csv(file.path(raw.dir,"GOM_OISST.csv"),
                 col_types = cols(temp = col_double(),t = col_date()))
-  gb<-read_csv(file.path(raw.dir,"GB_OISST - Vincent Saba - NOAA Federal.csv"),
+  gb<-read_csv(file.path(raw.dir,"GB_OISST.csv"),
                col_types = cols(temp = col_double(),t = col_date()))
-  mab<-read_csv(file.path(raw.dir,"MAB_OISST - Vincent Saba - NOAA Federal.csv"),
+  mab<-read_csv(file.path(raw.dir,"MAB_OISST.csv"),
                 col_types = cols(temp = col_double(),t = col_date()))
   #GB
-  ts <- heatwaveR::ts2clm(gb, climatologyPeriod = c("1982-01-01", "2010-12-31"))
+  ts <- heatwaveR::ts2clm(gb, climatologyPeriod = c("1982-01-01", "2011-12-31"))
   gb.mhw <- heatwaveR::detect_event(ts)
   #GOM
-  ts <- heatwaveR::ts2clm(gom, climatologyPeriod = c("1982-01-01", "2010-12-31"))
+  ts <- heatwaveR::ts2clm(gom, climatologyPeriod = c("1982-01-01", "2011-12-31"))
   gom.mhw <- heatwaveR::detect_event(ts)
   #MAB
-  ts <- heatwaveR::ts2clm(mab, climatologyPeriod = c("1982-01-01", "2010-12-31"))
+  ts <- heatwaveR::ts2clm(mab, climatologyPeriod = c("1982-01-01", "2011-12-31"))
   mab.mhw <- heatwaveR::detect_event(ts)
 
   ### Take just clim
   #GB
   mhw<- gb.mhw$clim %>%
     mutate(EPU = c("GB"))# add EPU column
-  mhw.gb.year <- mhw[13515:13857,]## days in 2019 data set only went to dec 9, 2019
+  mhw.gb.year <- mhw[13880:14223,]## days in 2020 data set only went to dec 9, 2020
   #GOM
   mhw<- gom.mhw$clim %>%
     mutate(EPU = c("GOM"))# add EPU column
-  mhw.gom.year <- mhw[13515:13857,]## days in 2019 data set only went to dec 9, 2019
+  mhw.gom.year <- mhw[13880:14223,]## days in 2020 data set only went to dec 9, 2020
   #MAB
   mhw<- mab.mhw$clim %>%
     mutate(EPU = c("MAB"))# add EPU column
-  mhw.mab.year <- mhw[13515:13857,]## days in 2019 data set only went to dec 9, 2019
+  mhw.mab.year <- mhw[13880:14223,]## days in 2020 data set only went to dec 9, 2020
 #  mhw.mab.year2018 <- mhw[13150:13514,] Running 2018 for Kim H
   # bind dfs together for master list for plotting
   heatwave_year<- rbind(mhw.gb.year, mhw.gom.year, mhw.mab.year)
--- data-raw/get_seasonal_oisst_anom_gridded.R
+++ data-raw/get_seasonal_oisst_anom_gridded.R
@@ -13,7 +13,8 @@ rast_prep <- function(r){
   return(r)
 }
 
-raw.dir <- here::here("data-raw/gridded/sst_data")
+ltm.dir <- here::here("data-raw/gridded")
+raw.dir <- here::here("data-raw")
 crs <- "+proj=longlat +lat_1=35 +lat_2=45 +lat_0=40
 +lon_0=-77 +x_0=0 +y_0=0 +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"
 
@@ -23,7 +24,7 @@ seasonal_sst_anomaly_gridded_ltm_nc <- "sst.day.mean.ltm.1982-2010.nc"
 #here: https://www.esrl.noaa.gov/psd/data/gridded/data.noaa.oisst.v2.highres.html
 # but are removed after use as they are too large to store on github
 sst.2019 <- rast_prep(stack(file.path(raw.dir, seasonal_sst_anomaly_gridded_day_nc)))
-ltm <- rast_prep(stack(file.path(raw.dir, seasonal_sst_anomaly_gridded_ltm_nc)))
+ltm <- rast_prep(stack(file.path(ltm.dir, seasonal_sst_anomaly_gridded_ltm_nc)))
 
 winter.ltm <- ltm[[1:90]]
 spring.ltm <- ltm[[91:181]]
@@ -33,7 +34,7 @@ fall.ltm <- ltm[[274:365]]
 winter.anom <- sst.2019[[1:90]] - winter.ltm
 spring.anom <- sst.2019[[91:181]] - spring.ltm
 summer.anom <- sst.2019[[182:273]] - summer.ltm
-fall.anom <- sst.2019[[274:365]] - fall.ltm
+fall.anom <- sst.2019[[274:347]] - fall.ltm ### switched to cover just dates included in 2020.nc up to Dec 13th.
 
 
 rast_process <- function(r, season){
