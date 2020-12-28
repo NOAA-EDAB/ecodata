8ca69d895515a879c4d16e90005ef7e1b5194829
kimberly-bastille <kimberly.bastille@gmail.com>
2020-12-28 12:01:51 -0500

    aquaculture from chris s

--- data-raw/get_aquaculture.R
+++ data-raw/get_aquaculture.R
@@ -1,21 +1,20 @@
-#Processing code for oyster harvest data in the Mid-Atlantic
-
-#These data were collected directly from shellfish aquaculture surveys
-# performed by state agencies and universities.
-
-#NJ: http://njseagrant.org/new-jersey-shellfish-aquaculture-situation-outlook-report-new/
-#VA: http://www.vims.edu/research/units/centerspartners/map/aquaculture/docs_aqua/vims_mrr_2018-9.pdf
-#MD: Data from MD Aquaculture Coordinating Council meeting reports.
-# Newest reports available upon request
+#
 
 library(dplyr)
 library(tidyr)
-
+library(readxl)
 raw.dir <- here::here("data-raw")
-aquaculture_csv <- "mab_oyster_harvest.csv"
+aquaculture_xlsx <- "Aquaculture.edited.xlsx"
 
 get_aquaculture <- function(save_clean = F){
-  aquaculture <- read.csv(file.path(raw.dir,aquaculture_csv))
+  dat <- read_excel(file.path(raw.dir,aquaculture_xlsx))
+
+  aquaculture<- dat %>%
+    tidyr::pivot_longer(cols = c(Pieces,`Shellfish lease Acres`, `Production/Acre`),
+                        names_to = "Var", values_to = "Value") %>%
+    dplyr::rename(Time = Year,
+                  Region = State)
+
 
   if (save_clean){
     usethis::use_data(aquaculture, overwrite = T)
@@ -25,8 +24,8 @@ get_aquaculture <- function(save_clean = F){
   # metadata ----
   attr(aquaculture, "tech-doc_url") <- "https://noaa-edab.github.io/tech-doc/aquaculture.html"
   attr(aquaculture, "data_files")   <- list(
-    aquaculture_csv = aquaculture_csv)
+    aquaculture_xlsx = aquaculture_xlsx)
   attr(aquaculture, "data_steward") <- c(
-    "Kimberly Bastille <kimberly.bastille@noaa.gov>")
+    "Chris Schillaci <christopher.shillaci@noaa.gov>")
 }
 get_aquaculture(save_clean = T)
