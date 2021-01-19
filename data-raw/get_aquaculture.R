aa33b04f9d00b5741b2b8e2e9c14b9d127764443
kimberly-bastille <kimberly.bastille@gmail.com>
2021-01-19 13:39:34 -0500

    update aquaculture and grayseal legend

--- data-raw/get_aquaculture.R
+++ data-raw/get_aquaculture.R
@@ -4,18 +4,26 @@ library(dplyr)
 library(tidyr)
 library(readxl)
 raw.dir <- here::here("data-raw")
-aquaculture_xlsx <- "Aquaculture.edited.xlsx"
+aquaculture_xlsx <- "aqauculture2.xlsx"
 
 get_aquaculture <- function(save_clean = F){
   dat <- read_excel(file.path(raw.dir,aquaculture_xlsx))
 
-  aquaculture<- dat %>%
+  ne_aquaculture<- dat %>%
+    mutate(Pieces = as.numeric(Pieces)) %>%
     tidyr::pivot_longer(cols = c(Pieces,`Shellfish lease Acres`, `Production/Acre`),
                         names_to = "Var", values_to = "Value") %>%
     dplyr::rename(Time = Year,
                   Region = State)
+  dat2 <- read_excel(file.path(raw.dir,aquaculture_xlsx), sheet = "Mid Atlantic")
 
+  mab_aquaculture<- dat2  %>%
+    dplyr::rename(Time = Year,
+                  Region = State,
+                  Value = Pieces) %>%
+    dplyr::mutate(Var = c("Pieces"))
 
+  aquaculture <- rbind(ne_aquaculture, mab_aquaculture)
   if (save_clean){
     usethis::use_data(aquaculture, overwrite = T)
   } else {
