ce3c151b699d4dacff643a800d9dcaeff446f404
kimberly-bastille <kimberly.bastille@gmail.com>
2021-01-25 11:31:49 -0500

    add expected n

---
+++ data-raw/get_exp_n.R
@@ -0,0 +1,41 @@
+
+
+library(dplyr)
+library(stringr)
+library(tidyr)
+
+raw.dir <- here::here("data-raw")
+exp_n_rdata <- "Expected_number_of_species_21.RData"
+get_exp_n <- function(save_clean = F){
+
+  load(file.path(raw.dir, exp_n_rdata))
+
+
+
+  exp_n2<-ESn.epu %>% filter(str_detect(Var,  'ESn - Bigelow|ESn - Albatross')) %>%
+    tidyr::separate(Var, c("Season", "EPU", "trash1", "Var")) %>%
+    dplyr::select(-trash1)
+
+  exp_n<-ESn.epu %>% filter(str_detect(Var,  'ESn Standard Deviation')) %>%
+    tidyr::separate(Var, c("Season", "EPU", "trash1", "trash2", "trash3","Var")) %>%
+    dplyr::mutate(Var=recode(Var,
+                             `Albatross`="AlbatrossSD",
+                             `Bigelow`="BigelowSD")) %>%
+    dplyr::select(-trash1, -trash2, -trash3) %>%
+    rbind(exp_n2)
+
+
+
+  if (save_clean){
+    usethis::use_data(exp_n, overwrite = T)
+  } else {
+    return(exp_n)
+  }
+  # metadata ----
+  attr(exp_n, "tech-doc_url") <- "https://noaa-edab.github.io/tech-doc"
+  attr(exp_n, "data_files")   <- list(
+    exp_n_rdata = exp_n_rdata)
+  attr(exp_n, "data_steward") <- c(
+    "Sean Lucey <sean.lucey@noaa.gov>")
+}
+get_exp_n(save_clean = T)
