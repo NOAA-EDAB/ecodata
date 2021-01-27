cd1994d175f833f0279624cf4d27381fcac70f4a
kimberly-bastille <kimberly.bastille@gmail.com>
2021-01-27 11:27:44 -0500

    update code chunks with include_url

--- chunk-scripts/human_dimensions_MAB.Rmd-wind-dev-NE.R
+++ chunk-scripts/human_dimensions_MAB.Rmd-wind-dev-NE.R
@@ -1,2 +1,3 @@
 
-knitr::include_graphics(file.path(image.dir, "NE2021119.jpg"))
+knitr::include_url("https://raw.githubusercontent.com/NOAA-EDAB/ecodata/master/docs/images/NE2021119.jpg")
+#knitr::include_graphics(file.path(image.dir, "NE2021119.jpg"))
--- chunk-scripts/human_dimensions_MAB.Rmd-wind-dev-cumul.R
+++ chunk-scripts/human_dimensions_MAB.Rmd-wind-dev-cumul.R
@@ -1,2 +1,3 @@
 
-knitr::include_graphics(file.path(image.dir, "All_2021119-01.jpg"))
+knitr::include_url("https://raw.githubusercontent.com/NOAA-EDAB/ecodata/master/docs/images/All_2021119-01.jpg")
+#knitr::include_graphics(file.path(image.dir, "All_2021119-01.jpg"))
