f09334beaf90544f3a73d5dbb6810f3081fc4a79
kimberly-bastille <kimberly.bastille@gmail.com>
2021-02-11 12:33:04 -0500

    add uncertainty to ch bay temp figure

--- data-raw/get_ch_bay_temp.R
+++ data-raw/get_ch_bay_temp.R
@@ -11,8 +11,10 @@ get_ch_bay_temp <- function(save_clean = F){
   ch_bay_temp<-read_csv(file.path(raw.dir,ch_bay_temp_csv), col_names = FALSE) %>%
     janitor::row_to_names(1) %>%
     dplyr::rename("Year2020" = "2020 Daily",
-                  "YearLTA" = "2010-2019 avg") %>%
-    tidyr::pivot_longer(c("Year2020", "YearLTA"), names_to = "Var", values_to = "Value") %>%
+                  "YearLTA" = "2010-2019 avg",
+                  "minLTA" = "2010-2019 min",
+                  "maxLTA" = "2010-2019 max") %>%
+    tidyr::pivot_longer(c("Year2020", "YearLTA", "minLTA", "maxLTA"), names_to = "Var", values_to = "Value") %>%
     dplyr::mutate(Units = c("C"),
                   Value = as.numeric(Value))  %>%
     dplyr::rename(Time = Date) %>%
