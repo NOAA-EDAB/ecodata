a5f351f4f0b51f95e5e4ec00aae811017b550f4d
kimberly-bastille <kimberly.bastille@gmail.com>
2021-03-04 08:28:42 -0500

    update wind survey map

--- chunk-scripts/human_dimensions_MAB.Rmd-wind-dev-survey.R
+++ chunk-scripts/human_dimensions_MAB.Rmd-wind-dev-survey.R
@@ -1,3 +1,3 @@
 
-knitr::include_graphics(file.path(image.dir, "SurveyMap2021128.png"))
+knitr::include_graphics(file.path(image.dir, "SurveyMap202133.png"))
 #knitr::include_url("https://raw.githubusercontent.com/NOAA-EDAB/ecodata/master/docs/images/SurveysWindFarmAreas_20201229.jpg")
