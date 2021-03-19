d7f6f399f137892dea39bdbe210a4b23efa7a0a7
kimberly-bastille <kimberly.bastille@gmail.com>
2021-03-19 11:38:36 -0400

    add seal pup documentation

---
+++ R/seal_pups.R
@@ -0,0 +1,19 @@
+#' Seal Pups
+#'
+#' Data include timeseires of seal pup count on four colonies.
+#'
+#' @format Data set contains 77 rows and 6 columns
+#'
+#' \itemize{
+#'     \item Time: Year.
+#'     \item Count: Number of seal pups.
+#'     \item Colony: Colony name..
+#'     \item fitted: GLM modelled values.
+#'     \item upper: Upper CV.
+#'     \item lower: Lower CV.
+#' }
+#'
+#' @details
+#' More information including processing and indicator derivation steps are available at
+#' \url{https://noaa-edab.github.io/tech-doc.html}.
+"seal_pups"
