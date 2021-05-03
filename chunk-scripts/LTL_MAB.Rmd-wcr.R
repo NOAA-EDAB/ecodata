0a224fbaf1faadb669a32764f94703b4680ed686
kimberly-bastille <kimberly.bastille@gmail.com>
2021-05-03 16:03:31 -0400

    fix wcr figure

--- chunk-scripts/LTL_MAB.Rmd-wcr.R
+++ chunk-scripts/LTL_MAB.Rmd-wcr.R
@@ -10,11 +10,11 @@ wcr<- upper.line %>%
 
 wcr %>% 
   ggplot2::ggplot(aes(x = Time, y = Value))+
-  ggplot2::geom_point()+
-  ggplot2::geom_line()+
-  ggplot2::annotate("rect", fill = shade.fill, alpha = shade.alpha,
+    ggplot2::annotate("rect", fill = shade.fill, alpha = shade.alpha,
       xmin = x.shade.min , xmax = x.shade.max,
       ymin = -Inf, ymax = Inf) +
+  ggplot2::geom_point()+
+  ggplot2::geom_line()+
   ggplot2::ylab("Warm Core Ring Births")+
   ggplot2::xlab(element_blank())+
   ggplot2::ggtitle("Warm Core Rings")+
