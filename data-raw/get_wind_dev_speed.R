7dd73ece50023ec4225f63a91bb74a6339c43518
kimberly-bastille <kimberly.bastille@gmail.com>
2021-11-30 19:01:43 -0500

    Start wind development

--- data-raw/get_wind_dev_speed.R
+++ data-raw/get_wind_dev_speed.R
@@ -35,3 +35,48 @@ get_wind_dev_speed <- function(save_clean = F){
   }
 }
 get_wind_dev_speed(save_clean = T)
+
+
+
+
+
+
+
+
+
+
+
+raw.dir <- here::here("data-raw")
+wind_dev_speed_csv<- "Wind_Cumul_Develop_2021-22 - Angela Silva - NOAA Affiliate.csv"
+
+get_wind_dev_speed <- function(save_clean = F){
+  wind_dev_speed <- read.csv(file.path(raw.dir,wind_dev_speed_csv)) %>%
+    dplyr::filter(!Constr_Years == "Construction is estimated to be 2 years for each project.") %>%
+    tidyr::pivot_longer(cols = c(  "Cumul_Proj_Acres" , "Cumul_.FDNS" , "Cumul_Offsh_Cbl__Acres",
+                                   "Cumul_OffExp_Inter_Cab_Miles", "TBNSinstall_No"),
+                        names_to = "Var", values_to = "Value") %>%
+    tidyr::separate(Constr_Years, into = c("Time", "Location"), sep = "[()]") %>%
+    dplyr::mutate(Var = paste0(Var, "::", Location),
+                  Time = as.numeric(Time),
+                  Value = as.numeric(gsub(",","",Value)),
+                  EPU = c("All")) %>%
+    dplyr::select(!Location) %>%
+    dplyr::filter(!Value == "NA")
+
+  # metadata ----
+  attr(wind_dev_speed, "tech-doc_url") <- "https://noaa-edab.github.io/tech-doc.html"
+  attr(wind_dev_speed, "data_files")   <- list(
+    wind_dev_speed_xlsx = wind_dev_speed_xlsx)
+  attr(wind_dev_speed, "data_steward") <- c(
+    "Angela Silva <angela.silva@noaa.gov")
+  attr(wind_dev_speed, "plot_script") <- list(
+    `hd_MAB_0` = "human_dimensions_MAB.Rmd-wind-dev-speed0.R",
+    `hd_MAB_0.30` = "human_dimensions_MAB.Rmd-wind-dev-speed0.30.R")
+
+  if (save_clean){
+    usethis::use_data(wind_dev_speed, overwrite = T)
+  } else {
+    return(wind_dev_speed)
+  }
+}
+get_wind_dev_speed(save_clean = T)
