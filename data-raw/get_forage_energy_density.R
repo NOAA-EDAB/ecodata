0192e1565104958e25a4e85896e68276fb8b634f
kimberly-bastille <kimberly.bastille@gmail.com>
2021-12-10 11:08:09 -0500

    update forage fish energy density

--- data-raw/get_forage_energy_density.R
+++ data-raw/get_forage_energy_density.R
@@ -4,13 +4,13 @@ library(tidyverse)
 
 
 raw.dir <- here::here("data-raw")
-energy_density_csv<-"forage_energy_density.csv"
+energy_density_xlsx<-"2022SOE_Forage_ED_summary_Table.xlsx"
 get_forage_energy_density <- function(save_clean = F){
 
-  energy_density <- read.csv(file.path(raw.dir, energy_density_csv)) %>%
-    dplyr::select(Species, Year, Season, N, Energy.Density..J.GWW., StdDev.of.ED) %>%
-    dplyr::rename(Energy.Density_Mean = Energy.Density..J.GWW.,
-                  Energy.Density_SD = StdDev.of.ED)
+  energy_density <- read_excel(file.path(raw.dir, energy_density_xlsx)) %>%
+    dplyr::select(Species, Year, Season, N, "Energy Density (J/GWW)", "StdDev of ED") %>%
+    dplyr::rename(Energy.Density_Mean = "Energy Density (J/GWW)",
+                  Energy.Density_SD = "StdDev of ED")
 
   if (save_clean){
     usethis::use_data(energy_density, overwrite = T)
@@ -20,7 +20,7 @@ get_forage_energy_density <- function(save_clean = F){
   # metadata ----
   attr(energy_density, "tech-doc_url") <- "https://noaa-edab.github.io/tech-doc/forage-fish-energy-density.html"
   attr(energy_density, "data_files")   <- list(
-    energy_density_csv = energy_density_csv)
+    energy_density_xlsx = energy_density_xlsx)
   attr(energy_density, "data_steward") <- c(
     " Mark Wuenschel <mark.wuenschel@noaa.gov>")
 }
