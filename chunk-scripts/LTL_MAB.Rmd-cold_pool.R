6f33a6a0e2849abf7c2adf28e33902759870f2a6
kimberly-bastille <kimberly.bastille@gmail.com>
2021-12-10 13:28:14 -0500

    fix ltl and mega plots - update chunk scripts

--- chunk-scripts/LTL_MAB.Rmd-cold_pool.R
+++ chunk-scripts/LTL_MAB.Rmd-cold_pool.R
@@ -31,19 +31,20 @@ cp<- rbind(cpi, ei, pi)
 
 cp %>% ggplot2::ggplot() + 
  #Highlight last ten years
-  #ggplot2::annotate("rect", fill = shade.fill, alpha = shade.alpha,
-  #    xmin = x.shade.min , xmax = x.shade.max) +
-  ecodata::geom_gls(aes(x = Time, y = Value),
-             alpha = trend.alpha, size = trend.size) +
+  ggplot2::annotate("rect", fill = shade.fill, alpha = shade.alpha,
+      xmin = x.shade.min , xmax = x.shade.max,
+      ymin = -Inf, ymax = Inf)+
   ggplot2::geom_line(aes(x = Time, y = Value), size = lwd) +
   ggplot2::geom_point(aes(x = Time, y = Value), size = pcex) +
-  ggplot2::geom_ribbon(aes(x = Time, ymin = Lower, ymax = Upper), fill = "gray")+
+  #ggplot2::geom_ribbon(aes(x = Time, ymin = Lower, ymax = Upper), fill = "gray")+
   ggplot2::geom_hline(aes(yintercept = 0))+
   #   size = hline.size,
    #  alpha = hline.alpha,
     # linetype = hline.lty)+
   ggplot2::ggtitle("Cold Pool Index")+
   ggplot2::facet_wrap(~Var, scales = "free")+
+  ecodata::geom_gls(aes(x = Time, y = Value))+#,
+             #alpha = trend.alpha, size = trend.size) 
   ggplot2::ylab("Cold Pool") +
   ggplot2::xlab("")+
   ecodata::theme_ts()+
