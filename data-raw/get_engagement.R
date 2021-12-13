6c3959a19e0912801c8e803a970fa7215f3abc8f
kimberly-bastille <kimberly.bastille@gmail.com>
2021-12-13 10:57:46 -0500

    update community engagement and reliance

--- data-raw/get_engagement.R
+++ data-raw/get_engagement.R
@@ -9,31 +9,36 @@ library(janitor)
 
 raw.dir <- here::here("data-raw")
 
-engagement_xlsx<-"SocVul_in_Top_Fishing_Communities_updated 012921.xlsx"
+engagement_xlsx<-"EJ_in_Top_Fishing_Communities_WENG_121021 - Changhua Weng - NOAA Affiliate.xlsx"
 get_eng_rel <- function(save_clean = F){
 
   ## MAB
-  d1 <-read_excel(file.path(raw.dir, engagement_xlsx), sheet =  "Top 10 Communities_Mid-Atlantic")
-  col_names <- d1 %>%
-    slice(3)
-  dta <- d1 %>%
-    dplyr::slice(-1,-2,-3, -(17:36)) %>% # Removes the rows containing new and original names
-    rlang::set_names(., nm = col_names) %>%
-    dplyr::mutate(Fishery = c(rep("Commercial",13),rep("Recreational",17)))
-  ## NE
-  d2 <-read_excel(file.path(raw.dir, engagement_xlsx), sheet =  "Top 10 Communities_New England")
-  col_names <- d2 %>%
-    slice(3)
-  dta2 <- d2 %>%
-    slice(-1,-2,-3, -(21:36)) %>% # Removes the rows containing new and original names
-    rlang::set_names(., nm = col_names) %>%
-    dplyr::mutate(Fishery = c(rep("Commercial",17),rep("Recreational",14)))
+  d1 <-read_excel(file.path(raw.dir, engagement_xlsx), sheet =  "Mid-Atlantic_Bubble Chart")
 
-  col.names <-c("Region", "StAbb", "Community", "Eng", "Rel", "Rating", "Fishery")
+  dta <- d1 %>%
+    dplyr::slice(-(15:17)) %>% # Removes the rows containing new and original names
+    dplyr::rename("Eng" = "Commercial Engagement Index",
+                  "Rel" = "Commercial Reliance Index") %>%
+    dplyr::mutate(Fishery = c(rep("Commercial",14),rep("Recreational",16)),
+                  Region = c("MAB"))
+  ## NE
+  d2 <-read_excel(file.path(raw.dir, engagement_xlsx), sheet =  "New England_Bubble Chart")
+
+  dta2 <- d2 %>%
+    slice( -(18:22)) %>% # Removes the rows containing new and original names
+    dplyr::rename("Eng" = "Commercial Engagement Index",
+                  "Rel" = "Commercial Reliance Index")  %>%
+    dplyr::mutate(Fishery = c(rep("Commercial",17),rep("Recreational",16)),
+                  Region = c("NE"))
+
+
   engagement <- dta %>% rbind(dta2) %>%
-    dplyr::mutate(ComEng = as.numeric(ComEng),
-                  ComRel = as.numeric(ComRel))
-  colnames(engagement) = col.names
+    dplyr::rename("Community" = "Commuity Name",
+                  "Rating" = "EJ Rating") %>%
+    dplyr::mutate(Eng = as.numeric(Eng),
+                  Rel = as.numeric(Rel)) %>%
+    dplyr::filter(!Rating == "NA")
+
 
   # metadata ----
   attr(engagement, "tech-doc_url") <- "https://noaa-edab.github.io/tech-doc/community-engagement.html"
