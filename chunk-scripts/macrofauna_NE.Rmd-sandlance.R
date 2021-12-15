31029e1a759d1215b9e6296057bdc5043bf34328
kimberly-bastille <kimberly.bastille@gmail.com>
2021-12-15 17:10:55 -0500

    update macrofauna

---
+++ chunk-scripts/macrofauna_NE.Rmd-sandlance.R
@@ -0,0 +1,13 @@
+
+ecodata::sandlance %>% 
+  ggplot()+
+  ggplot2::geom_point(aes(x=Time, y = Value, color = Var))+
+  ggplot2::geom_line(aes(x=Time, y = Value, color = Var))+
+  ggplot2::ggtitle("Sandlance")+
+  ggplot2::ylab("Number of Individuals")+
+  ggplot2::xlab(element_blank())+
+  ecodata::theme_ts()+
+  ecodata::theme_title()+
+  ggplot2::theme(legend.title = element_blank(), 
+                 legend.position = "bottom")+
+  ecodata::theme_facet()
