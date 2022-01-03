1deb2d7a0014c7cf27aa457a4faa1bcd10098b59
kimberly-bastille <kimberly.bastille@gmail.com>
2022-01-03 15:01:35 -0500

    first draft EJ spider plots

--- data-raw/get_engagement.R
+++ data-raw/get_engagement.R
@@ -102,32 +102,4 @@ get_eng_rel <- function(save_clean = F){
 get_eng_rel(save_clean = T)
 
 
-### spiderplot
-create_beautiful_radarchart <- function(data, color = "#00AFBB",
-                                        vlabels = colnames(data), vlcex = 0.7,
-                                        caxislabels = NULL, title = NULL, ...){
-  radarchart(
-    data, axistype = 1,
-    # Customize the polygon
-    pcol = color, pfcol = scales::alpha(color, 0.5), plwd = 2, plty = 1,
-    # Customize the grid
-    cglcol = "grey", cglty = 1, cglwd = 0.8,
-    # Customize the axis
-    axislabcol = "grey",
-    # Variable labels
-    vlcex = vlcex, vlabels = vlabels,
-    caxislabels = caxislabels, title = title, ...
-  )
-}
-p1<- engagement %>%
-  dplyr::filter(Region == "MAB",
-                Fishery == "Commercial") %>%
-  dplyr::select(Community, PDI ,
-                PCI, PI)  %>%
-  tibble::column_to_rownames(var="Community")
 
-p2<- t(p1)
-p2<- data.frame(p2) %>%
-
-
-fmsb::radarchart(p2)
