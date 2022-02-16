8c10f09849f9195cc6eb9f40be3a3e2ffaa411cc
kimberly-bastille <kimberly.bastille@gmail.com>
2022-02-16 14:12:31 -0500

    add hudson river flow for catalog

---
+++ chunk-scripts/LTL_MAB.Rmd-hudson-river-flow.R
@@ -0,0 +1,14 @@
+
+ecodata::hudson_river_flow %>% 
+  ggplot2::ggplot(aes(x = Time, y = Value))+
+    ggplot2::annotate("rect", fill = shade.fill, alpha = shade.alpha,
+      xmin = x.shade.min , xmax = x.shade.max,
+      ymin = -Inf, ymax = Inf) +
+  ggplot2::geom_point()+
+  ggplot2::geom_line()+
+  ecodata::geom_gls(aes(x = Time, y = Value)) +
+  ecodata::theme_ts()+
+  ecodata::theme_title()+
+  ggplot2::ylab(expression("mean flowrate (m"^3*" s"^-1*")"))+
+  ggplot2::xlab(element_blank())+
+  ggplot2::ggtitle("Hudson River Flow")
---
+++ data-raw/get_hudson_river_flow.R
@@ -0,0 +1,37 @@
+# Hudson River Flow
+
+library(tidyverse)
+library(readxl)
+library(stringr)
+library(readr)
+
+raw.dir <- here::here("data-raw")
+
+hrf_csv<-"Gruenburg_RiverFlow2021 - Laura Gruenburg.csv"
+
+get_hudson_river_flow <- function(save_clean = F){
+
+  dat <- read.csv(file.path(raw.dir,hrf_csv), header=TRUE, stringsAsFactors=FALSE)
+
+  hudson_river_flow <- dat %>%
+    dplyr::select(!X) %>%
+    dplyr::mutate(EPU = c("MAB"),
+                  Var = c("flowrate")) %>%
+    dplyr::rename(Time = year,
+                  Value = flowrate)
+
+  # metadata ----
+  attr(hudson_river_flow, "tech-doc_url") <- "https://noaa-edab.github.io/tech-doc.html"
+  attr(hudson_river_flow, "data_files")   <- list(
+    hrf_csv = hrf_csv )
+  attr(hudson_river_flow, "data_steward") <- c(
+    "Laura Gruenburg <laura.gruenburg@stonybrook.edu>")
+  attr(hudson_river_flow, "plot_script") <- list()
+
+  if (save_clean){
+    usethis::use_data(hudson_river_flow, overwrite = TRUE)
+  } else {
+    return(hudson_river_flow)
+  }
+}
+get_hudson_river_flow(save_clean = T)
