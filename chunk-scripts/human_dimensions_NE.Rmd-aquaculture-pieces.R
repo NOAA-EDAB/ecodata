ac75dc9398dab8ab1ddf862deb4be63043899b10
kimberly-bastille <kimberly.bastille@gmail.com>
2022-02-16 13:21:33 -0500

    add package:: to aquaculture plot code to fix tech doc

--- chunk-scripts/human_dimensions_NE.Rmd-aquaculture-pieces.R
+++ chunk-scripts/human_dimensions_NE.Rmd-aquaculture-pieces.R
@@ -1,7 +1,7 @@
 
 ecodata::aquaculture %>%
-  ungroup() %>% 
-  mutate(Region = as.character(Region)) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::mutate(Region = as.character(Region)) %>% 
   dplyr::filter(!Region == "VA",
                 !Region == "NJ",
                 !Region == "MD",
@@ -9,10 +9,10 @@ ecodata::aquaculture %>%
                 !Value == "NA") %>% 
   dplyr::mutate(Time = as.integer(Time), 
                 Value = as.numeric(Value))%>% 
-  filter(Var == "Pieces") %>% 
-  group_by(Time) %>% 
-  summarise(Value = sum(Value)) %>% 
-  ungroup() %>% 
+  dplyr::filter(Var == "Pieces") %>% 
+  dplyr::group_by(Time) %>% 
+  dplyr::summarise(Value = sum(Value)) %>% 
+  dplyr::ungroup() %>% 
   ggplot2::ggplot() +
  #Highlight last ten years
   ggplot2::geom_line(aes(x = Time, y = Value), size = lwd) +
@@ -20,8 +20,8 @@ ecodata::aquaculture %>%
   ggplot2::ggtitle("Total Oyster Production in New England")+
   ggplot2::ylab(expression("Oysters production (Pieces)")) +
   ggplot2::xlab("")+
-  theme(legend.position="bottom", 
+  ggplot2::theme(legend.position="bottom", 
         legend.title = element_blank())+
-  scale_x_continuous(breaks=c(2009,2011,2013,2015, 2017, 2019))+
+  ggplot2::scale_x_continuous(breaks=c(2009,2011,2013,2015, 2017, 2019))+
   ecodata::theme_ts()+
   ecodata::theme_title()
