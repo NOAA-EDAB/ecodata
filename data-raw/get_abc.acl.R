7d5572c352fe8f9b25280ea16585516d3dc61479
kimberly-bastille <kimberly.bastille@gmail.com>
2022-09-28 15:30:44 -0400

    add NEFMC quota and catch

--- data-raw/get_abc.acl.R
+++ data-raw/get_abc.acl.R
@@ -8,9 +8,11 @@ library(readr)
 raw.dir <- here::here("data-raw")
 
 abc.acl_xlsx <- "ABC_ACL_catch - Brandon Muffley - NOAA Affiliate.xlsx"
+NEFMC_abc_acl_xlsx <- "NEFMC_abc_acl.xlsx"
+
 get_abc.acl <- function(save_clean = F){
   # import data
-  abc.acl<-read_excel(file.path(raw.dir,abc.acl_xlsx)) %>%
+  ma<-readxl::read_excel(file.path(raw.dir,abc.acl_xlsx)) %>%
     janitor::row_to_names(.,1) %>%
     dplyr::rename(spec = "Species/Sector") %>%
     tidyr::pivot_longer(cols = c("ABC/ACL 2012",   "Catch 2012",     "ABC/ACL 2013" ,
@@ -33,7 +35,7 @@ get_abc.acl <- function(save_clean = F){
                               "Blueline Tilefish Recreational - ABC/ACL", "Blueline Tilefish Recreational - Catch"))
 
 
-  blueline<-   read_excel(file.path(raw.dir,abc.acl_xlsx)) %>%
+  blueline<-   readxl::read_excel(file.path(raw.dir,abc.acl_xlsx)) %>%
     janitor::row_to_names(.,1) %>%
     dplyr::rename(spec = "Species/Sector") %>%
     tidyr::pivot_longer(cols = c("ABC/ACL 2012",   "Catch 2012",     "ABC/ACL 2013" ,
@@ -57,7 +59,16 @@ get_abc.acl <- function(save_clean = F){
     dplyr::select(Time, Var, Value, EPU, Units)
 
 
-  abc.acl <- abc.acl %>% rbind(blueline)
+  ne<- readxl::read_excel(file.path(raw.dir, NEFMC_abc_acl_xlsx)) %>%
+    tidyr::pivot_longer(cols = c("Quota", "Catch_Landings"), names_to = "Var",
+                        values_to = "Value") %>%
+    dplyr::mutate(Var = paste0(Species, "-", Var),
+                  EPU = c("NE"),
+                  Units = c("mt")) %>%
+    dplyr::select(Time, Var, Value, EPU, Units)
+
+
+  abc.acl <- ma %>% rbind(blueline, ne)
   # metadata ----
   attr(abc.acl, "tech-doc_url") <- "https://noaa-edab.github.io/tech-doc/mafmc-abcacl-and-catch.html"
   attr(abc.acl, "data_files")   <- list(
@@ -75,7 +86,43 @@ get_abc.acl <- function(save_clean = F){
 }
 get_abc.acl(save_clean = T)
 
-
-
+# groundfish<- abc.acl %>% filter(EPU == "NE") %>%
+#   tidyr::separate(col = Var, into = c("Fishery", "Var"), sep = "-") %>%
+#   tidyr::pivot_wider(names_from = "Var", values_from = "Value") %>%
+#   filter(stringr::str_detect(Fishery, "Groundfish")) %>%
+#   dplyr::group_by(Time) %>%
+#   dplyr::summarise(Quota, Catch_Landings) %>%
+#   dplyr::mutate(Fishery = "Groundfish",
+#                 EPU = "NE",
+#                 Units = "mt")
+#
+# sm_mesh<- abc.acl %>% filter(EPU == "NE") %>%
+#   tidyr::separate(col = Var, into = c("Fishery", "Var"), sep = "-") %>%
+#   tidyr::pivot_wider(names_from = "Var", values_from = "Value") %>%
+#   filter(stringr::str_detect(Fishery, "Small Mesh")) %>%
+#   dplyr::group_by(Time) %>%
+#   dplyr::summarise(Quota, Catch_Landings) %>%
+#   dplyr::mutate(Fishery = "Small Mesh",
+#                 EPU = "NE",
+#                 Units = "mt")
+#
+# abc.acl %>% filter(EPU == "NE") %>%
+#   tidyr::separate(col = Var, into = c("Fishery", "Var"), sep = "-") %>%
+#   tidyr::pivot_wider(names_from = "Var", values_from = "Value") %>%
+#   dplyr::filter(!stringr::str_detect(Fishery, "Groundfish"),
+#                 !stringr::str_detect(Fishery, "Small Mesh")) %>%
+#   rbind(groundfish, sm_mesh) %>%
+#   dplyr::mutate(Value = Catch_Landings/Quota) %>%
+#   ggplot2::ggplot()+
+#   ggplot2::geom_bar(aes( y = Value, x = Time, fill = Fishery),
+#                     stat="identity", position = "stack" )+
+#   ggplot2::ggtitle("ABC or ACL for NEFMC Managed Species")+
+#   #ggplot2::ylab(expression("ABC/ACL"))+
+#   ggplot2::theme(legend.text = element_text(size = 10),
+#                  legend.key.height = unit(2, "mm"))+
+#   ggplot2::ylab("ABC or ACL")+
+#   ggplot2::xlab(element_blank())+
+#   ecodata::theme_ts()+
+#   ecodata::theme_title()
 
 
