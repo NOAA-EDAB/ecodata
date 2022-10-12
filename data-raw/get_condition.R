12f178550bbd4dd6cd0b6e5183c2e933877b5d55
kimberly-bastille <kimberly.bastille@gmail.com>
2022-10-12 10:32:30 -0400

    add condition and  transition dates (full shelf from kevin)

---
+++ data-raw/get_condition.R
@@ -0,0 +1,36 @@
+## get condition
+## condition factor calculations from Laurel Smith using
+## BTS data by species aggregated to annual condition index.
+
+library(dplyr)
+library(tidyr)
+
+raw.dir <- here::here("data-raw")
+condition_csv <- "AnnualRelCond2021_GOM.csv"
+
+get_condition <- function(save_clean = F){
+  dat <- read.csv(file.path(raw.dir,condition_csv))
+
+  condition <- dat %>%
+    tidyr::pivot_longer(cols = c(MeanCond),
+                        names_to = "Var", values_to = "Value") %>%
+    dplyr::select(YEAR, Species, Value, EPU) %>%
+    dplyr::rename(Time = YEAR,
+                  Var = Species) %>%
+    dplyr::mutate(Units = c("MeanCond"))
+
+  # metadata ----
+  attr(condition, "tech-doc_url") <- "https://noaa-edab.github.io/tech-doc/condition.html"
+  attr(condition, "data_files")   <- list(
+    condition_csv = condition_csv)
+  attr(condition, "data_steward") <- c(
+    "Laurel Smith <laurel.smith@noaa.gov>")
+
+
+  if (save_clean){
+    usethis::use_data(condition, overwrite = T)
+  } else {
+    return(condition)
+  }
+}
+get_condition(save_clean = T)
