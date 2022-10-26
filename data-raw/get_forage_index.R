5a878705d6ad8615a210f85ac1c0db52137af2a9
kimberly-bastille <kimberly.bastille@gmail.com>
2022-10-26 11:35:51 -0400

    add forage index

---
+++ data-raw/get_forage_index.R
@@ -0,0 +1,27 @@
+## Forage Index
+raw.dir<- here::here("data-raw/")
+ann<- "annualforageindex - Sarah Gaichas - NOAA Federal.rds"
+fal<- "fallforageindex - Sarah Gaichas - NOAA Federal.rds"
+spr<- "springforageindex - Sarah Gaichas - NOAA Federal.rds"
+
+get_forage_index <- function(save_clean = F){
+
+  annual<-readRDS(file.path(raw.dir, ann))
+  fall<- readRDS(file.path(raw.dir, fal))
+  spring<-readRDS(file.path(raw.dir, spr))
+
+  forage_index<- annual %>% rbind(fall, spring)
+
+
+  if (save_clean){
+    usethis::use_data(forage_index, overwrite = T)
+  } else {
+    return(forage_index)
+  }
+}
+get_forage_index(save_clean = T)
+
+
+
+
+
