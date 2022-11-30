f731101a29f054b1e1a5e62446cf3c20feb292a3
kimberly-bastille <kimberly.bastille@gmail.com>
2022-11-30 14:16:41 -0500

    update code chunks

--- chunk-scripts/human_dimensions_MAB.Rmd-rec_hms.R
+++ chunk-scripts/human_dimensions_MAB.Rmd-rec_hms.R
@@ -1,12 +1,12 @@
 
 ecodata::rec_hms %>% 
-  filter(EPU == "MAB") %>% 
+  dplyr::filter(EPU == "MAB") %>%
+  tidyr::separate(Var, c("Var", "X"), sep = "-") %>%
+  dplyr::mutate(Value = Value/10000) %>% 
   ggplot2::ggplot()+
   ggplot2::geom_point(aes(x = Time, y = Value, color = Var))+
   ggplot2::geom_line(aes(x = Time, y = Value, color = Var))+
-  # ecodata::geom_lm(aes(x = Time, y = Value,
-  #              group = Var))+
-  ggplot2::ylab("Catch (N)")+
+  ggplot2::ylab(expression("Catch (N"^4*")"))+
   ggplot2::ggtitle("Recreational Shark Landings")+
   ggplot2::xlab(element_blank())+
   ggplot2::theme(legend.title = element_blank())+
--- chunk-scripts/human_dimensions_NE.Rmd-rec_hms.R
+++ chunk-scripts/human_dimensions_NE.Rmd-rec_hms.R
@@ -1,12 +1,14 @@
 
 ecodata::rec_hms %>% 
-  dplyr::filter(EPU == "NE") %>% 
+  dplyr::filter(EPU == "NE") %>%
+  tidyr::separate(Var, c("Var", "X"), sep = "-") %>%
+  dplyr::mutate(Value = Value/10000) %>% 
   ggplot2::ggplot()+
   ggplot2::geom_point(aes(x = Time, y = Value, color = Var))+
   ggplot2::geom_line(aes(x = Time, y = Value, color = Var))+
-  ggplot2::ylab("Catch (N)")+
-  ggplot2::xlab("")+ 
+  ggplot2::ylab(expression("Catch (N"^4*")"))+
   ggplot2::ggtitle("Recreational Shark Landings")+
+  ggplot2::xlab(element_blank())+
   ggplot2::theme(legend.title = element_blank())+
   ecodata::theme_ts()+
   ecodata::theme_title()
