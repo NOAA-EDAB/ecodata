fafa5dee097f67eda009e95594e418215092a2f2
kimberly-bastille <kimberly.bastille@gmail.com>
2022-12-02 14:14:30 -0500

    add bottom temp maps and time series from Hubert

--- data-raw/get_bottom_temp_comp.R
+++ data-raw/get_bottom_temp_comp.R
@@ -6,7 +6,7 @@ library(tidyr)
 
 raw.dir <- here::here("data-raw")
 bt_csv <- "bt_temp_product_epu - Hubert duPontavice - NOAA Affiliate.csv"
-
+bt_seasonal_csv<- "bt_temp_times_series_anomaly_epu.csv"
 get_bottom_temp_comp <- function(save_clean = F){
 
   bottom_temp_comp<- read.csv(file.path(raw.dir,bt_csv))  %>%
@@ -14,8 +14,21 @@ get_bottom_temp_comp <- function(save_clean = F){
     dplyr::rename("Time" = "year",
                   "EPU" = "subarea",
                   "Value" = "bt_temp") %>%
-    dplyr::mutate(Var = c("Bottom Temp"),
+    dplyr::mutate(Var = c("Annual_Bottom Temp"),
                   Units = c("degree C"))
+  bottom_temp_comp_seasonal<- read.csv(file.path(raw.dir,bt_seasonal_csv))  %>%
+    dplyr::select(!X) %>%
+    dplyr::mutate(season = recode(season, "1" = "Winter",
+                                  "2" = "Spring", "3" = "Summer",
+                                  "4" = "Fall")) %>%
+    dplyr::rename("Time" = "year",
+                  "EPU" = "subarea",
+                  "Value" = "anomaly") %>%
+    dplyr::mutate(Var = paste0(season,"_Bottom Temp Anomaly"),
+                  Units = c("degree C")) %>%
+    dplyr::select(Time, Value, Var, EPU, Units)
+
+  bottom_temp_comp<- rbind(bottom_temp_comp, bottom_temp_comp_seasonal)
 
   if (save_clean){
     usethis::use_data(bottom_temp_comp, overwrite = T)
@@ -26,16 +39,27 @@ get_bottom_temp_comp <- function(save_clean = F){
 get_bottom_temp_comp(save_clean = T)
 
 
-bt_nc <- "bt_temp_product_season_1959_2022 - Hubert duPontavice - NOAA Affiliate.nc"
+bt_nc <- "bt_temp_anomaly_season_2022.nc"
 
 get_bottom_temp_seasnonal_gridded <- function(save_clean = F){
 
-  bottom_temp_seasonal_gridded<- raster::stack(file.path(raw.dir,bt_nc))
+  b1<- raster::stack(file.path(raw.dir,bt_nc))
+  seasonal_bt_anomaly_gridded<- as.data.frame(b1) %>%
+    cbind(coordinates(b1)) %>%
+    dplyr::rename("Winter" = X1,
+                  "Spring" = X2,
+                  "Summer" = X3,
+                  "Fall" = X4,
+                  "Latitude" = y,
+                  "Longitude" = x) %>%
+    tidyr::pivot_longer(cols = c(Winter,Spring,
+                                 Summer, Fall),
+                        names_to = "Season", values_to = "Value")
 
   if (save_clean){
-    usethis::use_data(bottom_temp_seasonal_gridded, overwrite = T)
+    usethis::use_data(seasonal_bt_anomaly_gridded, overwrite = T)
   } else {
-    return(bottom_temp_seasonal_gridded)
+    return(seasonal_bt_anomaly_gridded)
   }
 }
 get_bottom_temp_seasnonal_gridded(save_clean = T)
