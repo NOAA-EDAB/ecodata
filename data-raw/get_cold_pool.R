e84f73811ea6e6243739e1de634b2f61325092b0
kimberly-bastille <kimberly.bastille@gmail.com>
2023-01-11 14:27:17 -0500

    update cold pool

--- data-raw/get_cold_pool.R
+++ data-raw/get_cold_pool.R
@@ -5,7 +5,7 @@ library(tidyr)
 
 raw.dir <- here::here("data-raw")
 
-cold_pool_csv <- "cold_pool_indice_2022 - Hubert duPontavice - NOAA Affiliate.csv" # "Glorys12v1_ColdPool_Extents.nc" #File from Zhoumin
+cold_pool_csv <- "cold_pool_indice_jan2023.csv" # "Glorys12v1_ColdPool_Extents.nc" #File from Zhoumin
 
 
 ### Cold pool index from Hubert du Pontavice
