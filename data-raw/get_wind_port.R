7cd1eae8ce011249fe7e87d8d8f541d277436736
kimberly-bastille <kimberly.bastille@gmail.com>
2023-01-17 10:17:12 -0500

    update wind port

--- data-raw/get_wind_port.R
+++ data-raw/get_wind_port.R
@@ -8,7 +8,7 @@ library(readr)
 raw.dir <- here::here("data-raw")
 
 #### Angela Silva
-wind_port_xlsx<-"EJwind_public - Angela Silva - NOAA Affiliate.xlsx"
+wind_port_xlsx<-"EJ_wind_SoE2023_01132023.xlsx"
 
 get_wind_port <- function(save_clean = F){
   df<- data.frame(State = c(" ME", " MA", " RI", " CT", " NY", " NJ", " MD", " VA", " NC"),
@@ -16,30 +16,30 @@ get_wind_port <- function(save_clean = F){
 
   # import data
   wind_port<-readxl::read_excel(file.path(raw.dir,wind_port_xlsx)) %>%
-    #tidyr::separate(VTR_PORT_ST, into = c("City", "State"), sep = ",") %>%
-    dplyr::rename("perc_rev_max" ="perc_MAX",
-                  "perc_rev_min" ="perc_MIN",
-                  "City" = "VTR_PORT",
-                  "wea_rev" = "WEA_MAX") %>%
-    dplyr::mutate(City = dplyr::recode(City,"DAVISVILLE, RI" = "NORTH KINGSTOWN, RI"),
-           City = dplyr::recode(City,"HAMPTON BAY, NY" ="HAMPTON BAY/SHINNECOCK, NY"),
-           City = dplyr::recode(City,"SHINNECOCK, NY" = "HAMPTON BAY/SHINNECOCK, NY"),
-           City = dplyr::recode(City,"BARNEGAT, NJ" = "BARNEGAT LIGHT, NJ" ),
-           City = dplyr::recode(City,"LONG BEACH, NJ" = "BARNEGAT LIGHT, NJ"),
-           City = dplyr::recode(City,"MENEMSHA, MA" = "CHILMARK, MA"),
-           City = dplyr::recode(City,"BASS RIVER/YARMOUTH, MA" = "BASS RIVER, MA")) %>%
+    tidyr::separate(VTR_PORT_ST, into = c("City", "State"), sep = ",") %>%
+    # dplyr::rename("perc_rev_max" ="perc_MAX",
+    #               "perc_rev_min" ="perc_MIN",
+    #               "City" = "VTR_PORT",
+    #               "wea_rev" = "WEA_MAX") %>%
+    #dplyr::rename("City" = "VTR_PORT") %>%
+    dplyr::mutate(City = dplyr::recode(City,"DAVISVILLE" = "NORTH KINGSTOWN"),
+           City = dplyr::recode(City,"HAMPTON BAY" ="HAMPTON BAY/SHINNECOCK"),
+           City = dplyr::recode(City,"SHINNECOCK" = "HAMPTON BAY/SHINNECOCK"),
+           City = dplyr::recode(City,"BARNEGAT" = "BARNEGAT LIGHT" ),
+           City = dplyr::recode(City,"LONG BEACH" = "BARNEGAT LIGHT"),
+           City = dplyr::recode(City,"MENEMSHA" = "CHILMARK"),
+           City = dplyr::recode(City,"BASS RIVER/YARMOUTH" = "BASS RIVER")) %>%
     dplyr::group_by(City) %>%
-    dplyr::mutate( wea_rev = sum(wea_rev)) %>%
+    dplyr::mutate( WEA_MAX = sum(WEA_MAX)) %>%
     dplyr::slice(1) %>%
     dplyr::ungroup() %>%
-    dplyr::mutate(
-           perc_rev_max = perc_rev_max*100,
-           perc_rev_min = perc_rev_min*100,
-           total_rev = 100 - perc_rev_min - perc_rev_max) %>%
-    dplyr::select(City, perc_rev_min, perc_rev_max, TOT_MAX, EJ, Gent, wea_rev) %>%
-    tidyr::pivot_longer(cols = c(perc_rev_min, perc_rev_max, TOT_MAX, EJ, Gent, wea_rev),
+    dplyr::mutate(perc_MAX = perc_MAX*100,
+           perc_MIN = perc_MIN*100,
+           total_rev = 100 - perc_MIN - perc_MAX) %>%
+    dplyr::select(State, City, perc_MIN, perc_MAX, TOT_MAX, EJ, Gent, WEA_MAX) %>%
+    tidyr::pivot_longer(cols = c(perc_MIN, perc_MAX, TOT_MAX, EJ, Gent, WEA_MAX),
                         names_to = "Var", values_to = "Value") %>%
-    tidyr::separate(City, into = c("City", "State"), sep = ",") %>%
+    #tidyr::separate(City, into = c("City", "State"), sep = ",") %>%
     left_join(df, by = "State")
 
 
