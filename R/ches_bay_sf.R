703a3dbf0a658790511cc9956917969cad302f43
andybeet <22455149+andybeet@users.noreply.github.com>
2023-08-01 22:54:45 -0400

    added missign r data documentation files

--- R/ches_bay_sf.R
+++ R/ches_bay_sf.R
@@ -10,4 +10,4 @@
 #' }
 #'
 #' @details More information available at \url{https://noaa-edab.github.io/tech-doc/https://noaa-edab.github.io/tech-doc/chesapeake-bay-salinity-and-temperature.html}
-"ch_bay_sf"
+"ches_bay_sf"
