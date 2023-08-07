508ae97d6b8fd2acd2f287b0d118f1a8a8bec5ce
andybeet <22455149+andybeet@users.noreply.github.com>
2023-08-07 10:28:33 -0400

    plot_bottom_temp_anomoly: - glorys - survey
    by region

---
+++ R/plot_bottom_temp_anomaly_survey.R
@@ -0,0 +1,74 @@
+#' plot bottom anomaly temperature time series using NEFSC survey data
+#'
+#' ecodata::bottom_temp
+#'
+#' @param region Character vector. Regional EPUs ("GB","MAB") to overly on figure. (Default = NULL, use all)
+#' @param shadedRegion Numeric vector. Years denoting the shaded region of the plot
+#' @param shade.fill Character string. Color of shaded region. (Default = "lightgrey)
+#' @param shade.alpha Numeric scalar. Alpha of shaded region (Default = 0.5)
+#' @param hline.size Numeric scalar Line width of horizontal line. (Default = 1)
+#' @param hline.alpha Numeric scalar. Alpha of horizontal line (Default = 0.5)
+#' @param hline.lty Character string. Line type of horizontal line (Default = "dashed")
+#'
+#' @importFrom ggplot2 aes element_text theme element_rect  margin
+#' @importFrom magrittr "%>%"
+#'
+#' @return ggplot object
+#'
+#' @export
+
+plot_bottom_temp_anomaly_survey <- function(region = NULL,
+                                          shadedRegion=c(2012,2022),
+                                          shade.fill="lightgrey",
+                                          shade.alpha=0.3,
+                                          hline.size = 1,
+                                          hline.alpha = 0.5,
+                                          hline.lty = "dashed") {
+  # find all EPUs in dtaa set
+  regions <- unique(ecodata::bottom_temp$EPU)
+
+  if(is.null(region) || !(region %in% regions)){
+     stop(paste("please specify a region to plot:", paste(regions,collapse = ",")))
+  }
+
+  x.shade.min <- shadedRegion[1]
+  x.shade.max <- shadedRegion[2]
+
+  temp_anom <- ecodata::bottom_temp %>%
+    dplyr::filter(EPU %in% region) %>%
+    tidyr::complete(Time = tidyr::full_seq(min(bottom_temp$Time):max(bottom_temp$Time),1),
+                    tidyr::nesting(Var)) %>%
+    dplyr::mutate(hline = 0)%>%
+    dplyr::filter(Var == "bottom temp anomaly in situ")
+
+  gl_bt<- ecodata::bottom_temp_glorys%>%
+    dplyr::filter(EPU %in% region)
+
+  p <- ggplot2::ggplot() +
+    ggplot2::annotate("rect", fill = shade.fill, alpha = shade.alpha,
+                      xmin = x.shade.min , xmax = x.shade.max,
+                      ymin = -Inf, ymax = Inf) +
+    ggplot2::geom_line(aes(x = temp_anom$Time, y = temp_anom$Value), color = "black") +
+    ggplot2::geom_point(aes(x = temp_anom$Time, y = temp_anom$Value), size = 1, color = "black") +
+    ggplot2::geom_point(aes(x = gl_bt$Time, y = gl_bt$Value), size = 1, color = "red") +
+    ggplot2::geom_line(aes(x = gl_bt$Time, y = gl_bt$Value), color = "red") +
+    ecodata::geom_gls(aes(x = temp_anom$Time, y = temp_anom$Value)) +
+    #ecodata::geom_lm(aes(x = temp_anom$Time, y = temp_anom$Value))+
+    ggplot2::ylab("Temperature (C)") +
+    ggplot2::xlab(element_blank())+
+    ggplot2::ggtitle(paste(region," Bottom temperature anomaly")) +
+    ggplot2::scale_x_continuous(expand = c(0.01, 0.01)) +
+    ggplot2::theme(strip.text=element_text(hjust=0),
+                   plot.title = element_text(size = 12))+
+    #ggplot2::facet_wrap(~ gl_bt$EPU)+
+    #ggplot2::geom_hline(aes(yintercept = hline),
+    #         size = hline.size,
+    #         alpha = hline.alpha,
+    #         linetype = hline.lty) +
+    ecodata::theme_ts()+
+    ecodata::theme_facet()+
+    ecodata::theme_title()
+
+
+  return(p)
+}
