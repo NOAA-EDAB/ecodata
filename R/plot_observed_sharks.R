9a45edc8b9e7933bd8912d702b7ae5acc618233d
andybeet <22455149+andybeet@users.noreply.github.com>
2023-12-06 16:47:04 -0500

    added plot_nao added plot_observed_sharks added plot_long_term_sst

---
+++ R/plot_observed_sharks.R
@@ -0,0 +1,64 @@
+#' plot NE observer program shark data
+#'
+#' plots observed_shark data set
+#'
+#' @param shadedRegion Numeric vector. Years denoting the shaded region of the plot (most recent 10)
+#' @param report Character string. Which SOE report ("MidAtlantic", "NewEngland")
+#'
+#' @return ggplot object
+#'
+#'
+#' @export
+#'
+
+plot_observed_sharks <- function(shadedRegion = shadedRegion,
+                              report="MidAtlantic") {
+
+  # generate plot setup list (same for all plot functions)
+  setup <- ecodata::plot_setup(shadedRegion = shadedRegion,
+                               report=report)
+
+  # which report? this may be bypassed for some figures
+  if (report == "MidAtlantic") {
+    filterEPUs <- c("MAB")
+  } else {
+    filterEPUs <- c("GB", "GOM")
+  }
+
+  # optional code to wrangle ecodata object prior to plotting
+  # e.g., calculate mean, max or other needed values to join below
+   fix<- ecodata::observed_sharks |>
+     dplyr::filter(EPU %in% filterEPUs)
+
+  # code for generating plot object p
+  # ensure that setup list objects are called as setup$...
+  # e.g. fill = setup$shade.fill, alpha = setup$shade.alpha,
+  # xmin = setup$x.shade.min , xmax = setup$x.shade.max
+  #
+  p <- fix |>
+    ggplot2::ggplot(ggplot2::aes(x = Time, y = Value, color=Var))+
+    ggplot2::annotate("rect", fill = setup$shade.fill, alpha = setup$shade.alpha,
+        xmin = setup$x.shade.min , xmax = setup$x.shade.max,
+        ymin = -Inf, ymax = Inf) +
+    ggplot2::geom_point()+
+    ggplot2::geom_line()+
+    ggplot2::ggtitle("Observed Sharks")+
+    ggplot2::ylab(expression("Number of Sharks per Haul"))+
+    ggplot2::xlab(ggplot2::element_blank())+
+    ggplot2::facet_wrap(.~EPU)+
+    #ecodata::geom_gls()+
+    ecodata::theme_ts()+
+    ecodata::theme_facet()+
+    ecodata::theme_title()
+
+   # optional code for New England specific (2 panel) formatting
+    # if (report == "NewEngland") {
+    #   p <- p +
+    #     ggplot2::theme(legend.position = "bottom",
+    #                    legend.title = ggplot2::element_blank())
+    #
+    # }
+
+    return(p)
+
+}
