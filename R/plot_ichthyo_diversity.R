2e42792e191dc55bec5a8a5eddcffac7488698b5
andybeet <22455149+andybeet@users.noreply.github.com>
2023-12-07 16:08:39 -0500

    added plot_ichthyo_diversity

---
+++ R/plot_ichthyo_diversity.R
@@ -0,0 +1,73 @@
+#' plot Larval diversity
+#'
+#' plots ichthyo_diversity data set.
+#'
+#' @param shadedRegion Numeric vector. Years denoting the shaded region of the plot (most recent 10)
+#' @param report Character string. Which SOE report ("MidAtlantic", "NewEngland")
+#'
+#' @return ggplot object
+#'
+#'
+#' @export
+#'
+
+plot_ichthyo_diversity <- function(shadedRegion = shadedRegion,
+                              report="MidAtlantic") {
+
+  # generate plot setup list (same for all plot functions)
+  setup <- ecodata::plot_setup(shadedRegion = shadedRegion,
+                               report=report)
+
+  # which report? this may be bypassed for some figures
+  if (report == "MidAtlantic") {
+    filterEPUs <- c("MAB")
+  } else {
+    filterEPUs <- c("GB", "GOM")
+  }
+
+  # optional code to wrangle ecodata object prior to plotting
+  # e.g., calculate mean, max or other needed values to join below
+   fix<- ecodata::ichthyo_diversity |>
+     dplyr::filter(EPU %in% filterEPUs) |>
+     dplyr::mutate(Var = stringr::word(Var,1))  |>
+     dplyr::group_by(Var) |>
+     dplyr::mutate(hline = mean(Value, na.rm = T))
+
+  # code for generating plot object p
+  # ensure that setup list objects are called as setup$...
+  # e.g. fill = setup$shade.fill, alpha = setup$shade.alpha,
+  # xmin = setup$x.shade.min , xmax = setup$x.shade.max
+  #
+  p <- fix |>
+    ggplot2::ggplot(ggplot2::aes(x = Time, y = Value, group = Var))+
+    ggplot2::annotate("rect", fill = setup$shade.fill, alpha = setup$shade.alpha,
+        xmin = setup$x.shade.min , xmax = setup$x.shade.max,
+        ymin = -Inf, ymax = Inf) +
+    ggplot2::geom_point()+
+    ggplot2::geom_line()+
+    ggplot2::geom_hline(ggplot2::aes(yintercept = hline,
+                                     group = Var),
+                        linewidth = setup$hline.size,
+                        alpha = setup$hline.alpha,
+                        linetype = setup$hline.lty)+
+    ggplot2::ggtitle(paste0(report,": Larval diversity"))+
+    ggplot2::ylab(expression("Shannon index"))+
+    ggplot2::xlab(ggplot2::element_blank())+
+    ggplot2::facet_wrap(.~EPU)+
+    #ecodata::geom_gls()+
+    ecodata::theme_ts()+
+    ecodata::theme_facet()+
+    ecodata::theme_title()
+
+   # optional code for New England specific (2 panel) formatting
+    if (report == "NewEngland") {
+      p <- p +
+        ggplot2::theme(legend.position = "bottom",
+                       legend.title = ggplot2::element_blank())
+
+    }
+
+    return(p)
+
+
+}
