1022b9952722831c6b3eb5767eaae484759081bc
Brandon Beltz - NOAA Affiliate <136381970+BBeltz1@users.noreply.github.com>
2024-01-22 12:55:32 -0500

    Add new annual_chl_pp indicator
    Wrote a new get function to import a new dataset from Kim Hyde. Added new data file to ecodata/data-raw folder. Built ecodata with new indicator.

---
+++ data-raw/get_annual_chl_pp.R
@@ -0,0 +1,27 @@
+library(dplyr)
+library(tidyr)
+library(stringr)
+
+raw.dir <- here::here("data-raw")
+
+# input files ----
+chl_ppd_ann_csv <- "MERGED_ANNUAL_SUM-NES_EPU_NOESTUARIES-PPD-VGPM2_STATS-V2024-SOE_FORMAT.csv"
+
+# transformation ----
+annual_chl_pp <- read.csv(file.path(raw.dir, chl_ppd_ann_csv)) %>%
+  dplyr::select(PERIOD, UNITS, VARIABLE, VALUE, SUBAREA) %>%
+  dplyr::rename(Time = PERIOD, Units = UNITS, Var = VARIABLE,
+                EPU =SUBAREA, Value = VALUE) %>%
+  tibble::as_tibble()
+
+# metadata ----
+#attr(chl_pp, "tech-doc_url") <- "https://noaa-edab.github.io/tech-doc/chl-pp.html"
+#attr(chl_pp, "data_files")   <- list(
+  #chl_csv = chl_csv,
+  #ppd_csv = ppd_csv)
+#attr(chl_pp, "data_steward") <- c(
+  #"Kimberly Hyde <kimberly.hyde@noaa.gov>")
+#attr(chl_pp, "plot_script") <- list(
+  #`ltl_NE_anom` = "LTL_NE.Rmd-chl-pp-anom.R")
+
+usethis::use_data(annual_chl_pp, overwrite = T)
