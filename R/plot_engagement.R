1038ebd829092c690be2d622ba574baf7c5340a9
andybeet <22455149+andybeet@users.noreply.github.com>
2024-01-25 16:00:01 -0500

    plot_engagement, plot_ch_bay_sst plots fixed based on field name change

--- R/plot_engagement.R
+++ R/plot_engagement.R
@@ -13,7 +13,7 @@
 #'
 
 plot_engagement <- function(shadedRegion = NULL,
-                              report="MidAtlantic",
+                            report="MidAtlantic",
                             varName="Commercial") {
 
   # generate plot setup list (same for all plot functions)
@@ -30,7 +30,7 @@ plot_engagement <- function(shadedRegion = NULL,
   # optional code to wrangle ecodata object prior to plotting
   # e.g., calculate mean, max or other needed values to join below
   eng<-ecodata::engagement |>
-    dplyr::filter(Region == filterEPUs,
+    dplyr::filter(EPU == filterEPUs,
                   Fishery == varName) |>
     dplyr::rename("EJRating" = "EJ Rating")
 
@@ -43,8 +43,8 @@ plot_engagement <- function(shadedRegion = NULL,
   eng2<-eng |>
     ggplot2::ggplot()+
     ggplot2::geom_point(ggplot2::aes(x = Eng, y = Rel, color = EJRating), size = 2)+
-    ggplot2::geom_vline(xintercept = 1, linetype = "dashed",color = "black", size = 0.5)+
-    ggplot2::geom_hline(yintercept = 1, linetype = "dashed", color = "black", size = 0.5) +
+    ggplot2::geom_vline(xintercept = 1, linetype = "dashed",color = "black", linewidth = 0.5)+
+    ggplot2::geom_hline(yintercept = 1, linetype = "dashed", color = "black", linewidth = 0.5) +
     ggrepel::geom_text_repel(ggplot2::aes(x = Eng, #geom_text_repel auto-jitters text around points
                                  y = Rel,
                                  label = Community,
@@ -75,49 +75,9 @@ plot_engagement <- function(shadedRegion = NULL,
 
     return(p)
 
-  # Paste commented original plot code chunk for reference
-  # Mid commercial only
-  # com<-ecodata::engagement %>%
-  #   dplyr::filter(Region == "MAB",
-  #                 Fishery == "Commercial") %>%
-  #   dplyr::rename("EJRating" = "EJ Rating")
-  #
-  # com2<-com %>%
-  #   ggplot2::ggplot()+
-  #   ggplot2::geom_point(aes(x = Eng, y = Rel, color = EJRating), size = 2)+
-  #   ggplot2::geom_vline(xintercept = 1, linetype = "dashed",color = "black", size = 0.5)+
-  #   ggplot2::geom_hline(yintercept = 1, linetype = "dashed", color = "black", size = 0.5) +
-  #   ggrepel::geom_text_repel(aes(x = Eng, #geom_text_repel auto-jitters text around points
-  #                                y = Rel,
-  #                                label = Community,
-  #                                color = EJRating), show.legend = FALSE, direction = "both", box.padding = 0.2, size = 3)+
-  #   ggplot2::scale_color_brewer(palette = "Dark2", #Change legend labels for clarity
-  #                               breaks = com$EJRating) +
-  #   xlim(-2,12.7)+
-  #   ylim(-1,3.0)+
-  #   theme(legend.position=c(0.85, 0.15),
-  #         #legend.title = element_blank(),
-  #         legend.background = element_blank(),
-  #         legend.box.background = element_rect(colour = "black"))+
-  #   labs(color = "EJ Vulnerability")+
-  #   ggplot2::xlab("Commercial Engagement Index") +
-  #   ggplot2::ylab("Commercial Reliance Index") +
-  #   ggplot2::ggtitle("Environmental Justice in Top Commercial Fishing Communities")+
-  #   #ggplot2::guides(color = FALSE) +
-  #   #theme_bw()
-  #   ecodata::theme_ts()+
-  #   ecodata::theme_title()
-  # #ecodata::theme_facet()
-  #
-  #
-  # gridExtra::grid.arrange(com2, bottom = textGrob("Low <---------------------------------------------------------------------------------------------------------------------------> High",
-  #                                                 x = 0.5, y = 1, gp = gpar(fontsize = 7)),
-  #                         left = textGrob("Low <--------------------------------------------------------------------------------------> High", rot = 90,
-  #                                         x = 1, y = 0.45, gp = gpar(fontsize = 7)))
-  #
-  #
 
 }
 
-attr(plot_engagement,"varName") <- c("Commercial","Recreational")
 attr(plot_commercial_div,"report") <- c("MidAtlantic","NewEngland")
+attr(plot_engagement,"varName") <- c("Commercial","Recreational")
+
