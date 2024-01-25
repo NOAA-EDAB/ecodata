bff8655d3d390f97a43f4fee1c5560b875553747
Sarah Gaichas <sarah.gaichas@noaa.gov>
2024-01-25 14:50:49 -0500

    add forage center of gravity

--- data-raw/get_forage_index.R
+++ data-raw/get_forage_index.R
@@ -3,14 +3,18 @@ raw.dir<- here::here("data-raw/")
 #ann<- "annualforageindex - Sarah Gaichas - NOAA Federal.rds"
 fal<- "fallforageindex - Sarah Gaichas - NOAA Federal.rds"
 spr<- "springforageindex - Sarah Gaichas - NOAA Federal.rds"
+falcog <- "fallforagecog.rds"
+sprcog <- "springforagecog.rds"
 
 get_forage_index <- function(save_clean = F){
 
   #annual<-readRDS(file.path(raw.dir, ann))
   fall<- readRDS(file.path(raw.dir, fal))
   spring<-readRDS(file.path(raw.dir, spr))
+  fallcog <- readRDS(file.path(raw.dir, falcog))
+  springcog <-readRDS(file.path(raw.dir, sprcog))
 
-  forage_index<- rbind(fall, spring)
+  forage_index<- rbind(fall, spring, fallcog, springcog)
 
 
   if (save_clean){
