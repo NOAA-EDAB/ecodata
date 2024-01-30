76e9312c1e484f7ac639984a03a452e90afed499
Sarah Gaichas <sarah.gaichas@noaa.gov>
2024-01-30 13:41:16 -0500

    add spawn_timing data and get function

---
+++ data-raw/get_spawn_timing.R
@@ -0,0 +1,15 @@
+## Spawn timing
+raw.dir<- here::here("data-raw/")
+sptime <- readRDS(file.path(raw.dir, fal))
+
+get_spawn_timing <- function(save_clean = F){
+
+  spawn_timing<- readRDS(file.path(raw.dir, sptime))
+
+  if (save_clean){
+    usethis::use_data(spawn_timing, overwrite = T)
+  } else {
+    return(spawn_timing)
+  }
+}
+get_spawn_timing(save_clean = T)
