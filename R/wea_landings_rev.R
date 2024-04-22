2ada343b1eb5d30a3fd19ca6328a8fe0f19f598d
Sarah Gaichas <sarah.gaichas@noaa.gov>
2024-04-22 16:40:42 -0400

    correct council abbr NEFMC in Council column

--- R/wea_landings_rev.R
+++ R/wea_landings_rev.R
@@ -2,13 +2,14 @@
 #'
 #' Data include top ten species Landings and Revenue from Wind Energy Areas.
 #'
-#' @format Data set contains 46 rows and 4 columns
+#' @format Data set contains 32 rows and 5 columns
 #'
 #' \itemize{
 #'     \item NEFMC, MAFMC, and ASMFC Managed Species: Managed Fishery.
 #'     \item perc_landings_max: Max Landings Percent from WEA.
 #'     \item perc_revenue_max: Max Revenue Percent from WEA.
 #'     \item Units: Units for landings or revenue.
+#'     \item Council: New England (NEFMC) or Mid Atlantic (MAFMC)
 #' }
 #'
 #' @details
