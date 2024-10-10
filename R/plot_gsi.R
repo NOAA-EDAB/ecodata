1fb0985eac69cb313b70c9d75b3061386723e069
andybeet <22455149+andybeet@users.noreply.github.com>
2024-10-10 14:42:55 -0400

    - example plot function change (plot_gsi) - added argument n = number of points in trend fit

--- R/plot_gsi.R
+++ R/plot_gsi.R
@@ -5,6 +5,8 @@
 #' @param shadedRegion Numeric vector. Years denoting the shaded region of the plot (most recent 10)
 #' @param report Character string. Which SOE report ("MidAtlantic", "NewEngland")
 #' @param varName Character string. Which variable to plot ("gsi","westgsi")
+#' @param n Numeric scalar. Number of values used in short term trend
+#'
 #' @return ggplot object
 #'
 #'
@@ -13,7 +15,8 @@
 
 plot_gsi <- function(shadedRegion = NULL,
                      report="MidAtlantic",
-                     varName = "gsi") {
+                     varName = "gsi",
+                     n = 10) {
 
   # generate plot setup list (same for all plot functions)
   setup <- ecodata::plot_setup(shadedRegion = shadedRegion,
@@ -61,6 +64,7 @@ plot_gsi <- function(shadedRegion = NULL,
     ggplot2::ylab("Anomaly")+
     ggplot2::xlab(ggplot2::element_blank())+
     ecodata::geom_gls()+
+    ecodata::geom_lm(n=n)+
     ecodata::theme_ts()+
     ggplot2::geom_hline(ggplot2::aes(yintercept = hline),
                         linewidth = setup$hline.size,
