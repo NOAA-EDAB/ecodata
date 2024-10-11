e4df5b51262a2a29d03a1f273f86fdeb55f43c7b
andybeet <22455149+andybeet@users.noreply.github.com>
2024-10-11 14:50:19 -0400

    changed default argument to plot function to zero for backward compatibility

--- R/plot_gsi.R
+++ R/plot_gsi.R
@@ -5,7 +5,7 @@
 #' @param shadedRegion Numeric vector. Years denoting the shaded region of the plot (most recent 10)
 #' @param report Character string. Which SOE report ("MidAtlantic", "NewEngland")
 #' @param varName Character string. Which variable to plot ("gsi","westgsi")
-#' @param n Numeric scalar. Number of values used in short term trend
+#' @param n Numeric scalar. Number of years used (from most recent year) to estimate short term trend . Default = 0 (No trend calculated)
 #'
 #' @return ggplot object
 #'
@@ -16,7 +16,7 @@
 plot_gsi <- function(shadedRegion = NULL,
                      report="MidAtlantic",
                      varName = "gsi",
-                     n = 10) {
+                     n = 0) {
 
   # generate plot setup list (same for all plot functions)
   setup <- ecodata::plot_setup(shadedRegion = shadedRegion,
