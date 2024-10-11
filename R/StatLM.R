8481cebcfe6deb8da273afc960c902576b1b5024
andybeet <22455149+andybeet@users.noreply.github.com>
2024-10-11 14:49:14 -0400

    added if clause to only fit short term trend if number of points >4

--- R/StatLM.R
+++ R/StatLM.R
@@ -17,57 +17,60 @@ StatLM <- ggplot2::ggproto("StatLM",
                               #nBootSamples <- 499
 ###########################################################################
                               `%>%` <- magrittr::`%>%`
-                              dataUse <- data %>%
-                                dplyr::arrange(x) %>%
-                                # Select last n years
-                                dplyr::filter(x %in% (max(x)-(n-1)):max(x)) %>%
-                                dplyr::mutate(x = x-min(x)+1)
 
-                              # print("########RAW##########")
-                               #print(data)
-                              # print((max(data$x)-(n-1)))
-                              # print(max(data$x))
-                              # print(data$x-min(data$x)+1)
-                              #
-                              # print("#######USE##########")
-                              # print(dataUse)
-                              # print("#######################")
+                              if(n>4) {
+                                dataUse <- data %>%
+                                  dplyr::arrange(x) %>%
+                                  # Select last n years
+                                  dplyr::filter(x %in% (max(x)-(n-1)):max(x)) %>%
+                                  dplyr::mutate(x = x-min(x)+1)
 
-                              xmax <- max(data$x)
-                              xmin <- xmax-n +1
+                                # print("########RAW##########")
+                                 #print(data)
+                                # print((max(data$x)-(n-1)))
+                                # print(max(data$x))
+                                # print(data$x-min(data$x)+1)
+                                #
+                                # print("#######USE##########")
+                                # print(dataUse)
+                                # print("#######################")
 
-                              # Linear model with AR1 error
-                              linear_ar1 <-
-                                try(arfit::fit_real_data(dataUse,nBootSims=nBootSamples))
-                              #print(linear_ar1$pValue)
-                              if (is.na(linear_ar1$pValue)){
-                                return(best_lm <- data.frame(model = NA,
-                                                             coefs..Intercept = NA,
-                                                             coefs.time = NA,
-                                                             coefs.time2 = NA,
-                                                             pval = NA))
+                                xmax <- max(data$x)
+                                xmin <- xmax-n +1
 
-                              }
+                                # Linear model with AR1 error
+                                linear_ar1 <-
+                                  try(arfit::fit_real_data(dataUse,nBootSims=nBootSamples))
+                                #print(linear_ar1$pValue)
+                                if (is.na(linear_ar1$pValue)){
+                                  return(best_lm <- data.frame(model = NA,
+                                                               coefs..Intercept = NA,
+                                                               coefs.time = NA,
+                                                               coefs.time2 = NA,
+                                                               pval = NA))
 
-                              # pick out model. Either null (no trend) or alternative (trend)
-                              if (linear_ar1$pValue <= pValThreshold) { # Trend detected
-                                coefs <- linear_ar1$alt$betaEst
-                                xMat <- as.matrix(cbind(rep(1,n),dataUse$x)) # design matrix
+                                }
 
-                              # } else { # no trend
-                              #   coefs <- linear_ar1$null$betaEst
-                              #   xMat <- as.matrix(cbind(rep(1,n))) # design matrix
-                              # }
+                                # pick out model. Either null (no trend) or alternative (trend)
+                                if (linear_ar1$pValue <= pValThreshold) { # Trend detected
+                                  coefs <- linear_ar1$alt$betaEst
+                                  xMat <- as.matrix(cbind(rep(1,n),dataUse$x)) # design matrix
 
-                                # predict
+                                # } else { # no trend
+                                #   coefs <- linear_ar1$null$betaEst
+                                #   xMat <- as.matrix(cbind(rep(1,n))) # design matrix
+                                # }
 
-                                predy <- xMat %*% coefs
+                                  # predict
 
-                                newtime <- seq(xmin, xmax, length.out=n)
-                                fittedData <- data.frame(x = newtime,
-                                                     y = predy)
+                                  predy <- xMat %*% coefs
 
-                                return(fittedData)
+                                  newtime <- seq(xmin, xmax, length.out=n)
+                                  fittedData <- data.frame(x = newtime,
+                                                       y = predy)
+
+                                  return(fittedData)
+                                }
                               }
 
                             }
