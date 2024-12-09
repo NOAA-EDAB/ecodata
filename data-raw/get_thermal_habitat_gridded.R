59155138ef472ac61e135d6612bfd31072e65af6
Joseph Caracappa (NOAA) <57966543+jcaracappa1@users.noreply.github.com>
2024-12-09 16:56:24 -0500

    changed get_thermal_habitat_gridded to pull from ERDDAP before formatting

--- data-raw/get_thermal_habitat_gridded.R
+++ data-raw/get_thermal_habitat_gridded.R
@@ -1,26 +1,77 @@
 ### Thermal Habitat Persistence - Joe Caracappa
 
-#library(dplyr)
-#library(usethis)
+# install.packages('rerddap')
+library(dplyr)
+library(usethis)
+library(rerddap)
+library(terra)
 
-get_thermal_habitat_gridded <- function(save_clean = F){
+get_thermal_habitat_gridded <- function(threshold, year, depth.bins, save_clean = F){
 
-  thermal_habitat_gridded<-readr::read_csv(here::here("data-raw/thermal_habitat_persistence_2023 - Joseph Caracappa - NOAA Federal.csv"),
-                       show_col_types = F) |>
-    dplyr::mutate(Units = "Number of Days",
-                  Depth = paste0(min.depth,"-",max.depth,"m"),
-                  EPU = "All") |>
-    dplyr::select(-c(min.depth,max.depth)) |>
-    dplyr::rename(Time = year,
-                  Value = Ndays,
-                  Var = temp.threshold,
-                  Source = source,
-                  Latitude = latitude,
-                  Longitude = longitude) |>
-    dplyr::relocate(Time,EPU,Depth,Var,Value,Latitude,Longitude,Source)
+  yt.combs = expand.grid(year = year, threshold = threshold)%>%
+    dplyr::mutate(threshold.char = gsub('\\.','_',threshold))
 
-  #thermal_habitat_gridded$Depth <- factor(thermal_habitat_gridded$Depth, levels = c('0-25m','25-100m','100-3000m'))
+  #Read EPU shp
+  epu.mask = terra::vect(here::here('data-raw','gis','EPU_NOESTUARIES.shp'))
 
+  #Read Bathy Shp
+  depth.mask = terra::rast(here::here('data-raw','gis','GLORYS_depth_NEUS.nc'))
+
+  #Get combinations of EPU and depth bin
+  depth.df = data.frame(depth.min = depth.bins[-length(depth.bins)],
+                        depth.max = depth.bins[-1])%>%
+    dplyr::mutate(depth.id = 1:n(),
+                  depth.name = paste0(depth.min,'-',depth.max,'m'))
+
+  epu.depth.combs = expand.grid(epu.name = epu.mask$EPU,
+                                depth.id = depth.df$depth.id)%>%
+    dplyr::left_join(depth.df)
+
+  df.ind = 1
+  output.ls = list()
+
+  #Loop through year x threshold combinations
+  for(i in 1:nrow(yt.combs)){
+
+    #Download from ERDDAP
+    file.url = URLencode(paste0('http://nefsctest.nmfs.local:8080/erddap/griddap/annual_thermal_habitat_glorys.nc?nday_',yt.combs$threshold.char[i],'%5B(',yt.combs$year[i],'-01-01):1:(',yt.combs$year[i],'-01-01)%5D%5B(47.0):1:(34.0)%5D%5B(-78.0):1:(-62.00000000000001)%5D'),repeated = F,reserved = T)
+    data.file.name = here::here('data-raw',paste0('thermal_habitat_gridded_',yt.combs$threshold.char[i],'_',yt.combs$year[i],'.nc'))
+    system.download = paste('curl --compressed -g "',file.url,'" -o ',data.file.name,sep = '')
+    system(system.download)
+
+    #Convert to raster object
+    comb.data = terra::rast(data.file.name)
+
+    #Loop through EPU x depth bins
+    for(j in 1:nrow(epu.depth.combs)){
+
+      # Create mask EPU x depth
+      this.epu = epu.mask[which(epu.mask$EPU == epu.depth.combs$epu.name[j])]
+      depth.range = terra::clamp(depth.mask,lower = epu.depth.combs$depth.min[j], upper = epu.depth.combs$depth.max[j], values = F)
+      depth.epu.rast = terra::crop(terra::mask(depth.range,this.epu),this.epu)
+
+      # convert to dataframe
+      output.ls[[df.ind]] = as.data.frame(depth.epu.rast,xy =T) %>%
+        dplyr::rename(Value = 'GLORYS_depth_NEUS',
+                      Longitude = 'x',
+                      Latitude = 'y')%>%
+        dplyr::mutate(Time = yt.combs$year[i],
+                      EPU = epu.depth.combs$epu.name[j],
+                      Depth = epu.depth.combs$depth.name[j],
+                      Var = yt.combs$threshold[i],
+                      Source = 'GLORYS',
+                      Units = 'Number of Days above threshold')%>%
+        dplyr::select(Time, EPU, Depth, Var, Value, Latitude, Longitude, Source, Units)
+
+      df.ind = df.ind +1
+    }
+
+    #Delete temp file
+    file.remove(data.file.name)
+
+  }
+
+  thermal_habitat_gridded = dplyr::bind_rows(output.ls)
 
   if (save_clean){
     usethis::use_data(thermal_habitat_gridded, overwrite = T)
@@ -28,7 +79,10 @@ get_thermal_habitat_gridded <- function(save_clean = F){
     return(thermal_habitat_gridded)
   }
 }
-get_thermal_habitat_gridded(save_clean = T)
+get_thermal_habitat_gridded(threshold = c(15,18.5,24),
+                            year = 2023,
+                            depth.bins = c(0,25,100,3000),
+                            save_clean = T)
 
 
 
