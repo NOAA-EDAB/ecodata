554a924ddf19cd3cc087a7cd31c9cf2582e8eaf1
andybeet <22455149+andybeet@users.noreply.github.com>
2024-12-11 14:20:53 -0500

    added benthic_index doc file and plot function

---
+++ R/benthic_index.R
@@ -0,0 +1,17 @@
+#' Benthic Index
+#'
+#' Data include Megabenthos biomass index from predator diet data from NEFSC bottom trawl survey.
+#'
+#'
+#' @format n rows and 5 columns.
+#'
+#' \itemize{
+#'     \item Value: Value of variable \code{Var}.
+#'     \item Var: Variable calculated.
+#'     \item Time: Year.
+#'     \item EPU: Ecological Production Unit (EPU) where sampling occurred.
+#'     \item Units: grams per stomach.
+#' }
+#'
+#' @details Methods used to calculate data are available at \url{https://noaa-edab.github.io/tech-doc/benthic_index.html}.
+"benthic_index"
