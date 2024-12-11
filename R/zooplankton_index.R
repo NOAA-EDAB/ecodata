550993158e184619ee97c3e0d0f01b719f66ef0f
andybeet <22455149+andybeet@users.noreply.github.com>
2024-12-11 14:21:16 -0500

    added zooplankton_index doc file and plot function

---
+++ R/zooplankton_index.R
@@ -0,0 +1,17 @@
+#' Forage Index
+#'
+#' Data include Calanus finmarchicus biomass index from predator diet data from NEFSC bottom trawl survey.
+#'
+#'
+#' @format 888 rows and 5 columns.
+#'
+#' \itemize{
+#'     \item Value: Value of variable \code{Var}.
+#'     \item Var: Variable calculated.
+#'     \item Time: Year.
+#'     \item EPU: Ecological Production Unit (EPU) where sampling occurred.
+#'     \item Units: grams per stomach.
+#' }
+#'
+#' @details Methods used to calculate data are available at \url{https://noaa-edab.github.io/tech-doc/zooplankton_index.html}.
+"zooplankton_index"
