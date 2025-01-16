bb8c87c00eea16a2cdd82f0a2183dad1ba63d753
Brandon Beltz - NOAA Affiliate <136381970+BBeltz1@users.noreply.github.com>
2025-01-16 13:06:42 -0500

    Add mom6 cold pool to existing cold_pool dataset

--- data-raw/get_cold_pool.R
+++ data-raw/get_cold_pool.R
@@ -6,7 +6,7 @@ library(tidyr)
 raw.dir <- here::here("data-raw")
 
 cold_pool_csv <- "cold_pool_indices_1959_2024 - Joseph Caracappa - NOAA Federal.csv" # "Glorys12v1_ColdPool_Extents.nc" #File from Zhoumin
-
+cold_pool_mom6_csv <- "cold_pool_m6.csv - Laura Gruenburg - NOAA Federal.csv"
 
 ### Cold pool index from Hubert du Pontavice
 
@@ -14,13 +14,19 @@ cold_pool_csv <- "cold_pool_indices_1959_2024 - Joseph Caracappa - NOAA Federal.
 ### Cold Pool time series
 get_cold_pool <- function(save_clean = F){
 
+    cold_pool_mom6 <- read.csv(file.path(raw.dir, cold_pool_mom6_csv)) %>%
+      dplyr::select(-c("X", "Unit")) %>%
+      dplyr::mutate(Var = "cold_pool_index", EPU = "MAB")
+
     cold_pool <- read.csv(file.path(raw.dir, cold_pool_csv))  %>%
     tidyr::pivot_longer(cols = c("cold_pool_index" ,     "se_cold_pool_index",
                                  "persistence_index"  ,  "se_persistence_index",
                                  "extent_index",         "se_extent_index" ),
                         names_to = "Var",values_to = "Value") %>%
     dplyr::mutate(EPU = c("MAB")) %>%
-    dplyr::rename(Time = year)
+    dplyr::rename(Time = year, Source = source)
+
+    cold_pool <- rbind(cold_pool, cold_pool_mom6)
 
 
    # metadata ----
