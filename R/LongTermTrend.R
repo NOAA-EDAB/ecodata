f6b7a566b08bf33f4c4b33676a4dd5ba964d8abf
andybeet <22455149+andybeet@users.noreply.github.com>
2025-01-24 15:30:36 -0500

    removed reference to quadratic fits

--- R/LongTermTrend.R
+++ R/LongTermTrend.R
@@ -137,60 +137,46 @@ StatGLS <- ggplot2::ggproto("StatGLS",
                               }
 
                               # Polynomial model with normal error
-                              data$x2 <- data$x^2
-                              poly_norm <- nlme::gls(y ~ x + x2, data = data, na.action = na.omit)
-
-                              # Polynomial model with AR1 error
-                              poly_ar1 <-
-                                try(nlme::gls(y ~ x + x2,
-                                              data = data,
-                                              correlation = nlme::corAR1(form = ~x),
-                                              na.action = na.omit))
-                              if (class(poly_ar1) == "try-error"){
-                                return(best_lm <- data.frame(model = NA,
-                                                             aicc  = NA,
-                                                             coefs..Intercept = NA,
-                                                             coefs.time = NA,
-                                                             coefs.time2 = NA,
-                                                             pval = NA))
-                              }
+                              # data$x2 <- data$x^2
+                              # poly_norm <- nlme::gls(y ~ x + x2, data = data, na.action = na.omit)
+                              #
+                              # # Polynomial model with AR1 error
+                              # poly_ar1 <-
+                              #   try(nlme::gls(y ~ x + x2,
+                              #                 data = data,
+                              #                 correlation = nlme::corAR1(form = ~x),
+                              #                 na.action = na.omit))
+                              # if (class(poly_ar1) == "try-error"){
+                              #   return(best_lm <- data.frame(model = NA,
+                              #                                aicc  = NA,
+                              #                                coefs..Intercept = NA,
+                              #                                coefs.time = NA,
+                              #                                coefs.time2 = NA,
+                              #                                pval = NA))
+                              # }
 
 
 
                               # Calculate AICs for all models
                               df_aicc <-
-                                data.frame(model = c("poly_norm",
-                                                     "poly_ar1",
-                                                     "linear_norm",
+                                data.frame(model = c("linear_norm",
                                                      "linear_ar1"),
-                                           aicc  = c(AICcmodavg::AICc(poly_norm),
-                                                     AICcmodavg::AICc(poly_ar1),
-                                                     AICcmodavg::AICc(linear_norm),
+                                           aicc  = c(AICcmodavg::AICc(linear_norm),
                                                      AICcmodavg::AICc(linear_ar1)),
-                                           coefs = rbind(coef(poly_norm),
-                                                         coef(poly_ar1),
-                                                         c(coef(linear_norm), NA),
+                                           coefs = rbind(c(coef(linear_norm), NA),
                                                          c(coef(linear_ar1),  NA)),
-                                           # Calculate overall signifiance (need to use
+                                           # Calculate overall significance (need to use
                                            # ML not REML for this)
                                            pval = c(anova(update(constant_norm, method = "ML"),
-                                                         update(poly_norm, method = "ML"))$`p-value`[2],
-                                                   anova(update(constant_ar1, method = "ML"),
-                                                         update(poly_ar1, method = "ML"))$`p-value`[2],
-                                                   anova(update(constant_norm, method = "ML"),
                                                          update(linear_norm, method = "ML"))$`p-value`[2],
                                                    anova(update(constant_ar1, method = "ML"),
                                                          update(linear_ar1, method = "ML"))$`p-value`[2]))
 
                               best_lm <- df_aicc %>%
-                                dplyr::filter(grepl("linear",model)) %>% #Select only linear models (removal of poly fit)
+                                #dplyr::filter(grepl("linear",model)) %>% #Select only linear models (removal of poly fit)
                                 dplyr::filter(aicc == min(aicc)) #Select model with lowest AICc
 
-                              if (best_lm$model == "poly_norm") {
-                                model <- poly_norm
-                              } else if (best_lm$model == "poly_ar1") {
-                                model <- poly_ar1
-                              } else if (best_lm$model == "linear_norm") {
+                              if (best_lm$model == "linear_norm") {
                                 model <- linear_norm
                               } else if (best_lm$model == "linear_ar1") {
                                 model <- linear_ar1
