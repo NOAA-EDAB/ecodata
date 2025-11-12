28bc42c8791d17816f84c83e2378d6d61a529ed9
Brandon Beltz - NOAA Affiliate <136381970+BBeltz1@users.noreply.github.com>
2025-11-12 13:39:21 -0500

    refactor(exp_n): refactor get exp_n function for compatibility with workflow outputs

--- data-raw/get_exp_n.R
+++ data-raw/get_exp_n.R
@@ -1,46 +1,13 @@
-
-
 library(dplyr)
 library(stringr)
 library(tidyr)
 
 raw.dir <- here::here("data-raw")
-exp_n_rdata <- "Expected_number_of_species_25.RData"
+exp_n_Rds <- "exp_n.rds"
+
 get_exp_n <- function(save_clean = F){
 
-  load(file.path(raw.dir, exp_n_rdata))
-
-
-
-  exp_n2<-exp_n %>% filter(str_detect(Var,  'Species - Bigelow|Species - Albatross')) %>%
-    tidyr::separate(Var, c("Season", "EPU", "trash1","trash2", "trash3", "trash4", "Var")) %>%
-    dplyr::select(-trash1, -trash2, -trash3, -trash4)
-
-  exp_n<-exp_n %>% filter(str_detect(Var,  'Standard Deviation')) %>%
-    tidyr::separate(Var, c("Season", "EPU", "trash1", "trash2", "trash3","trash4",
-                           "trash5", "trash6","Var")) %>%
-    dplyr::mutate(Var=recode(Var,
-                             `Albatross`="AlbatrossSD",
-                             `Bigelow`="BigelowSD")) %>%
-    dplyr::select(-trash1, -trash2, -trash3, -trash4, -trash5, -trash6) %>%
-    rbind(exp_n2)%>%
-    tibble::as_tibble() %>%
-    dplyr::mutate(Var = paste0(Var, "-", Season)) %>%
-    dplyr::select(Time, Var, Value, EPU, Units)
-
-  exp_n<- exp_n
-
-  # metadata ----
-  attr(exp_n, "tech-doc_url") <- "https://noaa-edab.github.io/tech-doc/expected-number-of-species.html"
-  attr(exp_n, "data_files")   <- list(
-    exp_n_rdata = exp_n_rdata)
-  attr(exp_n, "data_steward") <- c(
-    "Sean Lucey <sean.lucey@noaa.gov>")
-  attr(exp_n, "plot_script") <- list(
-    `mf_MAB` = "macrofauna_MAB.Rmd-exp-n.R",
-    `mf_MAB_spring` = "macrofauna_MAB.Rmd-exp-n-spring.R",
-    `mf_NE` = "macrofauna_NE.Rmd-exp-n.R",
-    `mf_NE_spring` = "macrofauna_NE.Rmd-exp-n-spring.R")
+  exp_n <- readRDS(file.path(raw.dir, exp_n_Rds))
 
   if (save_clean){
     usethis::use_data(exp_n, overwrite = T)
