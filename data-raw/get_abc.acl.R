5c17e70a2ffd403df1e08342d5d9b7711fa32c10
Brandon Beltz - NOAA Affiliate <136381970+BBeltz1@users.noreply.github.com>
2025-11-12 11:43:22 -0500

    update(abc_acl): update dataset with most recent submission, remove old metadata from function

--- data-raw/get_abc.acl.R
+++ data-raw/get_abc.acl.R
@@ -7,7 +7,7 @@ library(readr)
 
 raw.dir <- here::here("data-raw")
 
-abc_acl_xlsx <- "MAFMC_ABC_ACL_catch through 2023 - Brandon Muffley - NOAA Affiliate.xlsx"
+abc_acl_xlsx <- "MAFMC_ABC_ACL_catch through 2024 - Brandon Muffley - NOAA Affiliate.xlsx"
 NEFMC_abc_acl_xlsx <- "NEFMC_abc_acl.xlsx"
 
 get_abc_acl <- function(save_clean = F){
@@ -19,14 +19,15 @@ get_abc_acl <- function(save_clean = F){
                                  "ABC or ACL 2013", "Catch 2013",
                                  "ABC or ACL 2014", "Catch 2014",
                                  "ABC or ACL 2015", "Catch 2015" ,
-                                 "ABC or ACL 2016",   "Catch 2016",
+                                 "ABC or ACL 2016", "Catch 2016",
                                  "ABC or ACL 2017", "Catch 2017",
-                                 "ABC or ACL 2018",   "Catch 2018",
-                                 "ABC or ACL 2019","Catch 2019",
-                                 "ABC or ACL 2020","Catch 2020",
-                                 "ABC or ACL 2021","Catch 2021",
-                                 "ABC or ACL 2022","Catch 2022",
-                                 "ABC or ACL 2023","Catch 2023"),
+                                 "ABC or ACL 2018", "Catch 2018",
+                                 "ABC or ACL 2019", "Catch 2019",
+                                 "ABC or ACL 2020", "Catch 2020",
+                                 "ABC or ACL 2021", "Catch 2021",
+                                 "ABC or ACL 2022", "Catch 2022",
+                                 "ABC or ACL 2023", "Catch 2023",
+                                 "ABC or ACL 2024", "Catch 2024"),
                         names_to = "Var", values_to = "Value") %>%
     #tidyr::separate(Var, c("Var", "Time"), sep = " ") %>%
     dplyr::mutate(#Var = paste(spec,"-", Var),
@@ -47,7 +48,6 @@ get_abc_acl <- function(save_clean = F){
                   EPU = c("MAB")) %>%
     dplyr::select(Time, Var, Value, EPU, Units)
 
-
   ne<- readxl::read_excel(file.path(raw.dir, NEFMC_abc_acl_xlsx), sheet = 3) %>%
     dplyr::mutate(Catch = as.numeric(Catch),
                   ABC = as.numeric(ABC),
@@ -60,19 +60,8 @@ get_abc_acl <- function(save_clean = F){
                   EPU = c("NE")) %>%
     dplyr::select(Time, Var, Value, EPU, Units)
 
-
   abc_acl <- ma %>% rbind(ma, ne) %>%
     tibble::as_tibble()
-
-
-  # metadata ----
-  attr(abc_acl, "tech-doc_url") <- "https://noaa-edab.github.io/tech-doc/mafmc-abcacl-and-catch.html"
-  attr(abc_acl, "data_files")   <- list(
-    abc_acl_xlsx = abc_acl_xlsx)
-  attr(abc_acl, "data_steward") <- c(
-    "Brandon Muffley <bmuffley@mafmc.org>")
-  attr(abc_acl, "plot_script") <- list(
-    `mf_MAB` = "human_dimensions_MAB.Rmd-abc-acl.R")
 
   if (save_clean){
     usethis::use_data(abc_acl, overwrite = T)
@@ -81,44 +70,3 @@ get_abc_acl <- function(save_clean = F){
   }
 }
 get_abc_acl(save_clean = T)
-
-# groundfish<- abc_acl %>% filter(EPU == "NE") %>%
-#   tidyr::separate(col = Var, into = c("Fishery", "Var"), sep = "-") %>%
-#   tidyr::pivot_wider(names_from = "Var", values_from = "Value") %>%
-#   filter(stringr::str_detect(Fishery, "Groundfish")) %>%
-#   dplyr::group_by(Time) %>%
-#   dplyr::summarise(Quota, Catch_Landings) %>%
-#   dplyr::mutate(Fishery = "Groundfish",
-#                 EPU = "NE",
-#                 Units = "mt")
-#
-# sm_mesh<- abc_acl %>% filter(EPU == "NE") %>%
-#   tidyr::separate(col = Var, into = c("Fishery", "Var"), sep = "-") %>%
-#   tidyr::pivot_wider(names_from = "Var", values_from = "Value") %>%
-#   filter(stringr::str_detect(Fishery, "Small Mesh")) %>%
-#   dplyr::group_by(Time) %>%
-#   dplyr::summarise(Quota, Catch_Landings) %>%
-#   dplyr::mutate(Fishery = "Small Mesh",
-#                 EPU = "NE",
-#                 Units = "mt")
-#
-# abc_acl %>% filter(EPU == "NE") %>%
-#   tidyr::separate(col = Var, into = c("Fishery", "Var"), sep = "-") %>%
-#   tidyr::pivot_wider(names_from = "Var", values_from = "Value") %>%
-#   dplyr::filter(!stringr::str_detect(Fishery, "Groundfish"),
-#                 !stringr::str_detect(Fishery, "Small Mesh")) %>%
-#   rbind(groundfish, sm_mesh) %>%
-#   dplyr::mutate(Value = Catch_Landings/Quota) %>%
-#   ggplot2::ggplot()+
-#   ggplot2::geom_bar(aes( y = Value, x = Time, fill = Fishery),
-#                     stat="identity", position = "stack" )+
-#   ggplot2::ggtitle("ABC or ACL for NEFMC Managed Species")+
-#   #ggplot2::ylab(expression("ABC/ACL"))+
-#   ggplot2::theme(legend.text = element_text(size = 10),
-#                  legend.key.height = unit(2, "mm"))+
-#   ggplot2::ylab("ABC or ACL")+
-#   ggplot2::xlab(element_blank())+
-#   ecodata::theme_ts()+
-#   ecodata::theme_title()
-
-
