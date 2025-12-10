09f35a4e2999a24ca85187d217cf3353e1d6a8d6
Brandon Beltz - NOAA Affiliate <136381970+BBeltz1@users.noreply.github.com>
2025-12-10 14:30:28 -0500

    update(wind_port): update dataset with most recent submission

--- data-raw/get_wind_port.R
+++ data-raw/get_wind_port.R
@@ -8,7 +8,7 @@ library(readr)
 raw.dir <- here::here("data-raw")
 
 #### Angela Silva
-wind_port_csv<-"wind_ej_output_2024-11-13.csv"
+wind_port_csv<-"wind_port_revenue_tyrell_2025-12-05 - Abigail Tyrell - NOAA Federal.csv"
 
 get_wind_port <- function(save_clean = F){
   df<- data.frame(State = c(" ME", " MA", " RI", " CT", " NY", " NJ", " MD", " VA", " NC"),
@@ -16,6 +16,7 @@ get_wind_port <- function(save_clean = F){
 
   # import data
   wind_port<-read.csv(file.path(raw.dir, wind_port_csv)) %>%
+    dplyr::filter(lease_status == "All leases") %>%
     tidyr::separate(PORT_STATE, into = c("City", "State"), sep = ",") %>%
     dplyr::mutate(City = dplyr::recode(City,"DAVISVILLE" = "NORTH KINGSTOWN"),
            City = dplyr::recode(City,"HAMPTON BAY" ="HAMPTON BAY/SHINNECOCK"),
@@ -27,22 +28,12 @@ get_wind_port <- function(save_clean = F){
            perc_MIN = perc_MIN*100,
            total_rev = 100 - perc_MIN - perc_MAX) %>%
     dplyr::rename(Gentrification = Gent, MaxVal = WEA_MAX) %>%
-    dplyr::select(State, City, perc_MIN, perc_MAX, EJ, Gentrification, MaxVal) %>%
-    tidyr::pivot_longer(cols = c(perc_MIN, perc_MAX, EJ, Gentrification, MaxVal),
+    dplyr::select(State, City, perc_MIN, perc_MAX, Demographics, Gentrification, MaxVal) %>%
+    tidyr::pivot_longer(cols = c(perc_MIN, perc_MAX, Demographics, Gentrification, MaxVal),
                         names_to = "Var", values_to = "Value") %>%
     #tidyr::separate(City, into = c("City", "State"), sep = ",") %>%
     left_join(df, by = "State") %>%
     dplyr::arrange(State)
-
-
-  # metadata ----
-  attr(wind_port, "tech-doc_url") <- "https://noaa-edab.github.io/tech-doc/wea-fishing-port-landings.html"
-  attr(wind_port, "data_files")   <- list(
-    wind_port_csv = wind_port_csv)
-  attr(wind_port, "data_steward") <- c(
-    "Angela Silva <angela.silva@noaa.gov>")
-  attr(wind_port, "plot_script") <- list(
-    `hd_MAB` = "human_dimensions_MAB.Rmd-wea-port-rev.R")
 
   if (save_clean){
     usethis::use_data(wind_port, overwrite = T)
