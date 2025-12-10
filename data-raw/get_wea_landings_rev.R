70dbde5f6b47219477035d10b6478732e23d8217
Brandon Beltz - NOAA Affiliate <136381970+BBeltz1@users.noreply.github.com>
2025-12-10 12:55:36 -0500

    update(wea_landings_rev): update dataset with most recent submission

--- data-raw/get_wea_landings_rev.R
+++ data-raw/get_wea_landings_rev.R
@@ -8,8 +8,7 @@ library(readr)
 raw.dir <- here::here("data-raw")
 
 ## Doug Christel
-#wind_xlsx<-"CHRISTEL_2022 State of the Ecosystem Report_Max WEA Species Landings and Revenue.xlsx"
-wind_rev_xlsx<-"WEA_landings_revenue_2008-2023_SOE 2025 - Douglas Christel - NOAA Federal.xlsx"
+wind_rev_xlsx<-"Active OSW Projects_Landings-Revenues_SOE 2026_Christel - Douglas Christel - NOAA Federal.xlsx"
 get_wea_landings_rev <- function(save_clean = F){
   # import data
    wea_landings_rev<-readxl::read_excel(file.path(raw.dir,wind_rev_xlsx),
@@ -43,17 +42,6 @@ get_wea_landings_rev <- function(save_clean = F){
     wea_landings_rev <- wea_landings_rev %>%
       dplyr::mutate(Council = replace(Council, wea_landings_rev$`NEFMC, MAFMC, and ASMFC Managed Species` == "Monkfish", "MAFMC/NEFMC")) %>%
       dplyr::mutate(Council = replace(Council, wea_landings_rev$`NEFMC, MAFMC, and ASMFC Managed Species` == "Spiny Dogfish", "MAFMC/NEFMC"))
-
-  # metadata ----
-  attr(wea_landings_rev, "tech-doc_url") <- "https://noaa-edab.github.io/tech-doc/fisheries-revenue-in-wind-development-areas.html"
-  attr(wea_landings_rev, "data_files")   <- list(
-    wind_rev_xlsx = wind_rev_xlsx)
-  attr(wea_landings_rev, "data_steward") <- c(
-    "Doug Christel <douglas.christel@noaa.gov>")
-  attr(wea_landings_rev, "plot_script") <- list(
-    `hd_MAB_wea_rev` = "human_dimensions_MAB.Rmd-wea-landings-rev.R",
-    `hd_MAB_wea_spp_rev` = "human_dimensions_MAB.Rmd-wea-spp-rev.R")
-
 
   if (save_clean){
     usethis::use_data(wea_landings_rev, overwrite = T)
