1f2bf5ad464ddc39dfe2322ffdb8d103316b3240
Brandon Beltz - NOAA Affiliate <136381970+BBeltz1@users.noreply.github.com>
2025-12-10 10:48:28 -0500

    update(long_term_sst): update dataset with most recent submission

--- data-raw/get_long_term_sst.R
+++ data-raw/get_long_term_sst.R
@@ -14,13 +14,6 @@ get_long_term_sst <- function(save_clean = F){
            Units = "degreesC") %>%
     tibble::as_tibble()
 
-  # metadata ----
-  attr(long_term_sst, "tech-doc_url") <- "https://noaa-edab.github.io/tech-doc/long-term-sea-surface-temperature.html"
-  attr(long_term_sst, "data_steward") <- c(
-    "Kevin Friedland <kevin.friedland@noaa.gov>")
-  attr(long_term_sst, "plot_script") <- list(
-    `ltl_MAB` = "LTL_MAB.Rmd-long-term-sst.R")
-
   if (save_clean) {
     usethis::use_data(long_term_sst, overwrite = T)
   } else {
