cdce597d3f564b0ad5f04cb81a47232ddb70102f
Brandon Beltz - NOAA Affiliate <136381970+BBeltz1@users.noreply.github.com>
2025-12-10 14:10:41 -0500

    update(wind_revenue): update dataset with most recent submission

--- data-raw/get_wind_revenue.R
+++ data-raw/get_wind_revenue.R
@@ -1,47 +1,45 @@
 library(dplyr)
 library(tidyr)
 library(readxl)
+
 raw.dir <- here::here("data-raw")
-wind_rev_xlsx<- "WEA_landings_revenue_2008-2023_SOE 2025 - Douglas Christel - NOAA Federal.xlsx"
+wind_rev_xlsx<- "Active OSW Projects_Landings-Revenues_SOE 2026_Christel - Douglas Christel - NOAA Federal.xlsx"
 
 get_wind_revenue<- function(save_clean = F){
   ne_wind_revenue <- readxl::read_excel(file.path(raw.dir,wind_rev_xlsx),
-                                        sheet = "NEFMC Land-Revenue Figures") %>%
-    janitor::row_to_names(.,1) %>%
+                                        sheet = "NEFMC Land-Revenue") %>%
     dplyr::slice(-1) %>%
+    janitor::row_to_names(.,1) %>%
     dplyr::mutate(EPU = "NE")
 
-  ne_wind_revenue <- ne_wind_revenue[-c(294:407),] %>%
-    dplyr::mutate(Species = dplyr::recode(Species, "All Managed Skates" = "Skates"))
+  ne_wind_revenue <- ne_wind_revenue[-c(307:407),] %>%
+    dplyr::rename(Time = Year,
+                  sum_value = "Sum of Value (2024 dollars)",
+                  sum_landing = "Sum of Landings (pounds*)")
 
   mab_wind_revenue <- readxl::read_excel(file.path(raw.dir,wind_rev_xlsx),
-                                         sheet = "MAFMC Land-Revenue Figures") %>%
-    janitor::row_to_names(.,1) %>%
+                                         sheet = "MAFMC Land-Revenue") %>%
     dplyr::slice(-1) %>%
-    dplyr::mutate(EPU = "MAB")
-
-  mab_wind_revenue <- mab_wind_revenue[-241,] %>%
-    dplyr::mutate(Species = dplyr::recode(Species, "SCUP" = "Scup"))
-
-    #for (i in 2012:2016){
-    #  mab_wind_revenue <- mab_wind_revenue %>%
-    #  rbind(c("Ocean Quahog", i, NA, NA, NA, NA, NA, NA, NA, NA, "MAB"))
-    #}
-
-    #for (i in 2018:2022){
-    #  mab_wind_revenue <- mab_wind_revenue %>%
-    #  rbind(c("Ocean Quahog", i, NA, NA, NA, NA, NA, NA, NA, NA, "MAB"))
-    #}
+    janitor::row_to_names(.,1) %>%
+        dplyr::mutate(EPU = "MAB")
 
   mab_wind_revenue <- mab_wind_revenue %>%
     dplyr::group_by(Species) %>%
-    dplyr::arrange(Year, .by_group = T)
+    dplyr::arrange(Year, .by_group = T) %>%
+    dplyr::rename(Time = Year,
+                  sum_value = "Sum of 2024 Value ($)",
+                  sum_landing = "Sum of Landings (pounds*)")
 
   wind_revenue <- ne_wind_revenue %>%
     rbind(mab_wind_revenue) %>%
-       dplyr::rename(Time = Year,
-                 sum_value = "Revenue",
-                 sum_landing = "Landings") %>%
+    dplyr::mutate(Species = dplyr::recode(Species, "Atlantic Surfclam" = "Surfclam")) %>%
+    dplyr::mutate(Species = dplyr::recode(Species, "Atlantic Herring" = "Atlantic herring")) %>%
+    dplyr::mutate(Species = dplyr::recode(Species, "Atlantic Halibut" = "Atlantic halibut")) %>%
+    dplyr::mutate(Species = dplyr::recode(Species, "Atlantic Cod" = "Atlantic cod")) %>%
+    dplyr::mutate(Species = dplyr::recode(Species, "Witch Flounder" = "Witch flounder")) %>%
+    dplyr::mutate(Species = dplyr::recode(Species, "Winter Flounder" = "Winter flounder")) %>%
+    dplyr::mutate(Species = dplyr::recode(Species, "Windowpane Flounder" = "Windowpane flounder")) %>%
+    dplyr::mutate(Species = dplyr::recode(Species, "Yellowtail Flounder" = "Yellowtail flounder")) %>%
     tidyr::pivot_longer(cols = c(sum_landing, sum_value),
                         names_to = "Var", values_to = "Value") %>%
     dplyr::mutate(Var = paste0(Species, "-", Var)) %>%
@@ -51,16 +49,6 @@ get_wind_revenue<- function(save_clean = F){
     dplyr::mutate(Value = as.double(Value),
                   Time = as.integer(Time)) %>%
     dplyr::mutate(Value = dplyr::na_if(Value, 0))
-
-  # metadata ----
-  attr(wind_revenue, "tech-doc_url") <- "https://noaa-edab.github.io/tech-doc.html"
-  attr(wind_revenue, "data_files")   <- list(
-    wind_rev_xlsx = wind_rev_xlsx)
-  attr(wind_revenue, "data_steward") <- c(
-    "Geret DePiper <geret.depiper@noaa.gov>")
-  attr(wind_revenue, "plot_script") <- list(
-    `hd_MAB` = "human_dimensions_MAB.Rmd-wind-revenue.R",
-    `hd_NE` = "human_dimensions_NE.Rmd-wind-revenue.R")
 
   if (save_clean){
     usethis::use_data(wind_revenue, overwrite = TRUE)
