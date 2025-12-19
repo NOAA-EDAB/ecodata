b4a19faaba25da7f01f29ebffbc600e2fa9794f8
Joseph Caracappa (NOAA) <57966543+jcaracappa1@users.noreply.github.com>
2025-12-19 12:00:30 -0500

    Added zoo_community: Ryan's zooplankton PCA timeseries

---
+++ R/zoo_community.R
@@ -0,0 +1,20 @@
+#' Zooplankton Communities
+#'
+#' EcoMon Zooplankton Principal Components: Zooplankton community composition on the Northeast US Continental Shelf
+#'
+#'
+#' @format 284 rows and 5 columns.
+#'
+#' \itemize{
+#'     \item Var: Principal Components
+#'     \item Value: Value of variable \code{Var}.
+#'     \item Units: Units of variable \code{Var}.
+#'     \item EPU: Ecological Production Units where data originated.
+#'     \item Time: Year.
+#' }
+#'
+#' @details
+#' These data represent an ecosystem-level look at zooplankton community composition using the dimension reduction technique
+#' of principal component analysis.
+#'
+"zoo_diversity"
