69dc7a5ad6687645e5408510955f8d4f5efbe2ea
Brandon Beltz - NOAA Affiliate <136381970+BBeltz1@users.noreply.github.com>
2026-01-05 12:40:08 -0500

    refactor(bottom_temp_model_anom): remove MOM6 component from get function

--- data-raw/get_bottom_temp_model_anom.R
+++ data-raw/get_bottom_temp_model_anom.R
@@ -5,19 +5,19 @@ library(dplyr)
 library(tidyr)
 
 raw.dir <- here::here("data-raw")
-bt_csv <- "bottom_temp_anomaly_2025_V3.csv"
-bottom_temp_mom6_csv <- "BT_m6_gl_2024_soe.csv - Laura Gruenburg - NOAA Federal.csv"
+bt_input <- "GLORYS_bottom_temp_model_anom_1993_2025.rds"
+#bottom_temp_mom6_csv <- "BT_m6_gl_2024_soe.csv - Laura Gruenburg - NOAA Federal.csv"
 
 get_bottom_temp_model_anom <- function(save_clean = F){
 
   bottom_temp_model_anom <- read.csv(file.path(raw.dir,bt_csv))%>%
     dplyr::arrange(Source,Time,EPU,Var)
 
-  bottom_temp_mom6 <- read.csv(file.path(raw.dir, bottom_temp_mom6_csv)) |>
-    dplyr::select(Time, Var, Value, Units, EPU, Source) |>
-    dplyr::filter(Source == 'MOM6')
+  #bottom_temp_mom6 <- read.csv(file.path(raw.dir, bottom_temp_mom6_csv)) |>
+    #dplyr::select(Time, Var, Value, Units, EPU, Source) |>
+    #dplyr::filter(Source == 'MOM6')
 
-  bottom_temp_model_anom <- rbind(bottom_temp_model_anom, bottom_temp_mom6)
+  #bottom_temp_model_anom <- rbind(bottom_temp_model_anom, bottom_temp_mom6)
 
   if (save_clean){
     usethis::use_data(bottom_temp_model_anom, overwrite = T)
