2e083c58e0ba27e192d096b8407ee121d1172d08
andybeet <22455149+andybeet@users.noreply.github.com>
2026-01-08 14:49:01 -0500

    remove legacy hard coded date to replace with variable. Fixed bug in plotting start date

--- R/plot_exp_n.R
+++ R/plot_exp_n.R
@@ -14,14 +14,14 @@
 #' @export
 #'
 
-plot_exp_n <- function(shadedRegion = NULL,
-                              report="MidAtlantic",
-                       varName="fall",
-                       n = 0) {
-
+plot_exp_n <- function(
+  shadedRegion = NULL,
+  report = "MidAtlantic",
+  varName = "fall",
+  n = 0
+) {
   # generate plot setup list (same for all plot functions)
-  setup <- ecodata::plot_setup(shadedRegion = shadedRegion,
-                               report=report)
+  setup <- ecodata::plot_setup(shadedRegion = shadedRegion, report = report)
 
   # which report? this may be bypassed for some figures
   if (report == "MidAtlantic") {
@@ -32,28 +32,33 @@ plot_exp_n <- function(shadedRegion = NULL,
 
   season <- stringr::str_to_upper(varName)
 
-  if (season == "fall"){
+  if (season == "FALL") {
     start_year = 1965
   } else {
     start_year = 1968
   }
+  end_year <- max(ecodata::exp_n$Time)
 
   # optional code to wrangle ecodata object prior to plotting
   # e.g., calculate mean, max or other needed values to join below
-  exp<- ecodata::exp_n |>
+  exp <- ecodata::exp_n |>
     tidyr::separate(Var, into = c("Var", "Season"), sep = "-") |>
-    dplyr::filter( EPU %in% filterEPUs, Season == season,
-                   stringr::str_detect(Var, 'AlbatrossSD|BigelowSD')) |>
+    dplyr::filter(
+      EPU %in% filterEPUs,
+      Season == season,
+      stringr::str_detect(Var, 'AlbatrossSD|BigelowSD')
+    ) |>
     dplyr::rename(VarSD = Var, ValueSD = Value)
 
-  exp2<- ecodata::exp_n |>
+  exp2 <- ecodata::exp_n |>
     tidyr::separate(Var, into = c("Var", "Season"), sep = "-") |>
-    dplyr::filter(EPU %in% filterEPUs, Season == season,
-                  Var %in% c("Albatross", "Bigelow"))   |>
+    dplyr::filter(
+      EPU %in% filterEPUs,
+      Season == season,
+      Var %in% c("Albatross", "Bigelow")
+    ) |>
     dplyr::left_join(exp) |>
-    dplyr::mutate(upper = Value+ValueSD,
-                  lower = Value - ValueSD)
-
+    dplyr::mutate(upper = Value + ValueSD, lower = Value - ValueSD)
 
   # code for generating plot object p
   # ensure that setup list objects are called as setup$...
@@ -64,15 +69,23 @@ plot_exp_n <- function(shadedRegion = NULL,
     ggplot2::ggplot(ggplot2::aes(x = Time, y = Value, fill = Var)) +
 
     #Highlight last ten years
-    ggplot2::annotate("rect", fill = setup$shade.fill, alpha = setup$shade.alpha,
-                      xmin = setup$x.shade.min , xmax = setup$x.shade.max ,
-                      ymin = -Inf, ymax = Inf) +
+    ggplot2::annotate(
+      "rect",
+      fill = setup$shade.fill,
+      alpha = setup$shade.alpha,
+      xmin = setup$x.shade.min,
+      xmax = setup$x.shade.max,
+      ymin = -Inf,
+      ymax = Inf
+    ) +
 
-    ggplot2::geom_ribbon(data = exp2,
-                         ggplot2::aes(ymax = pmax(upper, 0), ymin = lower, x = Time),
-                         alpha = 0.5) +
-    ggplot2::geom_line(size = setup$lwd-0.5) +
-    ggplot2::geom_point(size = setup$pcex-0.5) +
+    ggplot2::geom_ribbon(
+      data = exp2,
+      ggplot2::aes(ymax = pmax(upper, 0), ymin = lower, x = Time),
+      alpha = 0.5
+    ) +
+    ggplot2::geom_line(size = setup$lwd - 0.5) +
+    ggplot2::geom_point(size = setup$pcex - 0.5) +
     #ecodata::geom_lm(aes(x = Time, y = Value, group = Var))+
     #ecodata::geom_lm()+
     # scale_color_manual(values = series.col, aesthetics = "color")+
@@ -82,83 +95,88 @@ plot_exp_n <- function(shadedRegion = NULL,
     #           size = hline.size,
     #           alpha = hline.alpha,
     #           linetype = hline.lty)+
-    ggplot2::facet_wrap(EPU~.,scales = "free_y", ncol = 2) +
-    ggplot2::ggtitle(paste("Expected Number of Species -", season))+
+    ggplot2::facet_wrap(EPU ~ ., scales = "free_y", ncol = 2) +
+    ggplot2::ggtitle(paste("Expected Number of Species -", season)) +
     ecodata::geom_gls() +
-    ecodata::geom_lm(n=n) +
+    ecodata::geom_lm(n = n) +
     #Axis and theme
-    ggplot2::scale_x_continuous(breaks = seq(start_year, 2018, by = 10), expand = c(0.01, 0.01)) +
+    ggplot2::scale_x_continuous(
+      breaks = seq(start_year, end_year, by = 10),
+      expand = c(0.01, 0.01)
+    ) +
     ggplot2::ylab("n species per 1000 ind") +
-    ggplot2::xlab(ggplot2::element_blank())+
-    ecodata::theme_facet()+
-    ggplot2::theme(strip.text= ggplot2::element_text(hjust=0),
-                   legend.title = ggplot2::element_blank())+
+    ggplot2::xlab(ggplot2::element_blank()) +
+    ecodata::theme_facet() +
+    ggplot2::theme(
+      strip.text = ggplot2::element_text(hjust = 0),
+      legend.title = ggplot2::element_blank()
+    ) +
     ecodata::theme_title()
 
-   # optional code for New England specific (2 panel) formatting
-    if (report == "NewEngland") {
-      p <- p +
-        ggplot2::theme(legend.position = "bottom",
-                       legend.title = ggplot2::element_blank())
+  # optional code for New England specific (2 panel) formatting
+  if (report == "NewEngland") {
+    p <- p +
+      ggplot2::theme(
+        legend.position = "bottom",
+        legend.title = ggplot2::element_blank()
+      )
+  }
 
-    }
-
-    return(p)
-
+  return(p)
 }
 
-attr(plot_exp_n,"varName") <- c("fall","spring")
-attr(plot_exp_n,"report") <- c("MidAtlantic","NewEngland")
+attr(plot_exp_n, "varName") <- c("fall", "spring")
+attr(plot_exp_n, "report") <- c("MidAtlantic", "NewEngland")
 
 #plot_exp_n(report = "MidAtlantic")
 #plot_exp_n(report = "NewEngland", varName = "fall")
 
-  # Paste commented original plot code chunk for reference
-  # exp<- ecodata::exp_n %>%
-  #   tidyr::separate(Var, into = c("Var", "Season"), sep = "-") %>%
-  #   dplyr::filter( EPU == "MAB", Season == "FALL",
-  #                  stringr::str_detect(Var, 'AlbatrossSD|BigelowSD')) %>%
-  #   dplyr::rename(VarSD = Var, ValueSD = Value)
-  #
-  # exp2<- ecodata::exp_n %>%
-  #   tidyr::separate(Var, into = c("Var", "Season"), sep = "-") %>%
-  #   dplyr::filter(EPU == "MAB", Season == "FALL",
-  #                 Var %in% c("Albatross", "Bigelow"))   %>%
-  #   dplyr::left_join(exp) %>%
-  #   dplyr::mutate(upper = Value+ValueSD,
-  #                 lower = Value - ValueSD)
-  #
-  # exp2 %>%
-  #   ggplot2::ggplot(aes(x = Time, y = Value, fill = Var)) +
-  #
-  #   #Highlight last ten years
-  #   ggplot2::annotate("rect", fill = shade.fill, alpha = shade.alpha,
-  #                     xmin = x.shade.min , xmax = x.shade.max ,
-  #                     ymin = -Inf, ymax = Inf) +
-  #
-  #   ggplot2::geom_ribbon(data = exp2, aes(ymax = pmax(upper, 0), ymin = lower, x = Time),
-  #                        alpha = 0.5) +
-  #   ggplot2::geom_line(size = lwd-0.5) +
-  #   ggplot2::geom_point(size = pcex-0.5) +
-  #   #ecodata::geom_lm(aes(x = Time, y = Value, group = Var))+
-  #   #ecodata::geom_lm()+
-  #   # scale_color_manual(values = series.col, aesthetics = "color")+
-  #   #ggplot2::guides(color = FALSE) +
-  #   #ggplot2::geom_hline(aes(yintercept = hline,
-  #   #               group = Var),
-  #   #           size = hline.size,
-  #   #           alpha = hline.alpha,
-  #   #           linetype = hline.lty)+
-  #   ggplot2::facet_wrap(EPU~.,scales = "free_y", ncol = 2) +
-  #   ggplot2::ggtitle("Expected Number of Species - Fall")+
-  #   #ecodata::geom_gls() +
-  #   #Axis and theme
-  #   ggplot2::scale_x_continuous(breaks = seq(1965, 2015, by = 10), expand = c(0.01, 0.01)) +
-  #   ggplot2::ylab("n species per 1000 ind") +
-  #   ggplot2::xlab(element_blank())+
-  #   ecodata::theme_facet()+
-  #   ggplot2::theme(strip.text=element_text(hjust=0),
-  #                  legend.title = element_blank())+
-  #   ecodata::theme_title()
-  #
-  #
+# Paste commented original plot code chunk for reference
+# exp<- ecodata::exp_n %>%
+#   tidyr::separate(Var, into = c("Var", "Season"), sep = "-") %>%
+#   dplyr::filter( EPU == "MAB", Season == "FALL",
+#                  stringr::str_detect(Var, 'AlbatrossSD|BigelowSD')) %>%
+#   dplyr::rename(VarSD = Var, ValueSD = Value)
+#
+# exp2<- ecodata::exp_n %>%
+#   tidyr::separate(Var, into = c("Var", "Season"), sep = "-") %>%
+#   dplyr::filter(EPU == "MAB", Season == "FALL",
+#                 Var %in% c("Albatross", "Bigelow"))   %>%
+#   dplyr::left_join(exp) %>%
+#   dplyr::mutate(upper = Value+ValueSD,
+#                 lower = Value - ValueSD)
+#
+# exp2 %>%
+#   ggplot2::ggplot(aes(x = Time, y = Value, fill = Var)) +
+#
+#   #Highlight last ten years
+#   ggplot2::annotate("rect", fill = shade.fill, alpha = shade.alpha,
+#                     xmin = x.shade.min , xmax = x.shade.max ,
+#                     ymin = -Inf, ymax = Inf) +
+#
+#   ggplot2::geom_ribbon(data = exp2, aes(ymax = pmax(upper, 0), ymin = lower, x = Time),
+#                        alpha = 0.5) +
+#   ggplot2::geom_line(size = lwd-0.5) +
+#   ggplot2::geom_point(size = pcex-0.5) +
+#   #ecodata::geom_lm(aes(x = Time, y = Value, group = Var))+
+#   #ecodata::geom_lm()+
+#   # scale_color_manual(values = series.col, aesthetics = "color")+
+#   #ggplot2::guides(color = FALSE) +
+#   #ggplot2::geom_hline(aes(yintercept = hline,
+#   #               group = Var),
+#   #           size = hline.size,
+#   #           alpha = hline.alpha,
+#   #           linetype = hline.lty)+
+#   ggplot2::facet_wrap(EPU~.,scales = "free_y", ncol = 2) +
+#   ggplot2::ggtitle("Expected Number of Species - Fall")+
+#   #ecodata::geom_gls() +
+#   #Axis and theme
+#   ggplot2::scale_x_continuous(breaks = seq(1965, 2015, by = 10), expand = c(0.01, 0.01)) +
+#   ggplot2::ylab("n species per 1000 ind") +
+#   ggplot2::xlab(element_blank())+
+#   ecodata::theme_facet()+
+#   ggplot2::theme(strip.text=element_text(hjust=0),
+#                  legend.title = element_blank())+
+#   ecodata::theme_title()
+#
+#
