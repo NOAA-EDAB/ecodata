19c21a1214ad10c4f40de84942b735f09c90b4a1
Joseph Caracappa (NOAA) <57966543+jcaracappa1@users.noreply.github.com>
2026-01-09 16:19:28 -0500

    plot function

--- R/plot_engagement.R
+++ R/plot_engagement.R
@@ -50,24 +50,73 @@ plot_engagement <- function(shadedRegion = NULL,
       # tidyr::pivot_wider(names_from = Var, values_from = Value) |>
       dplyr::filter(EPU == filterEPUs)
 
+    eng.ts = eng |>
+      dplyr::filter(Var == 'fishing_mean_score') |>
+      dplyr::mutate(label = if_else(Time == max(Time), as.character(Town), NA_character_))
 
+    topEng <- eng |>
+      dplyr::filter(Time == max(Time)) |>
+      dplyr::arrange(desc(Value)) |>
+      head(n=10) |>
+      dplyr::pull(Town)
+
+    eng.ts = eng.ts |>
+      dplyr::filter(Town %in% topEng)
+
+
+    p = ggplot2::ggplot(data = eng.ts, ggplot2::aes(x=Time, y=Value, color=Town)) +
+      ggplot2::geom_point(size=3, alpha = 0.9)+
+      ggplot2::geom_path(linewidth=0.2)+
+      ggplot2::ylab("Port Commercial Fishing Activity Indicator score")+
+      ggplot2::xlab("Year")+
+      ggplot2::theme_bw()+
+      ggplot2::theme(panel.grid.major = ggplot2::element_blank(),
+            panel.grid.minor = ggplot2::element_line(color = "#8ccde3",
+                                            size = 0.2,
+                                            linetype = 2))+
+      ggplot2::scale_y_continuous(limits = c(0, 1)) +
+      ggplot2::scale_x_continuous(expand = ggplot2::expansion(mult = c(0.1, .2)),
+                         breaks = c(2007, 2010, 2015, 2020,2024))+
+      ggplot2::scale_alpha_discrete(range = c(0.15, 0.9)) +
+      ggplot2::scale_color_brewer(palette = "Paired")+
+      ggplot2::theme(legend.position = "none")+
+      ggrepel::geom_label_repel(ggplot2::aes(label = label),hjust=0,
+                       nudge_x = 1, xlim=c(2025,2028),
+                       na.rm = TRUE, max.overlaps = Inf)+
+      ggplot2::ggtitle(paste(setup$region, "Port Activity in Top", varName, "Fishing Communities"))
+
+    # code for generating table of community social vulnerabilities
+    t <- eng |>
+      dplyr::filter(tolower(Town) %in% tolower(topEng) & grepl('*_rank',Var) & !is.na(Value) & Var %in% indgroup) |>
+
+      dplyr::mutate(dplyr::across(dplyr::ends_with('_rank'), ~
+                                    dplyr::case_when(
+                                      . ==  1~ "low",
+                                      . == 2 ~ "med",
+                                      . == 3 ~ "med high",
+                                      . == 4 ~ "high",
+                                      TRUE ~ NA_character_
+                                    ))) |>
+      tidyr::pivot_wider(names_from = Var, values_from = Value) |>
+      dplyr::select(Community = Town,
+                    dplyr::ends_with('_rank'))
 
   }
 
   if(varName == "Recreational"){
     eng<-ecodata::engagement |>
-      # dplyr::select(-Units) |>
+      dplyr::filter(!is.na(Value)) |>
+      dplyr::select(-Units) |>
       #dplyr::distinct(Time, Var,  EPU, Units, .keep_all = T) |> #hack, remove later
       tidyr::separate(Var, into = c("Town", "StateVar"), sep = ",") |>
       tidyr::separate(StateVar, into = c("State", "Var"), sep = "-") |>
       tidyr::unite("Town", c(Town, State), sep = ",") |>
       tidyr::pivot_wider(names_from = Var, values_from = Value) |>
-      dplyr::filter(EPU == filterEPUs,
-                    !is.na(RecEng)) |>
+      dplyr::filter(EPU == filterEPUs) |>
       dplyr::select(-dplyr::starts_with("Com"))
 
 
-    eng <- eng |>
+    eng.plot <- eng |>
       dplyr::filter(is.finite(RecEng)) |>
       dplyr::filter(Time == max(Time)) |>
       dplyr::select(Time, EPU, Town,
@@ -78,20 +127,21 @@ plot_engagement <- function(shadedRegion = NULL,
                     dplyr::all_of(indgroup))
 
     # find the top ~20 communities for highlighting and plotting, using top 12 Eng and Rel
-    topEng <- eng |>
+    topEng <- eng.plot |>
       dplyr::select(Town, Eng) |>
       dplyr::arrange(desc(Eng)) |>
       head(n=12) |>
       dplyr::select(Town)
 
-    topRel <- eng |>
+    topRel <- eng.plot |>
       dplyr::select(Town, Rel) |>
       dplyr::arrange(desc(Rel)) |>
       head(n=12) |>
       dplyr::select(Town)
 
     topTowns <- dplyr::bind_rows(topEng, topRel) |>
-      dplyr::distinct()
+      dplyr::distinct() |>
+      dplyr::pull(Town)
 
     # raw scores not currently used but here for later
     # Social <- c("personal_disruption", "pop_composition", "poverty")
@@ -104,7 +154,7 @@ plot_engagement <- function(shadedRegion = NULL,
     # xmin = setup$x.shade.min , xmax = setup$x.shade.max
     #
 
-    p<-eng |>
+    p<-eng.plot |>
       ggplot2::ggplot(ggplot2::aes(x=Eng, y=Rel)) +
       ggplot2::geom_point(shape=1,
                           size=1.4, stroke=0.9, alpha=0.6,
@@ -121,7 +171,7 @@ plot_engagement <- function(shadedRegion = NULL,
       ggplot2::ylab(expression(paste("Population Relative Engagement Index", italic(" (factor score)"))))+
       ggplot2::xlab(expression(paste("Engagement Index", italic(" (factor score)"))))+
       ggplot2::guides(color=ggplot2::guide_legend(title="Engagement Score Rank"))+
-      ggrepel::geom_label_repel(ggplot2::aes(label = ifelse(Town %in% c(unlist(topTowns)),as.character(Town),'')),
+      ggrepel::geom_label_repel(ggplot2::aes(label = ifelse(Town %in% c(topTowns),as.character(Town),'')),
                                 size=2,
                                 force=2.5,
                                 box.padding   = 0.5,
@@ -137,7 +187,8 @@ plot_engagement <- function(shadedRegion = NULL,
 
     # code for generating table of community social vulnerabilities
     t <- eng |>
-      dplyr::filter(Town %in% c(unlist(topTowns))) |>
+      dplyr::filter(tolower(Town) %in% tolower(topTowns)) |>
+
       dplyr::mutate(dplyr::across(dplyr::ends_with('_rank'), ~
                                     dplyr::case_when(
                                       . ==  1~ "low",
@@ -147,7 +198,7 @@ plot_engagement <- function(shadedRegion = NULL,
                                       TRUE ~ NA_character_
                                     ))) |>
       dplyr::select(Community = Town,
-                    dplyr::ends_with('_rank'))
+                    dplyr::all_of(indgroup))
 
   }
 
