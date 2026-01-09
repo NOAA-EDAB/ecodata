b5391d503f058776d53866c3d20345910cf7f9b4
Abby Tyrell <77738923+atyrell3@users.noreply.github.com>
2026-01-09 18:01:49 -0500

    refactor facet order

--- R/plot_spawn_timing.R
+++ R/plot_spawn_timing.R
@@ -17,14 +17,14 @@
 #' @export
 #'
 
-plot_spawn_timing <- function(shadedRegion = NULL,
-                              report="MidAtlantic",
-                              varName = "Resting",
-                              n = 0) {
-
+plot_spawn_timing <- function(
+  shadedRegion = NULL,
+  report = "MidAtlantic",
+  varName = "Resting",
+  n = 0
+) {
   # generate plot setup list (same for all plot functions)
-  setup <- ecodata::plot_setup(shadedRegion = shadedRegion,
-                               report=report)
+  setup <- ecodata::plot_setup(shadedRegion = shadedRegion, report = report)
 
   # which report? this may be bypassed for some figures
   # dataset has NA EPU field but may be added later, so keep this here
@@ -38,8 +38,12 @@ plot_spawn_timing <- function(shadedRegion = NULL,
 
   # optional code to wrangle ecodata object prior to plotting
   # e.g., calculate mean, max or other needed values to join below
-  fix<- ecodata::spawn_timing |>
-    tidyr::separate(Var, into = c("Season", "Species", "Stock", "Var"), sep = "_")
+  fix <- ecodata::spawn_timing |>
+    tidyr::separate(
+      Var,
+      into = c("Season", "Species", "Stock", "Var"),
+      sep = "_"
+    )
 
   # code for generating plot object p
   # ensure that setup list objects are called as setup$...
@@ -50,44 +54,86 @@ plot_spawn_timing <- function(shadedRegion = NULL,
   # SPRING data plots by default for spring spawning species
   # May have to change to add fall option later if we get fall spawners
 
-
   filt <- fix |>
-    dplyr::filter(Season == "SPRING",
-                  Var == varName) |>
-    dplyr::mutate(Species = factor(Species, levels = c("Yellowtail", "Haddock")),
-                  Stock = factor(Stock, levels = c("GOM", "CC", "GB", "SNE")))
+    dplyr::filter(Season == "SPRING", Var == varName) |>
+    dplyr::mutate(
+      Species = factor(Species, levels = c("Yellowtail", "Haddock")),
+      Stock = factor(Stock, levels = c("GOM", "CC", "GB", "SNE")),
+      plt_var = paste(Species, Stock)
+    ) |>
+    dplyr::mutate(
+      plt_var = factor(
+        plt_var,
+        levels = c(
+          "Yellowtail CC",
+          "Yellowtail GB",
+          "Yellowtail SNE",
+          "Haddock GOM",
+          "Haddock GB"
+        )
+      )
+    )
 
   p <- ggplot2::ggplot(filt, ggplot2::aes(x = Time, y = Value)) +
-    ggplot2::annotate("rect", fill = setup$shade.fill, alpha = setup$shade.alpha,
-                      xmin = setup$x.shade.min , xmax = setup$x.shade.max,
-                      ymin = -Inf, ymax = Inf) +
+    ggplot2::annotate(
+      "rect",
+      fill = setup$shade.fill,
+      alpha = setup$shade.alpha,
+      xmin = setup$x.shade.min,
+      xmax = setup$x.shade.max,
+      ymin = -Inf,
+      ymax = Inf
+    ) +
     ggplot2::geom_point() +
     ggplot2::geom_line() +
     ecodata::geom_gls() +
-    ecodata::geom_lm(n=n)+
+    ecodata::geom_lm(n = n) +
     #ggplot2::geom_bar(stat="identity", position="fill") +
     #ggplot2::scale_fill_manual(values = mat.col) +
     ggplot2::ylab(filt$Units) +
-    ggplot2::facet_wrap(Species~Stock,
-                        labeller = ggplot2::label_wrap_gen(multi_line=FALSE)) +
+    ggplot2::facet_wrap(
+      ~plt_var,
+      dir = "v",
+      labeller = ggplot2::label_wrap_gen(multi_line = FALSE)
+    ) +
     ecodata::theme_facet() +
     ecodata::theme_title()
 
-  if(varName %in% c("Resting", "Ripe", "Spent", "Developing")){
-    p <- p + ggplot2::ggtitle(paste(stringr::str_to_sentence(filt$Season), varName, "Spawning Stage"))
+  if (varName %in% c("Resting", "Ripe", "Spent", "Developing")) {
+    p <- p +
+      ggplot2::ggtitle(paste(
+        stringr::str_to_sentence(filt$Season),
+        varName,
+        "Spawning Stage"
+      ))
   }
 
-  if(varName %in% c("MF", "meanTEMP", "meanJDAY")){
-    varLong <- dplyr::case_when(varName == "MF" ~ "Mature Females",
-                                varName == "meanTEMP" ~ "Mean Sampled Temperature",
-                                varName == "meanJDAY" ~ "Mean Sampling Date")
+  if (varName %in% c("MF", "meanTEMP", "meanJDAY")) {
+    varLong <- dplyr::case_when(
+      varName == "MF" ~ "Mature Females",
+      varName == "meanTEMP" ~ "Mean Sampled Temperature",
+      varName == "meanJDAY" ~ "Mean Sampling Date"
+    )
 
-    p <-p + ggplot2::ggtitle(paste(stringr::str_to_sentence(filt$Season), varLong))
+    p <- p +
+      ggplot2::ggtitle(paste(stringr::str_to_sentence(filt$Season), varLong))
+  }
+
+  if (varName == "Resting") {
+    p <- p +
+      ggplot2::ylab("Percent of females that have spawned")
   }
 
   return(p)
-
 }
 
-attr(plot_spawn_timing,"report") <- c("MidAtlantic","NewEngland")
-attr(plot_spawn_timing, "varName") <- c("Resting", "Ripe", "Spent", "Developing", "MF", "meanTEMP", "meanJDAY")
+attr(plot_spawn_timing, "report") <- c("MidAtlantic", "NewEngland")
+attr(plot_spawn_timing, "varName") <- c(
+  "Resting",
+  "Ripe",
+  "Spent",
+  "Developing",
+  "MF",
+  "meanTEMP",
+  "meanJDAY"
+)
