7759de9d01a73f5fb2c01520315b315815a6821f
Brandon Beltz - NOAA Affiliate <136381970+BBeltz1@users.noreply.github.com>
2026-01-14 10:45:27 -0500

    update(wcr): update dataset with preliminary birth frequency

--- data-raw/get_warm_core_rings.R
+++ data-raw/get_warm_core_rings.R
@@ -7,6 +7,7 @@ library(readxl)
 
 raw.dir <- here::here("data-raw")
 wcr_xlsx <- "WCR.xlsx"
+
 get_warm_core_rings <- function(save_clean = F){
   wcr <- read_excel(file.path(raw.dir,wcr_xlsx)) %>%
     dplyr::select(Time, WCR) %>%
@@ -16,6 +17,7 @@ get_warm_core_rings <- function(save_clean = F){
     tibble::add_row(Time = 2022, Value = 29) %>% #add 2022 data point
     tibble::add_row(Time = 2023, Value = 18) %>% #add 2023 data point
     tibble::add_row(Time = 2024, Value = 23) %>% #add 2024 data point
+    tibble::add_row(Time = 2025, Value = 25) %>% #add 2025 data point
         dplyr::mutate(EPU = c("All"),
            Var = c("Warm Core Rings"),
            Units = c("n")) %>% tibble::as_tibble() %>%
@@ -26,11 +28,5 @@ get_warm_core_rings <- function(save_clean = F){
   } else {
     return(wcr)
   }
-  # metadata ----
-  attr(wcr, "tech-doc_url") <- "https://noaa-edab.github.io/tech-doc/warm-core-rings.html"
-  attr(wcr, "data_files")   <- list(
-    wcr_xlsx = wcr_xlsx)
-  attr(wcr, "data_steward") <- c(
-    "Avijit Gangopadhyay <avijit.gangopadhyay@umassd.edu>")
 }
 get_warm_core_rings(save_clean = T)
