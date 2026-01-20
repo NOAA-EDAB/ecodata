3171f084705b6bea90b97531475bfa94788cb012
Brandon Beltz - NOAA Affiliate <136381970+BBeltz1@users.noreply.github.com>
2026-01-20 17:15:04 -0500

    docs(plot_rec_hms): add comment to mark plot exclusion for future reference

--- R/plot_rec_hms.r
+++ R/plot_rec_hms.r
@@ -32,6 +32,7 @@ plot_rec_hms <- function(shadedRegion = NULL, report = "MidAtlantic", n = 0) {
     dplyr::mutate(
       fac = dplyr::case_when(Group == "Scombridae" ~ "Tuna", TRUE ~ "Other")
     ) |>
+    # Exclude billfishes and tuna from plot, per HMS request - January 2026
     dplyr::filter(Group %in% c("LargeCoastal","SmallCoastal","Prohibited"))
 
   fix <- fix |>
