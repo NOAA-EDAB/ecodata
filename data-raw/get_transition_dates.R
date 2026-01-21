0c58e3206d0db390fe8f79cba11d8488b9ce2131
Brandon Beltz - NOAA Affiliate <136381970+BBeltz1@users.noreply.github.com>
2026-01-21 16:09:51 -0500

    update(trans_dates): update dataset with full 2025 year from OISST

--- data-raw/get_transition_dates.R
+++ data-raw/get_transition_dates.R
@@ -2,11 +2,22 @@ library(dplyr)
 library(tidyr)
 
 raw.dir <- here::here("data-raw")
-trans_dates_csv <- "transdates_101225.csv"
+transdates_csv <- "trans_dates_2025.csv"
 
 get_transition_dates <- function(save_clean = F){
 
-  trans_dates <- read.csv(file.path(raw.dir, trans_dates_csv))
+  trans_dates<- read.csv(file.path(raw.dir,transdates_csv)) %>%
+    tidyr::pivot_longer( cols = c("sprtrans","falltrans","sumlen","maxday"),
+                         names_to = "Var", values_to = "Value") %>%
+    dplyr::filter(!EPU == "NA") %>%
+    dplyr::rename(Time = Year) %>%
+    dplyr::select(Time, Value, Var, EPU) %>%
+    dplyr::mutate(Time = as.numeric(Time),
+                  Value = as.numeric(Value)) %>%
+    dplyr::group_by(Time, EPU, Var) %>%
+    dplyr::summarise(Value = unique(Value)) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(EPU = recode(EPU,"GBK" = "GB"))
 
   if (save_clean){
     usethis::use_data(trans_dates, overwrite = T)
