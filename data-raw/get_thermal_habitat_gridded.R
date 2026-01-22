371fdfaba4b5049945f1431f52d0981240a015a2
Brandon Beltz - NOAA Affiliate <136381970+BBeltz1@users.noreply.github.com>
2026-01-22 17:34:22 -0500

    update(thermal_habitat_gridded): update dataset with most recent data file from contributor

--- data-raw/get_thermal_habitat_gridded.R
+++ data-raw/get_thermal_habitat_gridded.R
@@ -9,7 +9,7 @@
 get_thermal_habitat_gridded <- function(save_clean = F){
 
     # load(file.path(raw.dir, th_gridded_rda))
-    thermal_habitat_gridded <- read.csv(here::here('data-raw','thermal_habitat_gridded_2024_only_SOE2025.csv')) |>
+    thermal_habitat_gridded <- readRDS(here::here("data-raw/GLORYS_thermal_habitat_gridded_1993_2025.rds")) |>
     dplyr::relocate(Time,EPU,Depth,Var,Value,Latitude,Longitude,Source)
 
   #thermal_habitat_gridded$Depth <- factor(thermal_habitat_gridded$Depth, levels = c('0-25m','25-100m','100-3000m'))
