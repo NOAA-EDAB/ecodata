eaf6c010f5fb7fb9721e4e64c6347a25e0040aa7
Brandon Beltz - NOAA Affiliate <136381970+BBeltz1@users.noreply.github.com>
2026-02-05 15:48:05 -0500

    feature(plot_abc_acl): change color palettes to more distinct ones that can accommodate many data classes

--- R/plot_abc_acl.R
+++ R/plot_abc_acl.R
@@ -13,13 +13,13 @@
 #' @export
 #'
 
-plot_abc_acl <- function(shadedRegion = NULL,
-                         report="MidAtlantic",
-                         plottype = "Stacked") {
-
+plot_abc_acl <- function(
+  shadedRegion = NULL,
+  report = "MidAtlantic",
+  plottype = "Stacked"
+) {
   # generate plot setup list (same for all plot functions)
-  setup <- ecodata::plot_setup(shadedRegion = shadedRegion,
-                               report=report)
+  setup <- ecodata::plot_setup(shadedRegion = shadedRegion, report = report)
 
   # which report? this may be bypassed for some figures
   if (report == "MidAtlantic") {
@@ -38,22 +38,24 @@ plot_abc_acl <- function(shadedRegion = NULL,
   # splitting code by report for now
 
   if (report == "MidAtlantic") {
-
     ABCs <- ecodata::abc_acl |>
       dplyr::filter(EPU == filterEPUs) |>
       tidyr::separate(col = Var, into = c("Fishery", "Var"), sep = "_") |>
       dplyr::filter(Var == "Quota") |>
-      dplyr::mutate(Fishery = gsub("Commercial", "C", Fishery),
-                    Fishery = gsub("Recreational", "R", Fishery)) |>
+      dplyr::mutate(
+        Fishery = gsub("Commercial", "C", Fishery),
+        Fishery = gsub("Recreational", "R", Fishery)
+      ) |>
       dplyr::group_by(Fishery, Time) |>
-      dplyr::summarise(Value = sum(Value),
-                       .groups="drop")
+      dplyr::summarise(Value = sum(Value), .groups = "drop")
 
     # Determine species order for stacked bar plot based on first year
     stackedOrder <- ABCs |>
       dplyr::filter(Time == min(ABCs$Time)) |>
       dplyr::arrange(Value) |>
-      dplyr::mutate(Order = 1:nrow(stackedOrder)) |>
+      dplyr::mutate(
+        Order = 1:nrow(dplyr::filter(ABCs, Time == min(ABCs$Time)))
+      ) |>
       dplyr::select(Fishery, Order)
 
     # Join stacked bar plot subset with ordered species list based on first year
@@ -65,57 +67,61 @@ plot_abc_acl <- function(shadedRegion = NULL,
       dplyr::mutate(Fishery = factor(Fishery, levels = unique(Fishery)))
 
     CatchABC <- ecodata::abc_acl |>
-      unique()|>
+      unique() |>
       dplyr::filter(EPU == filterEPUs) |>
       tidyr::separate(col = Var, into = c("Fishery", "Var"), sep = "_") |>
-      tidyr::pivot_wider(names_from = Var, values_from = Value)  |>
+      tidyr::pivot_wider(names_from = Var, values_from = Value) |>
       #tidyr::separate(Catch, into = c("Catch", "X"), sep = ",") %>%
-      dplyr::mutate(Catch = as.numeric(stringr::str_extract(Catch, pattern = "\\d+")),
-                    Quota = as.numeric(stringr::str_extract(Quota, pattern = "\\d+")),
-                    Value = Catch/Quota#,
-                    #Time = as.character(Time)
+      dplyr::mutate(
+        Catch = as.numeric(stringr::str_extract(Catch, pattern = "\\d+")),
+        Quota = as.numeric(stringr::str_extract(Quota, pattern = "\\d+")),
+        Value = Catch / Quota #,
+        #Time = as.character(Time)
       ) |>
       dplyr::filter(!is.na(Value))
 
     meanCatchABC <- CatchABC |>
       dplyr::group_by(Time) |>
-      dplyr::summarise(val = mean(Value),
-                       .groups="drop") |>
+      dplyr::summarise(val = mean(Value), .groups = "drop") |>
       dplyr::ungroup() |>
       dplyr::mutate(Time = as.numeric(Time))
-
   }
 
   if (report == "NewEngland") {
-
     ABCs <- ecodata::abc_acl |>
       dplyr::filter(EPU == filterEPUs) |>
-      tidyr::separate(col = Var, into = c("FMP", "Fishery", "Var"), sep = "_") |>
+      tidyr::separate(
+        col = Var,
+        into = c("FMP", "Fishery", "Var"),
+        sep = "_"
+      ) |>
       dplyr::filter(Var == "ABC") |>
       dplyr::group_by(Fishery, Time) |>
-      dplyr::summarise(Value = sum(Value),
-                       .groups="drop")
+      dplyr::summarise(Value = sum(Value), .groups = "drop")
 
     CatchABC <- ecodata::abc_acl |>
-      unique()|>
+      unique() |>
       dplyr::filter(EPU == filterEPUs) |>
-      tidyr::separate(col = Var, into = c("FMP", "Fishery", "Var"), sep = "_") |>
-      tidyr::pivot_wider(names_from = Var, values_from = Value)  |>
+      tidyr::separate(
+        col = Var,
+        into = c("FMP", "Fishery", "Var"),
+        sep = "_"
+      ) |>
+      tidyr::pivot_wider(names_from = Var, values_from = Value) |>
       #tidyr::separate(Catch, into = c("Catch", "X"), sep = ",") %>%
-      dplyr::mutate(Catch = as.numeric(stringr::str_extract(Catch, pattern = "\\d+")),
-                    Quota = as.numeric(stringr::str_extract(ABC, pattern = "\\d+")),
-                    Value = Catch/ABC#,
-                    #Time = as.character(Time)
-                    ) |>
+      dplyr::mutate(
+        Catch = as.numeric(stringr::str_extract(Catch, pattern = "\\d+")),
+        Quota = as.numeric(stringr::str_extract(ABC, pattern = "\\d+")),
+        Value = Catch / ABC #,
+        #Time = as.character(Time)
+      ) |>
       dplyr::filter(!is.na(Value))
 
     meanCatchABC <- CatchABC |>
       dplyr::group_by(Time) |>
-      dplyr::summarise(val = mean(Value),
-                       .groups="drop") |>
+      dplyr::summarise(val = mean(Value), .groups = "drop") |>
       dplyr::ungroup() |>
       dplyr::mutate(Time = as.numeric(Time))
-
   }
 
   # code for generating plot object p
@@ -124,45 +130,63 @@ plot_abc_acl <- function(shadedRegion = NULL,
   # xmin = setup$x.shade.min , xmax = setup$x.shade.max
   #
   if (plottype == "Stacked") {
+    # Define a new palette that accommodates 18+ data classes
+    if (report == "MidAtlantic") {
+      customPalette <- rev(Polychrome::light.colors(19))
+    } else {
+      customPalette <- Polychrome::palette36.colors()
+    }
 
-    p <-  ABCs |>
-      ggplot2::ggplot()+
-      ggplot2::geom_bar(ggplot2::aes(y = Value, x = Time, fill = Fishery), stat="identity", position = "stack" )+
-      ggplot2::scale_x_continuous(breaks= scales::pretty_breaks()) +
-      ggplot2::ggtitle("ABC or ACL for Managed Species")+
-      ggplot2::theme(legend.text = ggplot2::element_text(size = 8),
-                     legend.key.height = ggplot2::unit(2, "mm"))+
-      ggplot2::ylab("ABC or ACL, metric tons")+
-      ggplot2::xlab(ggplot2::element_blank())+
-      ecodata::theme_ts()+
-      ggplot2::guides(fill=ggplot2::guide_legend(ncol=1))+
-      ecodata::theme_title()
+    p <- ABCs |>
+      ggplot2::ggplot() +
+      ggplot2::geom_bar(
+        ggplot2::aes(y = Value, x = Time, fill = Fishery),
+        stat = "identity",
+        position = "stack"
+      ) +
+      ggplot2::scale_x_continuous(breaks = scales::pretty_breaks()) +
+      ggplot2::ggtitle("ABC or ACL for Managed Species") +
+      ggplot2::theme(
+        legend.text = ggplot2::element_text(size = 8),
+        legend.key.height = ggplot2::unit(2, "mm")
+      ) +
+      ggplot2::ylab("ABC or ACL, metric tons") +
+      ggplot2::xlab(ggplot2::element_blank()) +
+      ecodata::theme_ts() +
+      ggplot2::guides(fill = ggplot2::guide_legend(ncol = 1)) +
+      ecodata::theme_title() +
+      ggplot2::scale_fill_manual(values = unname(customPalette[-16]))
 
     return(p)
-
   }
 
   if (plottype == "Catch") {
-
     p <- CatchABC |>
-      ggplot2::ggplot()+
+      ggplot2::ggplot() +
       #geom_boxplot()+
-      ggplot2::geom_point(ggplot2::aes(x = Time, y = Value))+
-      ggplot2::geom_point(data = meanCatchABC, ggplot2::aes(x = Time, y = val), color = "red")+
-      ggplot2::geom_line(data = meanCatchABC, ggplot2::aes(x = Time, y = val), color = "red")+
-      ggplot2::geom_hline(yintercept = 1, linetype='dashed', col = 'gray')+
-      ggplot2::scale_x_continuous(breaks= scales::pretty_breaks()) +
-      ggplot2::ggtitle("Catch per ABC or ACL")+
-      ggplot2::ylab(expression("Catch / ABC or ACL"))+
-      ggplot2::theme(legend.title = ggplot2::element_blank())+
-      ggplot2::xlab(ggplot2::element_blank())+
-      ecodata::theme_ts()+
+      ggplot2::geom_point(ggplot2::aes(x = Time, y = Value)) +
+      ggplot2::geom_point(
+        data = meanCatchABC,
+        ggplot2::aes(x = Time, y = val),
+        color = "red"
+      ) +
+      ggplot2::geom_line(
+        data = meanCatchABC,
+        ggplot2::aes(x = Time, y = val),
+        color = "red"
+      ) +
+      ggplot2::geom_hline(yintercept = 1, linetype = 'dashed', col = 'gray') +
+      ggplot2::scale_x_continuous(breaks = scales::pretty_breaks()) +
+      ggplot2::ggtitle("Catch per ABC or ACL") +
+      ggplot2::ylab(expression("Catch / ABC or ACL")) +
+      ggplot2::theme(legend.title = ggplot2::element_blank()) +
+      ggplot2::xlab(ggplot2::element_blank()) +
+      ecodata::theme_ts() +
       ecodata::theme_title()
 
     return(p)
   }
-
 }
 
-attr(plot_abc_acl,"report") <- c("MidAtlantic","NewEngland")
-attr(plot_abc_acl,"plottype") <- c("Stacked","Catch")
+attr(plot_abc_acl, "report") <- c("MidAtlantic", "NewEngland")
+attr(plot_abc_acl, "plottype") <- c("Stacked", "Catch")
