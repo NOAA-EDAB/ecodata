82412ac77cb3ab26cb8284d0d372ba804a5c68c1
Maxwell Grezlik <maxwell.grezlik@noaa.gov>
2026-03-16 16:49:02 -0400

    Added argument to remove Tilefish in get_productivity_anomaly.R. Created the resulting data file

--- data-raw/get_productivity_anomaly.R
+++ data-raw/get_productivity_anomaly.R
@@ -8,6 +8,13 @@ get_productivity_anomaly <- function(save_clean = F) {
     productivity_anomaly_input
   ))
 
+  # Remove Tilefish (all variants) from Mid-Atlantic report per reviewer request (03/16/2026)
+  # Excludes: TILEFISH_Survey, NE LME TILEFISH_Survey, Tilefish -...-Assessment
+
+  productivity_anomaly <- productivity_anomaly[
+    !grepl("tilefish", productivity_anomaly$Var, ignore.case = TRUE),
+  ]
+
   if (save_clean) {
     usethis::use_data(productivity_anomaly, overwrite = T)
   } else {
