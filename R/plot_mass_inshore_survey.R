df4186d3a2b24c57f983044d93ea5ac809bf2546
Brandon Beltz - NOAA Affiliate <136381970+BBeltz1@users.noreply.github.com>
2026-09-25 11:29:05 -0400

    feature: change default `report` argument of `plot_mass_inshore_survey` to "NewEngland"

--- R/plot_mass_inshore_survey.R
+++ R/plot_mass_inshore_survey.R
@@ -14,7 +14,7 @@
 
 plot_mass_inshore_survey <- function(
   shadedRegion = NULL,
-  report = "MidAtlantic",
+  report = "NewEngland",
   n = 0
 ) {
   # generate plot setup list (same for all plot functions)
